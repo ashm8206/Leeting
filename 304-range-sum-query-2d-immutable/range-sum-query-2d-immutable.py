372408b568441eeb043b87c132ac6fd92ae2455f
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-01 23:34:40 -0700

    Time: 984 ms (76.99%) | Memory: 26.9 MB (95.85%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,26 +1,55 @@
 class NumMatrix:
 
-    def __init__(self, matrix: List[List[int]]):
-        self.prefix_mat = [[0]*len(matrix[0]) for _ in range(len(matrix))]
+    # https://www.youtube.com/watch?v=KE8MQuwE2yA
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if c == 0:
-                    self.prefix_mat[r][c] = matrix[r][c]
-                else:
-                    self.prefix_mat[r][c] = self.prefix_mat[r][c-1] + matrix[r][c]
+    # def __init__(self, matrix: List[List[int]]):
+    #     self.prefix_mat = [[0]*len(matrix[0]) for _ in range(len(matrix))]
+
+    #     for r in range(len(matrix)):
+    #         for c in range(len(matrix[0])):
+    #             if c == 0:
+    #                 self.prefix_mat[r][c] = matrix[r][c]
+    #             else:
+    #                 self.prefix_mat[r][c] = self.prefix_mat[r][c-1] + matrix[r][c]
+        
+    #     # print(self.prefix_mat)
+
+    # def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+  
+    #     result = 0
+    #     for r in range(row1, row2 + 1):
+    #         if col1 > 0:
+    #             result += self.prefix_mat[r][col2] - self.prefix_mat[r][col1-1]
+    #         else:
+    #             result+= self.prefix_mat[r][col2] 
+    #     return result
+
+    def __init__(self, matrix: List[List[int]]):
+
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        self.prefix_mat = [[0]*(COLS+1) for _ in range(ROWS+1)]
+
+        for r in range(1,ROWS+1):
+            prefix = 0
+            for c in range(1,COLS+1):
+                
+                prefix += matrix[r-1][c-1]
+                above =  self.prefix_mat[r-1][c]
+
+                self.prefix_mat[r][c] = prefix + above
         
         # print(self.prefix_mat)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-  
-        result = 0
-        for r in range(row1, row2 + 1):
-            if col1 > 0:
-                result += self.prefix_mat[r][col2] - self.prefix_mat[r][col1-1]
-            else:
-                result+= self.prefix_mat[r][col2] 
-        return result
+        row1, col1, row2, col2 = row1+1, col1+1, row2+1, col2+1
+
+        bottomRight = self.prefix_mat[row2][col2]
+        above = self.prefix_mat[row1-1][col2]
+        left = self.prefix_mat[row2][col1-1]
+        topRight = self.prefix_mat[row1-1][col1-1]
+
+        return bottomRight - above -left + topRight
 
 
             

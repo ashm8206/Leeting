0113acaad6a97c6fa7ce3bb68d2ad1522d095109
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-04 01:54:52 -0700

    Time: 32 ms (75.25%) | Memory: 16.5 MB (50.81%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,33 +1,53 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         # https://www.youtube.com/watch?v=qB0zZpBJlh8
+        
+        # O(N) linear time
+
+        # stack = []
+        # for char in s:
+
+        #     if char!=']':
+        #         stack.append(char)
+            
+        #     else:
+        #         # encoutered a closing bracket
+        #         substr = ''
+        #         while stack[-1]!='[':
+        #             substr = stack.pop() + substr
+        #             # append at the start, so result is not reveresed
+
+        #         stack.pop() # remove opening
+        #         k = ''
+        #         while stack and stack[-1].isdigit():
+        #             k  = stack.pop() + k 
+        #             """ for char in res:
+        #                     k = k*10 + int(char)
+        #                 since we process is reverse, above code wont work !
+        #             """
+
+        #         stack.append(substr*int(k))
+        # return "".join(stack)
 
         stack = []
+
         for char in s:
-
-            if char!=']':
-                stack.append(char)
             
-            else:
-                # encoutered a closing bracket
-                substr = ''
-                while stack[-1]!='[':
-                    substr = stack.pop() + substr
-                    # append at the start, so result is not reveresed
-
-                stack.pop() # remove opening
-                k = ''
+            if char==']':
+                res = ''
+                while stack and stack[-1]!='[':
+                    res = stack.pop() + res
+                # Pop '[' 
+                num = ''
+                stack.pop()
                 while stack and stack[-1].isdigit():
-                    k  = stack.pop() + k 
-                    """ for char in res:
-                            k = k*10 + int(char)
-                        since we process is reverse, above code wont work !
-                    """
-
-                stack.append(substr*int(k))
+                    num = stack.pop() + num
+                res = res * int(num)
+                stack.append(res)
+            else:
+                stack.append(char)
         return "".join(stack)
 
-                
 
 
 

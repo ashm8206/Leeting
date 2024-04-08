eace801dc2ba6d9e8f5a47841f5eb1b66956cb29
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-07 21:10:07 -0700

    Time: 130 ms (45.03%) | Memory: 18.1 MB (31.34%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -9,15 +9,16 @@ class Solution:
         # i moves chronologically
         # j also moves chronological to find next Non-Zero
         # This ensures that order is maintained
-        # lastZeroFoundAt = 0
-        # for fast in range(n):
+        lastZeroFoundAt = 0
+        for fast in range(n):
         #     # j = i
         #     # while j < n and nums[j]==0:
         #     #     j+=1
         #     # if j < n  and nums[j]!=0:
         #     #     nums[i], nums[j] = nums[j], nums[i]
-        #     if nums[fast]!=0 and nums[lastZeroFoundAt]==0:
-        #         nums[lastZeroFoundAt], nums[fast] = nums[fast], nums[lastZeroFoundAt]
+            if nums[fast]!=0:
+                nums[lastZeroFoundAt], nums[fast] = nums[fast], nums[lastZeroFoundAt]
+                lastZeroFoundAt+=1
 
         #     if nums[lastZeroFoundAt]!= 0:
         #         lastZeroFoundAt+=1
@@ -29,16 +30,16 @@ class Solution:
         # Method II
         # Similar to Remove Element
         # Overwrite elements + fill with zeroes
-        nextIndex = 0
+        # nextIndex = 0
 
-        for runner in range(n):
-            if nums[runner]!=0:
-                nums[nextIndex] = nums[runner]
-                nextIndex += 1
+        # for runner in range(n):
+        #     if nums[runner]!=0:
+        #         nums[nextIndex] = nums[runner]
+        #         nextIndex += 1
 
-        for i in range(nextIndex, n):
-            nums[i]=0
-        return nums
+        # for i in range(nextIndex, n):
+        #     nums[i]=0
+        # return nums
 
     
 

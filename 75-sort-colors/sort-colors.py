7d32899f8a49d79cd0b36fe754afe72845bc90d3
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-08 14:19:51 -0700

    Time: 36 ms (64.56%) | Memory: 16.4 MB (90.04%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -8,18 +8,52 @@ class Solution:
         # Time Complexity O(n), Space : O(1) --> hmap is constant space, doesnt grow with input
         #  It will be in-place, cuz yo over write the Array based on Hmap
 
-        
-        hmap = {0:0, 1:0, 2:0}
+        # Method I Bucket sort
+        # hmap = {0:0, 1:0, 2:0}
 
-        for num in nums:
-            hmap[num]+=1
+        # for num in nums:
+        #     hmap[num]+=1
         
-        index = 0
-        for key in range(0,3):
-            while hmap[key]:
-                nums[index] = key
-                hmap[key]-=1
-                index+=1
+        # index = 0
+        # for key in range(0,3):
+        #     while hmap[key]:
+        #         nums[index] = key
+        #         hmap[key]-=1
+        #         index+=1
+        # return nums
+
+        # Method II - One pass Two Pointer
+        n = len(nums)
+
+        zeroPtr = 0
+        twoPtr = n-1
+        runner = 0
+
+        while runner <= twoPtr:
+
+            if nums[runner]==0:
+                nums[zeroPtr],nums[runner] = nums[runner], nums[zeroPtr]
+                zeroPtr+=1
+                runner+=1
+
+            elif nums[runner]==2:
+                
+                nums[twoPtr],nums[runner] = nums[runner], nums[twoPtr]
+                twoPtr -=1
+
+            else:
+                runner +=1
+
         return nums
+
+
+        # [2,1,2]
+        # [2,1,2]
+
+        # [0,0 slowptr, .....,twoPtr .....]
+        # [2,0,1]
+        # [1,0,2]
+
+            
 
 

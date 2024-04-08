47209ea31550e0edaf16219d396d081e9e0999a3
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-08 14:42:32 -0700

    Time: 615 ms (5.01%) | Memory: 30.1 MB (6.90%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        # Brute force
+        # Compare Pair wise : O n^2 and O(1)
+
+        n = len(height)
+        max_area = -10**10
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         # area  = width * min(heights[l], heights[r])
+        #         area = (j-i) * min(height[i],height[j])
+        #         max_area = max(max_area,area)
+        # return max_area
+
+        # Greedy
+        #  Maximize the width * min (Max L, Max R)
+
+        L = 0
+        R = n-1
+
+        maxHeightL = height[L]
+        maxHeightR = height[R]
+
+        while L < R:
+            maxHeightL = max(height[L], maxHeightL)
+            maxHeightR = max(height[R], maxHeightR)
+
+            area = (R - L) * min(maxHeightL, maxHeightR)
+            max_area = max(area,max_area)
+
+            if height[L] <= height[R]:
+                L+=1
+            
+            else:
+                R-=1
+        return max_area
+
+
+

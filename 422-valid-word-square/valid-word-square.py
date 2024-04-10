9aa7708b062e9bf7ef6a23ea0c89f85d519599bb
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-09 17:58:40 -0700

    Time: 158 ms (5.17%) | Memory: 19.1 MB (7.01%) - LeetSync

---
+++ 422-valid-word-square/valid-word-square.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        cols = len(max(words, key=lambda x:len(x)))
+        rows = len(words)
+
+        row_wise = [['']*cols for i in range(rows)]
+
+        for i, word in enumerate(words):
+            for j, char in enumerate(word):
+
+                row_wise[i][j]=char
+        #
+        ans = True
+        for i in range(rows):
+            
+            for j in range(cols):
+                print(i,j)
+                if j >= rows or i>=cols or row_wise[i][j]!=row_wise[j][i]:
+                    ans &=False
+                else:
+                    ans &=True
+            
+            # if ans==True:
+            #     print(row_wise[i])
+            #     return True
+        return ans
+
+        

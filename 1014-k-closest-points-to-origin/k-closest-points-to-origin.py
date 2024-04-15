7b3654d1fe2cb0d91733d79c5fcfcceda03a3fa8
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-15 14:19:27 -0700

    Time: 3249 ms (5.81%) | Memory: 24.8 MB (5.06%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,56 @@
+import heapq
+import math
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        # heap = []
+        
+        
+        # for x, y in points:
+        #     dist = x**2 + y**2
+
+            
+        #     heapq.heappush(heap,(dist*-1,[x,y]))
+        #     #only after pushing we eject the smallest
+        #     # as heap reorders
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+    
+        # return [ls for _, ls in heap]
+
+        nums = [(-(x**2 + y**2),[x,y]) for x,y in points]
+
+        def partition(nums, l, r):
+
+            pivot = nums[r]
+            next_idx = l
+            
+            for i in range(l, r):
+                if nums[i][0] < pivot[0]:
+                    nums[i], nums[next_idx] = nums[next_idx], nums[i]
+                    next_idx+=1
+            nums[r], nums[next_idx] = nums[next_idx], nums[r]
+            return next_idx
+        
+        def quickSelect(nums, l, r, k):
+
+            if l==r:
+                return l
+
+            p = partition(nums, l, r)
+
+            if p < k:
+                return quickSelect(nums, p+1,r, k)
+            elif p > k:
+                return quickSelect(nums, l,p-1, k)
+            else:
+                return p
+
+        return [ ele[1] for ele in nums[quickSelect(nums,0, len(nums)-1,len(nums)-k):]]
+
+        
+        
+    
+            
+            

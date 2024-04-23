4676aa399f429f181f660734999e3cc8863675b8
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-22 21:28:38 -0700

    Time: 44 ms (28.65%) | Memory: 17.9 MB (99.75%) - LeetSync

---
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        
+        closest = 10**9
+        while root:
+            closest = min(closest, root.val, key = lambda x : (abs(x-target), x))
+            # the key is tuple becuz if there are smaller values
+            # we return the smallest value
+            if target < root.val:
+                root = root.left
+            else:
+                root = root.right
+        
+        return closest

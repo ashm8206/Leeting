e6b35e17449aff3947019a625259d649ff42f2ca
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-23 21:01:13 -0700

    Time: 45 ms (42.04%) | Memory: 17.4 MB (96.92%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        
+        res = []
+        path = []
+        curr_sum = 0
+        def helper(root, curr_sum, path):
+            if not root:
+                return
+
+            path.append(root.val)
+            curr_sum += root.val
+
+            if root.left is None and root.right is None:
+                if curr_sum == targetSum:
+                    res.append(list(path)) # new list 
+        
+
+                #Boolean Problems vs Filling the list In Problems
+            helper(root.left,curr_sum,path)
+            helper(root.right,curr_sum,path)
+            #This Step is Important while passing a Mutable list as slate.
+            # for #7 the if condition is not met
+            # 7 ---> Two null nodes on Left and Right
+            # pops 7 off the path
+
+            # then for #2  (7 is no longer on the path)
+            path.pop()
+        
+        helper(root,curr_sum,path)
+        return res
+
+        

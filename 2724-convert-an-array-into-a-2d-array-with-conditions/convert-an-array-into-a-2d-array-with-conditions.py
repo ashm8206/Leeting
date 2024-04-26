8acdf274a00b76e97594d3590e13b29e4aa76f68
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-26 15:58:51 -0700

    Time: 50 ms (71.45%) | Memory: 16.6 MB (76.80%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -4,25 +4,41 @@ class Solution:
         
 
         maps = defaultdict(int)
-        
-        maxFreq = -1
 
-        for num in nums:
-           maps[num]+=1
-           maxFreq = max(maxFreq,maps[num])
+        # Two Pass
         
-        ans = [[] for _ in range(maxFreq)]
+        # maxFreq = -1
 
-        for num in nums:
-            idx = maps.get(num)
-            ans[idx-1].append(num)
-            maps[num]-=1
+        # for num in nums:
+        #    maps[num]+=1
+        #    maxFreq = max(maxFreq,maps[num])
         
+        # ans = [[] for _ in range(maxFreq)]
+
+        # for num in nums:
+        #     idx = maps.get(num)
+        #     ans[idx-1].append(num)
+        #     maps[num]-=1
+        
+        # return ans
+
+        # One pass
+        ans = []
+        for num in nums:
+            maps[num]+=1
+            freq = maps.get(num)
+
+            # freq --> ans[freq-1]
+            # freq > len(ans) # start new array
+            # freq == len(ans) --> New Num if encountered, start new array
+            if freq > len(ans):
+                ans.append([])
+            
+            ans[freq-1].append(num)
         return ans
+            
 
-        # 1, 3, 4, 2  ()
-        # 1, 3
-        # 1
+
 
         
 

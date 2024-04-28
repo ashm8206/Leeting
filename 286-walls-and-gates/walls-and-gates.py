d71cb7d4af5d15072c498f9c09833a83f4bd2200
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-27 23:12:46 -0700

    Time: 175 ms (79.43%) | Memory: 23.1 MB (29.69%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -9,21 +9,24 @@ class Solution:
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
         INF = 2**31 - 1
         q = collections.deque()
+        visited = set()
 
         for r in range(m):
             for c in range(n):
                 if rooms[r][c]==0:
                     # gate idx and level, gates are at 0 level
                     q.append((r,c,0))
+                    visited.add((r,c))
 
     
         while q:
             r, c, level = q.popleft()
             for x, y in directions:
                 nr, nc = r+x, c+y
-                if 0 <= nr < m and 0 <= nc < n and rooms[nr][nc]==INF:
+                if 0 <= nr < m and 0 <= nc < n and rooms[nr][nc]==INF and (nr,nc) not in visited:
                     rooms[nr][nc] = level+1
                     q.append((nr,nc,level+1))
+                    visited.add((nr,nc))
         return rooms
                     
 

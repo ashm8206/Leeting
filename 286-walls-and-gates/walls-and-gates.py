d5a15cec49554cc3e4caf04a9aba584fe1afc4c2
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-27 21:58:53 -0700

    Time: 172 ms (85.44%) | Memory: 19.5 MB (66.23%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.py
@@ -0,0 +1,33 @@
+from collections import deque
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        
+        m, n = len(rooms), len(rooms[0])
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        INF = 2**31 - 1
+        q = collections.deque()
+
+        for r in range(m):
+            for c in range(n):
+                if rooms[r][c]==0:
+                    # gate idx and level, gates are at 0 level
+                    q.append((r,c,0))
+
+    
+        while q:
+            r, c, level = q.popleft()
+            for x, y in directions:
+                nr, nc = r+x, c+y
+                if 0 <= nr < m and 0 <= nc < n and rooms[nr][nc]==INF:
+                    rooms[nr][nc] = level+1
+                    q.append((nr,nc,level+1))
+        return rooms
+                    
+
+
+        
+
+        

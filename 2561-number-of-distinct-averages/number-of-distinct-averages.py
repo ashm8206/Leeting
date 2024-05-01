95e27ac0e561205325e34bea4ffa1e655dca8536
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-04-30 22:25:27 -0700

    Time: 21 ms (99.42%) | Memory: 16.6 MB (34.63%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.py
@@ -2,7 +2,30 @@ class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
         res = set()
 
-        nums.sort()
+        nums.sort() #---> Reduced to O(n) for counting sort
+
+        def cSort(nums):
+
+            n = len(nums)
+            maxNum = 100
+            output = [0]*n
+            counts = [0]*(maxNum+1)
+
+            for num in nums:
+                counts[num]+=1
+            
+            for i in range(maxNum+1):
+                if i > 0:
+                    counts[i]+=counts[i-1]
+
+            for num in nums:
+                output[counts[num]-1]=num
+                counts[num]-=1
+            
+            return output
+        
+        nums = cSort(nums)
+        
         i = 0
         j = len(nums)-1
         while i < j:

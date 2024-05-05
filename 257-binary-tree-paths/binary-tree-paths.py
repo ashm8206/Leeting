7a4bac358ee9c3b84915a9fae222e445e1a8cba6
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-05 00:32:24 -0700

    Time: 24 ms (99.13%) | Memory: 16.7 MB (11.80%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -30,23 +30,55 @@
 #         helper(root,'')
 #         return res
 
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-  def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-    ans = []
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        res = []
 
-    def dfs(root: Optional[TreeNode], path: List[str]) -> None:
-      if not root:
-        return
-      if not root.left and not root.right:
-        ans.append(''.join(path)  +str(root.val))
-        return
+        def helper(node,path):
 
-      path.append(str(root.val)+'->')
-      if root.left:
-        dfs(root.left, path)
-      if root.right:
-        dfs(root.right, path)
-      path.pop()
+            
 
-    dfs(root, [])
-    return ans
+            if node.left is None and node.right is None:
+                res.append("->".join(path[:] + [str(node.val)]))
+                return
+            
+            # path+='->'
+            path.append(str(node.val))
+            if node.left:
+                helper(node.left, path)
+        
+            if node.right:
+                helper(node.right, path)
+
+            # path.pop() # remove Left
+            # path.pop() # remove Right
+            path.pop()
+        
+        helper(root,[])
+        return res
+
+# class Solution:
+#   def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+#     ans = []
+
+#     def dfs(root: Optional[TreeNode], path: List[str]) -> None:
+#       if not root:
+#         return
+#       if not root.left and not root.right:
+#         ans.append(''.join(path)  +str(root.val))
+#         return
+
+#       path.append(str(root.val)+'->')
+#       if root.left:
+#         dfs(root.left, path)
+#       if root.right:
+#         dfs(root.right, path)
+#       path.pop()
+
+#     dfs(root, [])
+#     return ans

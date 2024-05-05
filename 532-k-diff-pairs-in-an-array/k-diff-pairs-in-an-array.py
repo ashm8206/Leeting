c0b9a10ebf400e2754caf4f933ce29de2841ff5e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-05 16:51:14 -0700

    Time: 83 ms (20.51%) | Memory: 18.5 MB (37.51%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,29 +1,26 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         
-        counter = Counter(nums)
-        # counter = {}
+        hmap = defaultdict(int)
+        unique_pairs = set()
 
-        ans = 0
-        for num in counter:
-        # for num in nums:
-
-            diff1 = num + k
-            # diff2 = num - k
-
-            
-
-            if diff1 in counter and k > 0:
-        #         print(num,diff1,'--')
-                ans += 1
-            elif k == 0 and counter[num] > 1: # > 1 ssuch that they form a pair
-                ans+=1
-    
-        return ans
-
-        # i = k +j +1 (3+2:5)
+        # i = j+k +1 (3+2:5)
         # i = j-k  + 1 (3-2:1)
+        
 
+        for num in nums:
+            diff1 = num-k
+            diff2 = num+k
+
+            if diff1 in hmap:
+                unique_pairs.add(min(diff1,num))
+            
+            if diff2 in hmap:
+                unique_pairs.add(min(diff2,num))
+        
+
+            hmap[num]+=1
+        return len(unique_pairs)
        
 
 

7c6cd448248d1a0fc5ae1f06609940824c5f7489
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-05 00:24:31 -0700

    Time: 40 ms (30.28%) | Memory: 16.6 MB (47.95%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -4,28 +4,47 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        res = []
+# class Solution:
+#     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+#         res = []
 
-        def helper(node,path):
+#         def helper(node,path):
 
-            path+=str(node.val)
+#             path+=str(node.val)
 
-            if node.left is None and node.right is None:
-                res.append((path))
-                return
+#             if node.left is None and node.right is None:
+#                 res.append((path))
+#                 return
             
-            path+='->'
+#             path+='->'
 
-            if node.left:
-                helper(node.left, path)
+#             if node.left:
+#                 helper(node.left, path)
         
-            if node.right:
-                helper(node.right, path)
+#             if node.right:
+#                 helper(node.right, path)
 
-            # path.pop() # remove Left
-            # path.pop() # remove Right
+#             # path.pop() # remove Left
+#             # path.pop() # remove Right
         
-        helper(root,'')
-        return res
+#         helper(root,'')
+#         return res
+
+class Solution:
+  def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+    ans = []
+
+    def dfs(root: Optional[TreeNode], path: List[str]) -> None:
+      if not root:
+        return
+      if not root.left and not root.right:
+        ans.append(''.join(path) + str(root.val))
+        return
+
+      path.append(str(root.val) + '->')
+      dfs(root.left, path)
+      dfs(root.right, path)
+      path.pop()
+
+    dfs(root, [])
+    return ans

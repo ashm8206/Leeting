669173b536c7599672800e6e45757d0478e1035f
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-05 17:28:37 -0700

    Time: 358 ms (76.51%) | Memory: 16.7 MB (18.49%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -10,13 +10,13 @@ class Solution:
 
             # There are k remaining values to add to the sum. The
             # average of these values is at least target // k.
-            average_value = target // k
+            # average_value = target // k
 
-            # We cannot obtain a sum of target if the smallest value
-            # in nums is greater than target // k or if the largest
-            # value in nums is smaller than target // k.
-            if average_value < nums[0] or nums[-1] < average_value:
-                return res
+            # # We cannot obtain a sum of target if the smallest value
+            # # in nums is greater than target // k or if the largest
+            # # value in nums is smaller than target // k.
+            # if average_value < nums[0] or nums[-1] < average_value:
+            #     return res
 
             if k == 2:
                 return twoSum(nums, target)

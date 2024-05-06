01e6ef1efa799bb7122a1a7c9353b1342a9ae1f6
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-05 21:58:58 -0700

    Time: 46 ms (91.45%) | Memory: 16.7 MB (19.12%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,21 +5,33 @@ class Solution:
         n = len(s)
         if n < 1:
             return 0
-
         L  = 0
-        window = ''
+
+        # window = ''
+
+        # for R in range(n):
+        #     if s[R] not in window:
+        #         window += s[R]
+        #         maxLen = max(maxLen, len(window))
+        #     else:
+                
+        #         L = window.index(s[R])
+        #         window = window[L+1:] + s[R]
+                
+        #     # print(L,R)
+        # # print(s[L])
+        # # maxLen = max(maxLen, R - L + 1)
+        # return maxLen
+
+        hmap = defaultdict(int)
 
         for R in range(n):
-            if s[R] not in window:
-                window += s[R]
-                maxLen = max(maxLen, len(window))
-            else:
-                
-                L = window.index(s[R])
-                window = window[L+1:] + s[R]
-                
-            # print(L,R)
-        # print(s[L])
-        # maxLen = max(maxLen, R - L + 1)
+            if s[R] in hmap:
+                if hmap[s[R]] >= L: #tmmzuxt --> m Start 2 --> 1 as t = 0
+                    L = hmap[s[R]] + 1
+            
+
+            maxLen = max(maxLen, R-L+1)
+            hmap[s[R]] = R
         return maxLen
             

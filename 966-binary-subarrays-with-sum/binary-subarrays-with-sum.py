a06a67d3ca433b731112d7b173f45cc1765a223e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-13 15:49:34 -0700

    Time: 215 ms (74.18%) | Memory: 20.3 MB (37.20%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,14 +1,27 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
 
-        count = 0
-        sums = 0
-        hmap = {0:1}
-
-        for i in range(len(nums)):
-            sums += nums[i]
-            if (sums - goal) in hmap:
-                count += hmap.get(sums - goal)
-            hmap[sums] = hmap.get(sums,0) + 1
-        return count
+        # sum[j] == sum[i] + k then sum[j]-sum[i] = k
         
+        hmap = {0:1}
+        curr_sum = 0
+        n = len(nums)
+        counts = 0
+
+        for i in range(n):
+            curr_sum += nums[i]
+            diff = curr_sum - goal
+
+            counts+= hmap.get(diff, 0)
+
+            hmap[curr_sum] = hmap.get(curr_sum, 0) + 1
+
+        return counts
+
+
+            # 3, 2  1: 2  goal 3
+
+            # 3, 3, 
+            # 1, 1
+
+            # 2

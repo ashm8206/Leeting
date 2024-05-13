38c1e75784cbdce394f8fd61cf00811ef8d0aa3e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-13 15:51:48 -0700

    Time: 223 ms (45.01%) | Memory: 20.3 MB (37.20%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -6,22 +6,14 @@ class Solution:
         hmap = {0:1}
         curr_sum = 0
         n = len(nums)
-        counts = 0
+        res = 0
 
         for i in range(n):
             curr_sum += nums[i]
             diff = curr_sum - goal
 
-            counts+= hmap.get(diff, 0)
+            res+= hmap.get(diff, 0)
 
             hmap[curr_sum] = hmap.get(curr_sum, 0) + 1
 
-        return counts
-
-
-            # 3, 2  1: 2  goal 3
-
-            # 3, 3, 
-            # 1, 1
-
-            # 2
+        return res

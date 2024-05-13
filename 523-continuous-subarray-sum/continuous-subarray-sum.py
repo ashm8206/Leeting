ac62e1f402c883afafba617aa9abe8c759adf454
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-13 16:27:45 -0700

    Time: 826 ms (66.05%) | Memory: 36.3 MB (39.24%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        hmap = {0: -1}
+        curr_sum = 0
+        n = len(nums)
+
+        for i in range(n):
+            curr_sum = (curr_sum + nums[i]) % k
+
+            if curr_sum in hmap.keys():
+                if i - hmap[curr_sum] > 1:
+                    return True
+            else:
+                hmap[curr_sum] = i
+        return False
+
+        

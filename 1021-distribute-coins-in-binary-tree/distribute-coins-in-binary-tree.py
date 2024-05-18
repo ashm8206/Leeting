ee14fc752ea999ad0be5408fbee5d6e676126e13
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-18 13:48:16 -0700

    Time: 38 ms (55.40%) | Memory: 16.4 MB (90.84%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
@@ -21,19 +21,14 @@ class Solution:
             # Gain or Losing  is the same as 1 move
             
             return root.val + leftMoves + rightMoves - 1
-        
 
-            
-
-            #  3, 2, 1  --> 2 + 1
-            #  4, 3, 2, 1 --> 3 + 2 + 1
-            
             #     0  1       2+ 1
-            #  2  (1, 1)    2
-            # 1, 1
-            # helper(root.left)
-            # helper(root.right)
+            #  0  (1, 1)    2
+            # 2, 2
 
+            #    4.     (-3) ==> abs 
+            #  0    0  (-2, , -1)
+            # 0        (-1)
         helper(root)
 
         return moves

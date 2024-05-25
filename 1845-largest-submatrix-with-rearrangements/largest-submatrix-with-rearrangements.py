fa9b265039fcaae01c6369736ac65a570247c139
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-05-24 18:52:57 -0700

    Time: 1107 ms (16.64%) | Memory: 39.3 MB (99.69%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.py
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.py
@@ -5,68 +5,51 @@ class Solution:
         1, 1, 2
         2, 1, 3
         """
-        heights = collections.defaultdict(list)
+    
         m = len(matrix)
         n = len(matrix[0])
-        # global_maxH = -10**9
-        # global_minH = 10**9
-
-        # for j in range(n):
-        #     col_height = 0
-        #     # maxHeight = -10**9
-        #     for i  in range(m):
-        #         if matrix[i][j]==1:
-        #             col_height+=1
-        #         else:
-        #             # Restart counting height
-        #             # while we can change order of columns
-        #             #  we cant change the order of rows 
-        #             # we must reset the max 1s Counter
-        #             # each time we get 0 
-        #             col_height = 0
-                    
-
-        #     heights[col_height].append(j) # append the column
-        #     global_maxH = max(global_maxH,col_height)
-        #     global_minH = min(global_minH,col_height)
-
         
-        # print(global_maxH)
-        # print(global_minH)
-        # print(heights)
-        # # {1: [0, 2, 4], 0: [1, 3]})
-        # curr_width = 1
-        # maxArea = -10**9
-        # for height in range(global_maxH, global_minH-1, -1):
-        #     # col_list = heights[height]
-        #     for _ in range(len(heights[height])):
-        #         area = height * curr_width
-        #         maxArea = max(maxArea, area)
-        #         # print(maxArea)
-        #         curr_width += 1
-        #     maxArea = max(maxArea, area)
-        # return maxArea
 
         # We need to examine each row. 
 
-        tower = [0]*n # first row for len(COLS)
-        maxArea = -10**9
-        for i in range(m):
+        # tower = [0]*n # first row for len(COLS)
+        # maxArea = -10**9
+        # for i in range(m):
 
+        #     for j in range(n):
+        #         if matrix[i][j] == 1:
+        #             tower[j]+=1
+        #         else:
+        #             tower[j] = 0
+                    
+        #     sorted_tower = sorted(tower, reverse = True)
+       
+        #     base = 1
+        #     for height in sorted_tower: # range(col)
+        #         area = height * base
+        #         maxArea = max(maxArea, area)
+        #         base +=1 
+        # return maxArea
+        previous_heights = []
+        maxArea = 0
+        for i in range(m):
+            heights = []
+            seen = [False]*n
+            for height, j in previous_heights:
+                if matrix[i][j]==1: # streak continues for that column
+                    heights.append((height+1, j))
+                    seen[j]=True
+                
+                # streak doesn't continue, conttributes 0, need not add to heights
             for j in range(n):
-                if matrix[i][j] == 1:
-                    tower[j]+=1
-                else:
-                    tower[j] = 0
-                # 1, 0, 2, 0, 4, 2
-                # 4, 2, 2, 1, 0, 0
-            sorted_tower = sorted(tower, reverse = True)
-            # print(sorted_tower)
-            base = 1
-            for height in sorted_tower: # range(col)
-                area = height * base
-                maxArea = max(maxArea, area)
-                base +=1 
+                if seen[j]==False and matrix[i][j]==1:
+                    # Renewing streak/ Seeing for the first time
+                    heights.append((1,j))
+            
+            for i in range(len(heights)):
+                maxArea = max(maxArea, heights[i][0]*(i+1))
+            previous_heights = heights
         return maxArea
+
                  
    

d488b8b960b6d004e1544fad672c430b9ada3bc0
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-06-08 23:08:24 -0700

    Time: 53 ms (81.92%) | Memory: 16.6 MB (69.76%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        len_s1, len_s2 = len(s1), len(s2)
+
+        if len_s2 < len_s1:
+            return False
+
+
+        s1_count, s2_count =  [0] * 26, [0] * 26
+
+        for ch in s1:
+            s1_count[ord(ch) - ord('a')] += 1
+
+        
+        for i in range(len_s2):
+
+            s2_count[ord(s2[i])-ord('a')]+=1
+
+            if i >= len_s1:
+                s2_count[ord(s2[i-len_s1])-ord('a')]-=1
+
+                # len(p) len window starts from i(end) - len_p + 1
+                #  2, 3, 4, (winLen 3)
+                #  1 is not included (index - len_p) to eject
+
+
+            if s1_count == s2_count:
+                return True
+        return False

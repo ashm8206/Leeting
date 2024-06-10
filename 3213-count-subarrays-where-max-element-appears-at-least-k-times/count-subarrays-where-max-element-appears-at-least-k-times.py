21c27ecbec52c982ebaed148ba0ccdcd4daff4ef
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-06-09 23:07:54 -0700

    Time: 861 ms (63.70%) | Memory: 30.8 MB (67.68%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,19 +1,39 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        
+
+        # https://www.youtube.com/watch?v=CZ-z1ViskzE
+        n = len(nums)
         maxElement = max(nums)
-
         ans = 0
+        max_cnt = 0 
 
-        list_of_max_element_idx = []
+        L = 0
 
-        for index, element in enumerate(nums):
-            if element == maxElement:
-                list_of_max_element_idx.append(index)
+        for R in range(n):
 
-            freq = len(list_of_max_element_idx)
-
-            if freq >= k:
-                ans += list_of_max_element_idx[-k] + 1
-                # counts the len
+            if nums[R] == maxElement:
+                max_cnt+=1
+            
+            while max_cnt == k:
+                if nums[L] == maxElement:
+                    max_cnt -=1
+                L = L + 1 # valid window 
+            # add the last index where it was still valid + 1
+            # to get total number of subarray ending at that last valid index
+            ans+= L 
         return ans
+
+        # method II
+
+        # list_of_max_element_idx = []
+
+        # for index, element in enumerate(nums):
+        #     if element == maxElement:
+        #         list_of_max_element_idx.append(index)
+
+        #     freq = len(list_of_max_element_idx)
+
+        #     if freq >= k:
+        #         ans += list_of_max_element_idx[-k] + 1
+        #         # counts the len
+        # return ans

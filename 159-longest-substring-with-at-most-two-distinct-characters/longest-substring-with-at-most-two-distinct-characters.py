5db1634b36366ea767a8b09aad0048ec869b8fad
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-06-09 21:13:31 -0700

    Time: 264 ms (42.38%) | Memory: 17.2 MB (72.56%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -7,14 +7,19 @@ class Solution:
         hmap = defaultdict(int)
 
         for R in range(len(s)):
-            hmap[s[R]] = R
+            hmap[s[R]] += 1
 
-            if len(hmap) > k:
-                # print(hmap, L)
-                del_idx = min(hmap.values()) # left most value
-                # print(del_idx)
-                del hmap[s[del_idx]]
-                L = del_idx +1
+            # if len(hmap) > k: 
+            #     # print(hmap, L)
+            #     del_idx = min(hmap.values()) # left most value o(n)
+            #     # print(del_idx)
+            #     del hmap[s[del_idx]]
+            #     L = del_idx +1
+            while len(hmap) > k:
+                hmap[s[L]]-=1
+                if hmap[s[L]]==0:
+                    del hmap[s[L]]
+                L = L + 1
 
                 # doesn't work incase of this : abaccc
                 #  you need to get min_idx

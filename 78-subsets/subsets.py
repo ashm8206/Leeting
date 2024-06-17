d643ef6e3c49c670d859e4b0c489ece8ab1cc9de
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-06-17 15:38:06 -0700

    Time: 42 ms (21.29%) | Memory: 16.7 MB (71.53%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,25 +4,27 @@ class Solution:
         res = []
         n = len(nums)
 
-        def dfs(slate, curr_idx):
+        # Method I Recursion: Take/Don't take
 
-            if curr_idx == n:
-                res.append(slate[:])
-                return
+        # def dfs(slate, curr_idx):
 
-            # dont Take
-            dfs(slate, curr_idx+1)
-            # Take
-            slate.append(nums[curr_idx]) 
-            dfs(slate, curr_idx+1)
-            slate.pop()
-
-        dfs([], 0)
-        return res
+        #     if curr_idx == n:
+        #         res.append(slate[:])
+        #         return
             
+        #     # Take
+        #     slate.append(nums[curr_idx]) 
+        #     dfs(slate, curr_idx+1)
+        #     slate.pop()
+
+        #     # dont Take
+        #     dfs(slate, curr_idx+1)
             
 
-
+        # dfs([], 0)
+        # return res
+            
+        # Method II. Intuitive "Back tracking"
 
         # def backtrack(slate,curr, k):
         #     if len(slate)==k:
@@ -35,14 +37,37 @@ class Solution:
 
         # for k in range(n+1):
         #     backtrack([], 0, k)
-        # return res
+        # return 
+        
 
-        # 1,2,3
-        # [:i]0 nums[i+1:]
-        # [] + [2,3] 0
-        # [1] + [3]  1
-        # [1,2] +    2
-        # [1,2,3].   3 (n+1)
+        # True Backtracking
 
-        # [1]   [] i=0
-        # [2] [] [2] 
+        def backstrack(slate, curr_idx):
+
+            res.append(slate[:]) # every iteration add the result
+
+            for idx in range(curr_idx, n):
+
+                slate.append(nums[idx])
+                backstrack(slate, idx+1) # take the next one
+                # keep  taking till you reach curr_idx = n
+                # then, backtrack Pop(), pop(), pop()
+                # till you reach the stem of the first choice, 
+                #             [2]
+                # (1,3) [1], [1,3] reached range(2,3) --pop [2,3]
+                # [1,2] --pop
+                # [1,2,3] --pop()
+
+                # subsets II
+                # Here is when you decide 
+                # range(1,3) [1], range(2,3) 
+                #  I have already added (0) (0,1), (0,1,2)
+                #  should I be adding (1,2) ??
+                #  if nums[2] == nums[1], just one behind, then no
+
+                #  this is a simple test case
+                #  It will be applied to General case [1, 2, 2, 2, 2]
+                slate.pop()
+
+        backstrack([],0)
+        return res

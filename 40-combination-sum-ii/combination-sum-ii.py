486820a2009c610aa5143c9d7c637d7c667144da
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-06-17 16:23:34 -0700

    Time: 49 ms (82.25%) | Memory: 16.7 MB (8.56%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -5,88 +5,90 @@ class Solution:
 
         # Sorted or Conter are two approaches for dealing with duplicates in inputstream
 
-        # SORTING
-        res = []
+        # Method - I SORTING
+        # res = []
       
-        candidates.sort()
+        # candidates.sort()
  
-        def backtrack(slate,currSum, curr_idx):
-            if currSum == 0:
-               res.append(slate[:])
-               return
+        # def backtrack(slate,currSum, curr_idx):
+        #     if currSum == 0:
+        #        res.append(slate[:])
+        #        return
             
 
         
-            for i in range(curr_idx, len(candidates)):
-                #constraint 1, don't create duplicate loops same candidate
-                if i > curr_idx and candidates[i] == candidates[i-1]:
-                    # you hav already processed this combination
-                    # When you processed i - 1
-                    continue
-
-                pick = candidates[i]
-
-                #optimization: 
-                if currSum - pick < 0:
-                    # we break here, cuz we sorted the input.
-                    #  any future addition, is going to be greater in value
-                    # This depleting are curr_sum further, so not worth it
-                    #  lets just break out of the loop
-                    break
-                slate.append(pick)
-                backtrack(slate, currSum - pick, i + 1 ) # use each idx only Once
-                slate.pop()
-
-        backtrack([],target, 0)
-        return res
-
-        # Method II - Counter
-        # def backtrack(comb, remain, curr, counter, results):
-        #     print(counter[curr:], comb)
-        #     if remain == 0:
-        #             # make a deep copy of the current combination
-        #             #   rather than keeping the reference.
-        #             results.append(list(comb))
-        #             return
-        #     if remain < 0:
-        #             return
-
-        #     for next_curr in range(curr, len(counter)):
-               
-        #         candidate, freq = counter[next_curr]
-
-        #         if freq <= 0:
+        #     for i in range(curr_idx, len(candidates)):
+        #         #constraint 1, don't create duplicate loops same candidate
+        #         if i > curr_idx and candidates[i] == candidates[i-1]:
+        #             # you hav already processed this combination
+        #             # When you processed i - 1
         #             continue
 
-        #         # add a new element to the current combination
-        #         comb.append(candidate)
-        #         counter[next_curr] = (candidate, freq-1)
+        #         pick = candidates[i]
 
-        #         # continue the exploration with the updated combination
-        #         backtrack(comb, remain - candidate, next_curr, counter, results)
+        #         #optimization: 
+        #         if currSum - pick < 0:
+        #             # we break here, cuz we sorted the input.
+        #             #  any future addition, is going to be greater in value
+        #             # This depleting are curr_sum further, so not worth it
+        #             #  lets just break out of the loop
+        #             break
+        #         slate.append(pick)
+        #         backtrack(slate, currSum - pick, i + 1 ) # use each idx only Once
+        #         slate.pop()
 
-        #         # backtrack the changes, so that we can try another candidate
-        #         counter[next_curr] = (candidate, freq)
-        #         comb.pop()
+        # backtrack([],target, 0)
+        # return res
 
-        # results = []  # container to hold the final combinations
-        # counter = Counter(candidates)
-        # # convert the counter table to a list of (num, count) tuples
-        # # because [1, 1, 6] == [6,1,1] are same. 
-        # # hence once we exhaust the counter for selection
-        # #then we move to next iteration, knowing that all valid combinations of the max 1s have been choosen and completed.
-        # # we shrink space by processing counter as list as and passing a 'start' ptr named next_curr to the the range func of the iteration
-        # # backtrack(comb, remain - candidate, next_curr, counter, results)
-        # # we don't increment the next_ptr as we need to give it another chance to exhaust
-        # # COMBINATION SUM I
-        # #https://leetcode.com/problems/combination-sum/
+        # Method II - Counter
+        def backtrack(slate, curr_sum, curr_idx):
+        
+            
+            if curr_sum == 0:
+                    # make a deep copy of the current combination
+                    # rather than keeping the reference.
+                    results.append(slate[:])
+                    return
 
-        # counter = [(c, counter[c]) for c in counter]
+            for i in range(curr_idx, len(counter)):
+               
+                candidate, freq = counter[i]
 
-        # backtrack(comb = [], remain = target, curr = 0,
-        #         counter = counter, results = results)
+                if freq <= 0 or freq > 0 and curr_sum - candidate < 0:
+                    continue
 
-        # return results
+                # add a new element to the current combination
+                slate.append(candidate)
+                counter[i] = (candidate, freq-1)
+
+                # continue the exploration with the updated combination
+                backtrack(slate, curr_sum - candidate, i,) 
+                # pass same "i" as we want to exhaust the counter before moving
+
+                # backtrack the changes, so that we can try another candidate
+                counter[i] = (candidate, freq)
+                slate.pop()
+
+        results = []  # container to hold the final combinations
+        counter = Counter(candidates)
+
+        # convert the counter table to a list of (num, count) tuples
+        # because [1, 1, 6] == [6,1,1] are same. 
+        # hence once we exhaust the counter for selection
+        # then we move to next iteration, knowing that all valid combinations of the max 1s have been choosen and completed.
+  
+        # Read COMBINATION SUM I for details
+        # https://leetcode.com/problems/combination-sum/
+
+        
+
+        results = []  # container to hold the final combinations
+        counter = Counter(candidates)
+        counter = [(c, counter[c]) for c in counter]
+
+        backtrack([], target,0)
+
+        return results
 
 
-        #
+    

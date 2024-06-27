d24bf64f7d3665449a4d8e09c35c606427d5942d
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-06-26 19:58:07 -0700

    Time: 29 ms (88.27%) | Memory: 16.3 MB (96.73%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -4,82 +4,59 @@ class Solution:
         
         # O(N) linear time
 
-        # stack = []
-        # for char in s:
-
-        #     if char!=']':
-        #         stack.append(char)
-            
-        #     else:
-        #         # encoutered a closing bracket
-        #         substr = ''
-        #         while stack[-1]!='[':
-        #             substr = stack.pop() + substr
-        #             # append at the start, so result is not reveresed
-
-        #         stack.pop() # remove opening
-        #         k = ''
-        #         while stack and stack[-1].isdigit():
-        #             k  = stack.pop() + k 
-        #             """ for char in res:
-        #                     k = k*10 + int(char)
-        #                 since we process is reverse, above code wont work !
-        #             """
-
-        #         stack.append(substr*int(k))
-        # return "".join(stack)
-
-        # stack = []
-
-        # for char in s:
-            
-        #     if char==']':
-        #         res = ''
-        #         while stack and stack[-1]!='[':
-        #             res = stack.pop() + res
-        #         # Pop '[' 
-        #         num = 0
-        #         k = 0
-        #         stack.pop()
-        #         ## Another alternative
-        #         # Can be done in the same way as stack
-        #         while stack and stack[-1].isdigit():
-        #             num = int(stack.pop())*10**k + num
-        #             k+=1
-        #         res = res * int(num)
-        #         stack.append(res)
-        #     else:
-        #         stack.append(char)
-        # return "".join(stack)
-
-
-        # Method II
-
         stack = []
-        counterStack = []
-
         for char in s:
+
+            if char==']':
+                # encoutered a closing bracket
+                substr = ''
+                while stack and stack[-1]!='[':
+                    substr = stack.pop() + substr
+                    # append at the start, so result is not reveresed
+
+                stack.pop() # remove opening bracket
+
+                num = ''
+                while stack and stack[-1].isdigit():
+                    num  = stack.pop() + num
+                    """ for char in res:
+                            num = num*10 + int(char)
+                    since we process is reverse for stack, above code wont work !
+                    """
+                stack.append(substr*int(num))
             
-            if char.isdigit():
-                # save as Str
-                counterStack.append(char)
-
-            elif char == "]":
-                res = ''
-                while stack and stack[-1]!="[":
-                    res = stack.pop() + res
-                stack.pop()  # "remove [ "
-
-                count = counterStack.pop()
-                stack.append(res*count)
-            else: # [ or char
-                stack.append(char)
-                if char == '[':
-                    res = ''
-                    while counterStack and not isinstance(counterStack[-1],int):
-                        res = counterStack.pop() + res
-                    counterStack.append(int(res))
+            else:
+                  stack.append(char)
         return "".join(stack)
+
+
+
+        # Method II - Two Stacks
+
+        # stack = []
+        # counterStack = []
+
+        # for char in s:
+            
+        #     if char.isdigit():
+
+        #         counterStack.append(char)
+
+        #     elif char == "]":
+        #         res = ''
+        #         while stack and stack[-1]!="[":
+        #             res = stack.pop() + res
+        #         stack.pop()  # "remove [ "
+        #         count = counterStack.pop()
+        #         stack.append(res*count)
+        #     else: # [ or char
+        #         stack.append(char)
+        #         if char == '[':
+        #             res = ''
+        #             while counterStack and not isinstance(counterStack[-1],int):
+        #                 res = counterStack.pop() + res
+        #             counterStack.append(int(res))
+        # return "".join(stack)
                     
 
 

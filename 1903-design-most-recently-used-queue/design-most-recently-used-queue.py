2ea2e326055da333f793f34f5e7154c03a77ec31
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-07-20 13:34:10 -0700

    Time: 176 ms (61.50%) | Memory: 18.6 MB (11.36%) - LeetSync

---
+++ 1903-design-most-recently-used-queue/design-most-recently-used-queue.py
@@ -0,0 +1,35 @@
+from sortedcontainers import SortedList
+
+# LinkedList / Array 
+
+# Arrays
+# get O(1)
+# Fetch in O(n) as you gotta move N elements
+
+# Doubly Linked, (Pop in the Middle),  Doubly works better as we need the pointer to the element before
+# get O(n)
+# fetch O(1)
+
+# TreeSet, AVL Tree SortedList 
+# What is AVL, How does Self-balancing Work
+# log(n) < SQRT(N)
+class MRUQueue:
+
+    def __init__(self, n: int):
+        self.q = SortedList([ (i, i) for i in range(1, n+1)])
+       
+        # Stored as Index, Value, sorted on Index
+
+    def fetch(self, k: int) -> int:
+        _ , value  = self.q.pop(k-1) # 1 indexed, make it 0
+        last_idx = self.q[-1][0] if self.q else 0
+        self.q.add((last_idx+1, value))
+
+        return value
+        
+        
+
+
+# Your MRUQueue object will be instantiated and called as such:
+# obj = MRUQueue(n)
+# param_1 = obj.fetch(k)

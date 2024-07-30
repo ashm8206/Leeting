07ee709be03b733a8d42de4184c10842fc184170
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-07-30 00:13:44 -0700

    Time: 1552 ms (27.26%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,14 +1,33 @@
 -- Write your PostgreSQL query statement below
 
-    select
-        product_id,
-        year as first_year, 
-        quantity,
-        price
-    from sales p1
-    where year = (
-            select 
-                min(year) 
-            from sales p2
-            where p1.product_id = p2.product_id
-     group by p2.product_id )
+    -- select
+    --     product_id,
+    --     year as first_year, 
+    --     quantity,
+    --     price
+    -- from sales p1
+    -- where year = (
+    --         select 
+    --             min(year) 
+    --         from sales p2
+    --         where p1.product_id = p2.product_id
+    --  group by p2.product_id )
+
+--Option 2
+
+SELECT 
+  product_id, 
+  year AS first_year, 
+  quantity, 
+  price 
+FROM 
+  Sales 
+WHERE 
+  (product_id, year) IN (
+    SELECT 
+      product_id, 
+      MIN(year) AS year 
+    FROM 
+      Sales 
+    GROUP BY 
+      product_id)

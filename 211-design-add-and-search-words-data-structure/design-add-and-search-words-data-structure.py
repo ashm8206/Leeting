0060b6914f2e72a35e57e2731480bc71efeb6415
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-01 22:34:50 -0700

    Time: 1061 ms (94.85%) | Memory: 51.2 MB (87.94%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -12,20 +12,33 @@ class WordDictionary:
         node["$"] = True
 
     def search_node(self, word: str, node : dict) -> bool:
-        # {'a': {'$': True}}
+
         for i, char in enumerate(word):
             
-            if char == ".":
-                for key in node.keys():
-                    # print(node, word)
-                    if key!='$' and self.search_node(word[i+1:],node[key]):
-                        # print("am i here?")
-                        return True
+            if char not in node:
+                if char == ".":
+                    for key in node.keys():
+                        """
+                        For every key at this level, excluding "$" ending char
+                        as it is not a valid char in input, but a stub created 
+                        by us.
+
+                        for every key, pass their mapping node[key] and 
+                        the word[i+1: ], as it is the next char after "."
+
+                        Start a new search(word, node[key]) for all key
+                        mappings at this level, if any of them match return True
+
+                        if None of they match, return True
+
+                        """
+                        if key!='$' and self.search_node(word[i+1:],node[key]):
+                            return True
                 return False
-            elif char in node:
-                node = node[char]
+
             else:
-                return False
+                node = node[char]
+           
         return "$" in node
 
 

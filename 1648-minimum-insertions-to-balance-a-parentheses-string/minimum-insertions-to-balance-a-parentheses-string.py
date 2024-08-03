eadbd890f58474a33895d97b0b5d98155dd2b151
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-02 23:45:24 -0700

    Time: 87 ms (86.17%) | Memory: 17.3 MB (45.13%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,47 +1,42 @@
 class Solution:
-
     def minInsertions(self, s: str) -> int:
-        # Keep track of how many closing brackets we need as we iterate through s.
-        # These are the invalid states where we need to insert brackets:
-        # 1. Odd number of closing brackets required. Requires inserting 1 more close bracket.
-        # 2. Negative number of closing brackets required (i.e excess closing brackets). Requires inserting 1 more close bracket if odd. Insert (closing brackets required // 2) opening brackets
-        # Handle excess close required brackets at the end of s as well.
-        
-        closeRequired = 0
-        count = 0
-        a  = 0
-        
-        while a < len(s):
-            if s[a] == "(":
-                closeRequired += 2
-                a += 1
+        l = len(s)        
+
+        # result = 0        
+        # candidates_right = []
+        # for i in range(l-1, -1, -1):
+        #     if s[i] == ")":
+        #         candidates_right.append(")")
+        #     else:
+        #         if len(candidates_right) == 0:
+        #             result += 2
+        #         elif len(candidates_right) == 1:
+        #             candidates_right.pop()
+        #             result += 1
+        #         else:
+        #             candidates_right.pop()
+        #             candidates_right.pop()
+        # return result + len(candidates_right)//2 + (len(candidates_right)%2) * 2
+
+        result = 0
+        right_needed = 0
+        for c in s:
+            if c == "(":
+                right_needed += 2
+                if right_needed % 2 == 1:
+                    result += 1
+                    right_needed -= 1
             else:
-                while a < len(s) and s[a] == ')':
-                    closeRequired -= 1
-                    a += 1
-                                    
-                # 1. Positive closeRequired
-                # Even closeRequired -> do nothing
-                # Odd closeRequired -> insert 1 more close bracket, subtract 2 closeRequired -> insert = 1
-                
-                # 2. Negative closeRequired 
-                # Even closeRequired -> Add closeRequired // 2 open brackets -> closeRequired = 0
-                # Odd closeRequired -> Add 1 extra closeRequired and insert [closeRequired //2] open brackets -> closeRequired = 0
-                
-                if closeRequired > 0:
-                    if closeRequired % 2 == 0:
-                        continue
-                    closeRequired -= 1
-                    count += 1
-                else:
-                    if abs(closeRequired) % 2 == 1:
-                        closeRequired -= 1
-                        count += 1
-                        
-                    count += abs(closeRequired) // 2
-                    closeRequired = 0
-        
-        return count + closeRequired
+                right_needed -= 1
+                if right_needed < 0:
+                    result += 1
+                    right_needed += 2
+        return result + right_needed
+
+
+
+
+
         
 
 

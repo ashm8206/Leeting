190ebd5c61c911d7f096a1909d7a7c12258ef215
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-03 15:16:42 -0700

    Time: 29 ms (94.25%) | Memory: 16.6 MB (21.23%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -4,27 +4,57 @@ class Solution:
         stack = []
 
         directories = path.split("/")
+        # # This deals with the multiple lash problem
+        # # split on '/' we are dealing only with directory names
 
-        for directory in directories:
-            if directory == '' or directory =='.':
-                continue
-            elif directory=='..':
+        print(directories)
+        for dr in directories:
+            if dr == '..':
                 if stack:
                     stack.pop()
+                    
+            elif dr == '.' or dr=='':
+                continue
             else:
-                stack.append(directory)
-        print(directories)
-        return "/"+ "/".join(stack)
+                # "..." is valid
+                # any directory name
+                stack.append(dr)
+        
+        return "/" +  "/".join(stack)
+
         
 
-        """"
-        Input: "/a//b//c//////d"
-        OP: "/a/b/c/d"
+        # """"
+        # Input: "/a//b//c//////d"
+        # OP: "/a/b/c/d"
 
-        "/home/user/./Downloads/../Pictures/././"
-        "/home/user/Pictures"
+        # "/home/user/./Downloads/../Pictures/././"
+        # "/home/user/Pictures"
 
-        "/../"
-        "/"
-        --> Split  : ['', '..', '']
-        """
+        # "/../"
+        # "/"
+        # --> Split  : ['', '..', '']
+        # """
+
+       
+        
+        print(directorties)
+        for dr in directorties:
+            if dr == '..':
+                if stack:
+                    stack.pop()
+                else:
+                    # if stack is empty and you cant pop, you have reached the root
+                    return '/'
+            elif dr == '.' or dr=='':
+                continue
+            else:
+                # "..." is valid
+                # any directory name
+                stack.append(dr)
+        
+        return "/" +  "/".join(stack)
+
+        # Join works by adding "-" or " " between two values of iterable
+            
+            

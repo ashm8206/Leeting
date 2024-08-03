eb50438ec0057914abb2fa3920188908dd9999a4
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-03 00:11:53 -0700

    Time: 92 ms (77.43%) | Memory: 17.3 MB (45.13%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.py
@@ -2,6 +2,8 @@ class Solution:
     def minInsertions(self, s: str) -> int:
         l = len(s)        
 
+        # Method I --> Doesnt work
+
         # result = 0        
         # candidates_right = []
         # for i in range(l-1, -1, -1):
@@ -16,7 +18,10 @@ class Solution:
         #         else:
         #             candidates_right.pop()
         #             candidates_right.pop()
+        # print(len(candidates_right)%2, result, len(candidates_right)//2  )
         # return result + len(candidates_right)//2 + (len(candidates_right)%2) * 2
+        # return unbalanced_closing + open_brackets_needed for //2 closed_back +
+        #  if closed==odd then (1 missing +1 closing)
 
         result = 0
         right_needed = 0
@@ -24,14 +29,24 @@ class Solution:
             if c == "(":
                 right_needed += 2
                 if right_needed % 2 == 1:
-                    result += 1
-                    right_needed -= 1
+                    result += 1 # add missing closing bracket
+                    right_needed -= 1 # balance off the right odd
             else:
+                # Found One Right
+                # Remove the 1 right_needed we counted
                 right_needed -= 1
                 if right_needed < 0:
+                    # excess of closing bracket
                     result += 1
+                    # Add one opening bracket, and the right needed for it
                     right_needed += 2
         return result + right_needed
+
+
+        # Method 3 :
+        #  replace '))' by "}", add space : O(n)
+        #  do simple minimum add 921
+        #  return len(openstack)*2 --> closing required  + closed (missing 1 open each)
 
 
 

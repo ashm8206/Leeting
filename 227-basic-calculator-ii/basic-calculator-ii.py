51d7fa90f6fdfb26f33a0ac4a931204a5493e13e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-05 15:30:17 -0700

    Time: 104 ms (57.11%) | Memory: 19.9 MB (54.44%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -6,10 +6,13 @@ class Solution:
         operation = '+'
         curr_num = ''
 
-        for ch in s:
+        for i, ch in enumerate(s):
+    
+            if ch.isdigit():
+                curr_num = curr_num + ch
 
-            if ch in ('+','-','*','/'):
-
+            if ch in ('+','-','*','/') or i == len(s)-1 :
+                
                 # Excecute the previous operation 1st
                 curr_num = int(curr_num)
                 if operation == '-':
@@ -25,30 +28,11 @@ class Solution:
                     stack.append(int(op1/op2))
                 else:
                     stack.append(int(curr_num))
-
-
                 # update the operation
                 # update the curr_number
                 curr_num = ''
                 operation = ch
-            else:
-                curr_num = curr_num + ch
-
-        curr_num = int(curr_num)
-        if operation == '-':
-            stack.append(-curr_num)
-        elif operation =='*':
-                    # no check required as expression is valid
-            op1 = stack.pop()
-            op2 = curr_num
-            stack.append(op1*op2)
-        elif operation =='/':
-            op1 = stack.pop()
-            op2 = curr_num
-            stack.append(int(op1/op2))
-        else:
-            stack.append(int(curr_num))
-
+            
 
         return sum(stack) 
         

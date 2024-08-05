6795e622634758157504a2997a1687cef7b61761
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-05 15:50:18 -0700

    Time: 76 ms (44.16%) | Memory: 18 MB (64.40%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,19 +1,53 @@
+# class Solution:
+#     def calculate(self, s: str) -> int:
+#         stack = deque()
+#         result = last_num = cur_num = 0
+#         pre_sign = '+'
+#         s += '+'
+#         for c in s:
+#             if c.isdigit():
+#                 cur_num = cur_num * 10 + int(c)
+#             if c == '(':
+#                 stack.append(result)
+#                 stack.append(last_num)
+#                 stack.append(pre_sign)
+#                 result = last_num = 0
+#                 pre_sign = '+'
+#             elif c in '+-*/)':
+#                 if pre_sign == '+':
+#                     result += last_num
+#                     last_num = cur_num
+#                 elif pre_sign == '-':
+#                     result += last_num
+#                     last_num = -cur_num
+#                 elif pre_sign == '*':
+#                     last_num *= cur_num
+#                 else:
+#                     last_num = int(last_num / cur_num)
+#                 if c == ')':
+#                     cur_num = result + last_num
+#                     pre_sign = stack.pop()
+#                     last_num = stack.pop()
+#                     result = stack.pop()
+#                 else:
+#                     pre_sign = c
+#                     cur_num = 0
+#         return result + last_num
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = deque()
+        self.index = 0
+        return self.evaluate(s + '+')
+
+    def evaluate(self, s: str) -> int:
         result = last_num = cur_num = 0
         pre_sign = '+'
-        s += '+'
-        for c in s:
-            if c.isdigit():
-                cur_num = cur_num * 10 + int(c)
-            if c == '(':
-                stack.append(result)
-                stack.append(last_num)
-                stack.append(pre_sign)
-                result = last_num = 0
-                pre_sign = '+'
-            elif c in '+-*/)':
+        while self.index < len(s):
+            if s[self.index].isdigit():
+                cur_num = cur_num * 10 + int(s[self.index])
+            elif s[self.index] == '(':
+                self.index += 1
+                cur_num = self.evaluate(s)
+            elif s[self.index] in '+-*/)':
                 if pre_sign == '+':
                     result += last_num
                     last_num = cur_num
@@ -24,12 +58,9 @@ class Solution:
                     last_num *= cur_num
                 else:
                     last_num = int(last_num / cur_num)
-                if c == ')':
-                    cur_num = result + last_num
-                    pre_sign = stack.pop()
-                    last_num = stack.pop()
-                    result = stack.pop()
-                else:
-                    pre_sign = c
-                    cur_num = 0
+                if s[self.index] == ')':
+                    break
+                pre_sign = s[self.index]
+                cur_num = 0
+            self.index += 1
         return result + last_num

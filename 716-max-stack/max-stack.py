4b5a3709a16bd740bf2bcd0ad8be6412a8d80c82
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-06 16:33:27 -0700

    Time: 355 ms (81.20%) | Memory: 65.2 MB (36.50%) - LeetSync

--- 716-max-stack/max-stack.py
+++ 716-max-stack/max-stack.py
@@ -1,33 +1,50 @@
-from sortedcontainers import SortedList
+# from sortedcontainers import SortedList
+import heapq
+
 class MaxStack:
 
     def __init__(self):
-        self.stack = SortedList()
-        self.max_values = SortedList()
+        # self.stack = SortedList()
+        # self.max_values = SortedList()
+        self.stack = []
+        self.heap = []
+        self.deleted_ids = set()
         self.id = 0
         
 
     def push(self, x: int) -> None:
-        self.stack.add((self.id, x))
-        self.max_values.add((x, self.id))
+        self.stack.append(( x, self.id))
+        heapq.heappush(self.heap, (-x, -self.id))
+        # self.max_values.add((x, self.id))
         self.id+=1
 
     def pop(self) -> int:
-        idx, val = self.stack.pop()
-        self.max_values.remove((val, idx))
+
+        while self.stack and self.stack[-1][1] in self.deleted_ids:
+            self.stack.pop()
+
+        val, idx = self.stack.pop()
+        self.deleted_ids.add(idx)
         return val
         
 
     def top(self) -> int:
-        return self.stack[-1][1]
+        while self.stack and self.stack[-1][1] in self.deleted_ids:
+            self.stack.pop()
+        return self.stack[-1][0] 
 
     def peekMax(self) -> int:
-        return self.max_values[-1][0]
+        while self.heap and -self.heap[0][1] in self.deleted_ids:
+            heapq.heappop(self.heap)
+        return -self.heap[0][0]
 
     def popMax(self) -> int:
-        val, idx = self.max_values.pop()
-        self.stack.remove((idx, val))
-        return val
+        while self.heap and -self.heap[0][1] in self.deleted_ids:
+            heapq.heappop(self.heap)
+
+        val, idx = heapq.heappop(self.heap)
+        self.deleted_ids.add(-idx)
+        return -val
 
 
 # Your MaxStack object will be instantiated and called as such:

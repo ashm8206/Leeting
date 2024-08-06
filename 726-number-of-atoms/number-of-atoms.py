92ed6cd325efef9e79cab10cd5939bb704aa56da
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-05 17:39:31 -0700

    Time: 38 ms (44.96%) | Memory: 16.6 MB (50.87%) - LeetSync

---
+++ 726-number-of-atoms/number-of-atoms.py
@@ -0,0 +1,42 @@
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        stack = []
+        atom = ''
+        count = ''
+        running_multiplier = 1
+
+        hmap = {}
+        
+
+
+        for ch in formula[::-1]:
+
+            if ch.isalpha():
+
+                if ch.isupper():
+                    atom = ch + atom 
+                    if count: 
+                        count = int(count) * running_multiplier
+                    else:
+                        count = 1 * running_multiplier
+                    hmap[atom] = hmap.get(atom, 0) + count
+
+                    count = ''
+                    atom = ''
+                if ch.islower():
+                    atom = ch + atom
+            elif ch.isdigit():
+                count = ch + count
+
+            elif ch is ')':
+                curr_multiplier = int(count) if count else 1
+                stack.append(curr_multiplier)
+                running_multiplier *= curr_multiplier
+                count = ""
+
+            elif ch == '(':
+                # reset it 
+                running_multiplier //= stack.pop()
+        
+        return "".join([ key + str(value) if value > 1 else key for key, value in sorted(hmap.items()) ])
+

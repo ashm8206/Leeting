0baa52b0a29ba4cf44a68402df71fd0ea5473822
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-06 14:33:03 -0700

    Time: 1093 ms (14.82%) | Memory: 56.5 MB (5.48%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -10,6 +10,8 @@ class Solution:
         # For post -processing initial indexes of positions is stored
         HMAP = { pos: i for i, pos in enumerate(positions)}
 
+        print(HMAP) # Yes Hmap of Pos: index in Position array works as all positions are unique
+
 
         for i, wt in enumerate(wt_positions):
             if directions[i] == 'L':
@@ -21,28 +23,29 @@ class Solution:
   
 
         stack = []
-        for r, pos in wt_positions:
-            while stack and r < 0 and stack[-1][0] > 0:
+
+        for wt, pos in wt_positions:
+            while stack and wt < 0 and stack[-1][0] > 0:
                 # collison only occurs with pos followed by negative
-                diff = r + stack[-1][0]
+                diff = wt + stack[-1][0]
                 if diff < 0:
                     # new robot Wins
                     stack.pop()
-                    r = (abs(r)-1)*(-1) 
+                    wt = (abs(wt)-1)*(-1) 
                     """
                     intersting piece of "decrement" has to be done on a positive value
                     """
                 elif diff > 0:
                     # new robot loses, but there is a collision, so we dec stack
-                    stack[-1][0] -=1
-                    r = 0  
+                    stack[-1][0] -= 1 
+                    wt = 0  
                 else:
                     # Both destroy each other
-                    r = 0 # dont add to stack
+                    wt = 0 # dont add to stack
                     stack.pop()
             
-            if r!=0: # positive or neagtive
-                stack.append([r,pos])
+            if wt!=0: # positive or neagtive
+                stack.append([wt,pos])
 
     
         # print(stack)

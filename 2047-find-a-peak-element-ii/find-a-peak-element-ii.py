523f084419d40e5e081d6225fef9f1fd08b027f3
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-15 19:26:57 -0700

    Time: 912 ms (29.01%) | Memory: 47.9 MB (43.82%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -11,31 +11,21 @@ class Solution:
         #         top = mid+1
         # return [bottom,mat[bottom].index(max(mat[bottom]))]
 
-        # rows, cols = len(mat), len(mat[0])
-        # leftCol, rightCol = 0, cols
-        # while leftCol < rightCol:
-        #     col = leftCol + (rightCol - leftCol) // 2
-        #     row = max(range(rows), key=lambda r: mat[r][col])
-        #     left = mat[row][col-1] if col > 0 else -1
-        #     right = mat[row][col+1] if col < cols - 1 else -1
-        #     if mat[row][col] > left and mat[row][col] > right:
-        #         return (row, col)
-        #     elif mat[row][col] < left:
-        #         rightCol = col
-        #     else:
-        #         leftCol = col + 1
-        # return []
-
-        l, r = 0, len(mat) - 1
-        while l < r:
-            i = (l + r) // 2
-            j = mat[i].index(max(mat[i]))
-
-            down = mat[i+1][j] if i+1 < len(mat) else -1
-
-            if mat[i][j] < down:
-                l = i + 1
+        rows, cols = len(mat), len(mat[0])
+        leftCol, rightCol = 0, cols - 1
+        while leftCol <= rightCol:
+            col = leftCol + (rightCol - leftCol) // 2
+            row = max(range(rows), key=lambda r: mat[r][col])
+            left = mat[row][col-1] if col > 0 else -1
+            right = mat[row][col+1] if col < cols - 1 else -1
+            if mat[row][col] > left and mat[row][col] > right:
+                return (row, col)
+            elif mat[row][col] < left:
+                rightCol = col
             else:
-                r = i
-        
-        return [l, mat[l].index(max(mat[l]))]
+                leftCol = col + 1
+        return []
+
+        # [[25,37,23,37,19],[45,19,2,43,26],[18,1,37,44,50]]
+
+      

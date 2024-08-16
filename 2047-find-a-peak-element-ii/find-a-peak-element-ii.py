00ac117056b2d2de15f99b84892274a975cee1b4
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-15 19:23:09 -0700

    Time: 899 ms (52.71%) | Memory: 47.8 MB (74.26%) - LeetSync

---
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        # Row nlog M = M*N 
+        # top = 0
+        # bottom = len(mat)-1
+        # while bottom > top:
+        #     mid = (top + bottom) // 2
+        #     if max(mat[mid]) > max(mat[mid+1]):
+        #         bottom = mid
+        #     else:
+        #         top = mid+1
+        # return [bottom,mat[bottom].index(max(mat[bottom]))]
+
+        rows, cols = len(mat), len(mat[0])
+        leftCol, rightCol = 0, cols
+        while leftCol < rightCol:
+            col = leftCol + (rightCol - leftCol) // 2
+            row = max(range(rows), key=lambda r: mat[r][col])
+            left = mat[row][col-1] if col > 0 else -1
+            right = mat[row][col+1] if col < cols - 1 else -1
+            if mat[row][col] > left and mat[row][col] > right:
+                return (row, col)
+            elif mat[row][col] < left:
+                rightCol = col
+            else:
+                leftCol = col + 1
+        return []

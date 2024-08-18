cdac954db0444a8b634243916c72d5c53c5a4250
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-17 21:14:02 -0700

    Time: 160 ms (33.28%) | Memory: 16.7 MB (43.76%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -2,28 +2,28 @@ class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
         
         # Method I - Optimal : O(n^2), O(1) space
-        def check(arr, start, end):
-            minNum = min(arr)
-            maxNum = max(arr)
+        # def check(arr, start, end):
+        #     minNum = min(arr)
+        #     maxNum = max(arr)
 
-            diff, remainder = divmod(maxNum - minNum, end - start)
+        #     diff, remainder = divmod(maxNum - minNum, end - start)
             
-            if remainder > 0:
-                '''There is a remainder, 
-                so range can't be divided equally'''
-                return False
+        #     if remainder > 0:
+        #         '''There is a remainder, 
+        #         so range can't be divided equally'''
+        #         return False
             
-            #okay we got 0 remainder, 
-            # so range can be divided equally, 
-            #  but are this numbers present in the array ??
+        #     #okay we got 0 remainder, 
+        #     # so range can be divided equally, 
+        #     #  but are this numbers present in the array ??
 
-            "We check that below"
-            curr = minNum
-            while curr < maxNum:
-                if curr not in arr:
-                    return False
-                curr += diff
-            return True
+        #     "We check that below"
+        #     curr = minNum
+        #     while curr < maxNum:
+        #         if curr not in arr:
+        #             return False
+        #         curr += diff
+        #     return True
 
         # Method II  O(n^2) / On^2logn, O(n) space
         def check(arr, start, end):

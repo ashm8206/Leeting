3da8278662f006590f575e2071e42a955d023fbe
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-18 11:09:29 -0700

    Time: 40 ms (24.38%) | Memory: 16.5 MB (70.65%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # dp = [[0] * n for _ in range(m)]
-        # dp[0][0] = 1 # Base case
-        
-
-        # for row in range(m):
-        #     for col in range(n):
-        #         if row > 0:
-        #             dp[row][col] += dp[row - 1][col]
-        #         if col > 0:
-        #             dp[row][col] += dp[row][col - 1]
-        
-        # return dp[m - 1][n - 1]
+        # 2*2 Mat
 
         0 # 0, 1
         1 # 1  2
@@ -26,20 +15,39 @@ class Solution:
         # return helper(m-1,n-1)
 
         # Memoization
-        cache = {}
+        # cache = {}
 
-        def helper(m,n):
-            if m==0 or n==0:
-                return 1
+        # def helper(m,n):
+        #     if m==0 or n==0:
+        #         return 1
             
-            if (m,n) in cache:
-                return cache[(m,n)]
+        #     if (m,n) in cache:
+        #         return cache[(m,n)]
             
-            cache[(m,n)] = helper(m-1,n) + helper(m, n-1)
+        #     cache[(m,n)] = helper(m-1,n) + helper(m, n-1)
             
-            return cache[(m,n)]
+        #     return cache[(m,n)]
 
-        return helper(m-1,n-1)
+        # return helper(m-1,n-1)
+
+        # Tabulation
+
+        dp = [ [ 0 for _ in range(n) ]for _ in range(m)]
+
+        # Tricky bit, but only 1 way of reaching at start
+        dp[0][0] = 1
+
+        for row in range(m):
+            for col in range(n):
+                if row > 0:
+                    dp[row][col] += dp[row-1][col] 
+                if col > 0:
+                    dp[row][col] += dp[row][col-1]
+        return dp[m-1][n-1] 
+
+
+
+
 
 
         

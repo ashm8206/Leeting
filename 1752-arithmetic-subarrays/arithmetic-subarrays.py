945a4614e09c3c538a90c6fb7a42859f14fb1e49
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-17 21:11:48 -0700

    Time: 151 ms (50.25%) | Memory: 16.7 MB (43.76%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -1,32 +1,47 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        # Can m ? > n 
-        # 4 6 = # (3-1)/2
-        # 4 9 = # 3/5
-        # 3 9 = 6/3
-        #  7,10 = 15/3
-        # 8, 9 = Max-min/Len(Range)
-        #  -12+3 /4
+        
+        # def check(arr, start, end):
+        #     minNum = min(arr)
+        #     maxNum = max(arr)
+
+        #     diff, remainder = divmod(maxNum - minNum, end - start)
+            
+        #     if remainder > 0:
+        #         '''There is a remainder, 
+        #         so range can't be divided equally'''
+        #         return False
+            
+        #     #okay we got 0 remainder, 
+        #     # so range can be divided equally, 
+        #     #  but are this numbers present in the array ??
+
+        #     "We check that below"
+        #     curr = minNum
+        #     while curr < maxNum:
+        #         if curr not in arr:
+        #             return False
+        #         curr += diff
+        #     return True
 
         def check(arr, start, end):
-            minNum = min(arr)
-            maxNum = max(arr)
-
-            diff, remainder = divmod(maxNum - minNum, end - start)
-            
-            if remainder > 0:
-                return False
-
-            curr = minNum
-            while curr < maxNum:
-                if curr not in arr:
+         
+            temp = sorted(arr) # Nlogn or N
+            # print(temp)
+            diff = set()
+            for i in range(1, end-start+1):
+                
+                diff.add(temp[i]-temp[i-1])
+                # print(i, diff)
+                if len(diff) > 1:
                     return False
-                curr += diff
             return True
 
         res = []
         for start, end in zip(l,r):
+            
             res.append(check(nums[start:end+1], start, end))
+           
         return res
 
 

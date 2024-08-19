6ee415b821c91fa02bac69f044eb9b03e2de4315
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-18 23:01:17 -0700

    Time: 45 ms (26.64%) | Memory: 16.8 MB (22.08%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        wordDict = set(wordDict)
+        n = len(s)
+
+        @lru_cache(maxsize=None)
+        def helper(start, s):
+            if start >= n:
+                return True
+                
+            for end in range(start, n+1):
+             
+                if s[start: end] in wordDict:
+              
+                    if helper(end, s):
+                        
+                        return True
+            return False
+
+        return helper(0, s)

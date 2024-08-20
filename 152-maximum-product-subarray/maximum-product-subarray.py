49407e17676f0e060987561112e9b1aa8ed9647f
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-19 18:46:40 -0700

    Time: 82 ms (9.19%) | Memory: 17 MB (81.40%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,33 +1,40 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         maxProduct = -10**9
-        maxSoFar = -10**9
-        minSoFar = 10**9
+        # maxSoFar = -10**9
+        # minSoFar = 10**9
 
         n = len(nums)
-        dp =[0]*n
-        dp_min = [0] * n
+        # dp =[0]*n
+        # dp_min = [0] * n
 
-        dp[0] = nums[0]
-        dp_min[0] = nums[0]
+        # dp[0] = nums[0]
+        # dp_min[0] = nums[0]
 
-        for i in range(n):
-            if i > 0:
-                dp[i] = max([dp[i-1]*nums[i], dp_min[i-1]*nums[i], nums[i]])
-                dp_min[i] = min([dp[i-1]*nums[i],dp_min[i-1]*nums[i], nums[i]])
-            else:
-                dp[i] = nums[i]
-                dp_min[i] = nums[i]
-
-            maxProduct = max([maxProduct, dp[i]])
-        return maxProduct
-        
-       
         # for i in range(n):
-        #     tempProd = max(nums[i], max(maxSoFar*nums[i],minSoFar*nums[i]))
-        #     maxProduct = max(tempProd,maxProduct)
-        #     maxSoFar = max(nums[i], max(maxSoFar*nums[i],minSoFar*nums[i]))
-        #     minSoFar = min(nums[i], min(maxSoFar*nums[i],minSoFar*nums[i]))
+        #     if i > 0:
+        #         dp[i] = max([dp[i-1]*nums[i], dp_min[i-1]*nums[i], nums[i]])
+        #         dp_min[i] = min([dp[i-1]*nums[i],dp_min[i-1]*nums[i], nums[i]])
+        #     else:
+        #         dp[i] = nums[i]
+        #         dp_min[i] = nums[i]
+
+        #     maxProduct = max([maxProduct, dp[i]])
+        # print(dp)
+        # print(dp_min)
         # return maxProduct
+        
+        maxSoFar = nums[0]
+        minSoFar = nums[0]
+        maxProduct = nums[0]
+
+        for i in range(1,n):
+            tempProd = max(nums[i], max(maxSoFar*nums[i],minSoFar*nums[i]))
+            maxProduct = max(tempProd,maxProduct)
+            tempMax= maxSoFar*nums[i]
+            tempMin = minSoFar*nums[i]
+            maxSoFar = max(nums[i], max(tempMax,tempMin))
+            minSoFar = min(nums[i], min(tempMax,tempMin))
+        return maxProduct
 
         

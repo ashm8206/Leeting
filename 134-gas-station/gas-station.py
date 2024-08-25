152fc609539ebc1616f5f28163b6d3e6f850b595
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-08-24 18:42:03 -0700

    Time: 869 ms (98.77%) | Memory: 26.3 MB (5.08%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
@@ -17,16 +18,34 @@ class Solution:
         #             return start
         # return -1
 
-        # O(N)
+        # Deque :
+        diff = [x - y for x, y in zip(gas, cost)] * 2
         n = len(gas)
-        curr_petrol = 0
-        prev_petrol  = 0
-        start = 0
-        for i in range(n):
-            curr_petrol += gas[i] - cost[i]
-            if curr_petrol < 0:
-                prev_petrol += curr_petrol
-                start = i+1
-                curr_petrol = 0
+        reach = deque([])
+        cur_tank = 0
+        for i in range(len(diff)):
+            while reach and cur_tank < 0:
+                cur_tank -= diff[reach.popleft()]
+
+            cur_tank += diff[i]
+            reach.append(i)
+            
+            if len(reach) == n and cur_tank >= 0:
+                return reach.popleft()
+        return -1
+                
+
+
+        # Greedy O(N)
+        # n = len(gas)
+        # curr_petrol = 0
+        # prev_petrol  = 0
+        # start = 0
+        # for i in range(n):
+        #     curr_petrol += gas[i] - cost[i]
+        #     if curr_petrol < 0:
+        #         prev_petrol += curr_petrol
+        #         start = i+1
+        #         curr_petrol = 0
         
-        return start if curr_petrol + prev_petrol >=0 else -1
+        # return start if curr_petrol + prev_petrol >=0 else -1

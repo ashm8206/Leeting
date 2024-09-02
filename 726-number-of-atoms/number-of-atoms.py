664fd13008a54b61f99cfe85fb3a9ca4be2e14bc
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-02 16:40:14 -0700

    Time: 39 ms (36.62%) | Memory: 16.6 MB (51.22%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.py
+++ 726-number-of-atoms/number-of-atoms.py
@@ -6,39 +6,6 @@ class Solution:
         running_multiplier = 1
         hmap = {}
         
-
-
-        # for ch in formula[::-1]:
-
-        #     if ch.isalpha():
-
-        #         if ch.isupper():
-        #             atom = ch + atom 
-        #             if count: 
-        #                 count = int(count) * running_multiplier
-        #             else:
-        #                 count = 1 * running_multiplier
-        #             hmap[atom] = hmap.get(atom, 0) + count
-
-        #             count = ''
-        #             atom = ''
-        #         if ch.islower():
-        #             atom = ch + atom
-        #     elif ch.isdigit():
-        #         count = ch + count
-
-        #     elif ch is ')':
-        #         curr_multiplier = int(count) if count else 1
-        #         multiplier_stack.append(curr_multiplier)
-        #         running_multiplier *= curr_multiplier
-        #         count = ""
-
-        #     elif ch == '(':
-        #         # reset it 
-        #         running_multiplier //= multiplier_stack.pop()
-        
-        # return "".join([ key + str(value) if value > 1 else key for key, value in sorted(hmap.items()) ])
-
         for ch in formula[::-1]:
             if ch.isalpha():
                 if ch.isupper():
@@ -66,6 +33,7 @@ class Solution:
                 count = ""
             elif ch == '(':
                 running_multiplier //= multiplier_stack.pop()
+                continue
 
         smap =[ k + str(v) if v > 1 else k for k, v in sorted(hmap.items(), key = lambda k : k[0])]
         print(smap)

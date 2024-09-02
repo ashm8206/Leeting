7b69262e9891120eb9317f6f7d4574780d33695e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-02 14:45:01 -0700

    Time: 129 ms (66.57%) | Memory: 31.2 MB (13.55%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -11,14 +11,13 @@ class Solution:
             nonlocal count
             if not root:
                 return 
-            
+
             if maxVal <= root.val:
                 count+=1
-            
-            maxVal= max(maxVal,root.val)
-        
-            helper(root.left,maxVal)
-            helper(root.right,maxVal)
 
+            maxVal = max(root.val, maxVal)
+            helper(root.left, maxVal)
+            helper(root.right, maxVal)
+            
         helper(root, -10**9)
         return count

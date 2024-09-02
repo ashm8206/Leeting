05220bb58e574f29bee591b3aa76ec585e018cb1
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-02 16:38:43 -0700

    Time: 45 ms (5.91%) | Memory: 16.6 MB (51.22%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.py
+++ 726-number-of-atoms/number-of-atoms.py
@@ -1,6 +1,6 @@
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        stack = []
+        multiplier_stack = []
         atom = ''
         count = ''
         running_multiplier = 1
@@ -8,34 +8,65 @@ class Solution:
         
 
 
+        # for ch in formula[::-1]:
+
+        #     if ch.isalpha():
+
+        #         if ch.isupper():
+        #             atom = ch + atom 
+        #             if count: 
+        #                 count = int(count) * running_multiplier
+        #             else:
+        #                 count = 1 * running_multiplier
+        #             hmap[atom] = hmap.get(atom, 0) + count
+
+        #             count = ''
+        #             atom = ''
+        #         if ch.islower():
+        #             atom = ch + atom
+        #     elif ch.isdigit():
+        #         count = ch + count
+
+        #     elif ch is ')':
+        #         curr_multiplier = int(count) if count else 1
+        #         multiplier_stack.append(curr_multiplier)
+        #         running_multiplier *= curr_multiplier
+        #         count = ""
+
+        #     elif ch == '(':
+        #         # reset it 
+        #         running_multiplier //= multiplier_stack.pop()
+        
+        # return "".join([ key + str(value) if value > 1 else key for key, value in sorted(hmap.items()) ])
+
         for ch in formula[::-1]:
-
             if ch.isalpha():
-
                 if ch.isupper():
-                    atom = ch + atom 
+                    atom = ch + atom  # pre-pend the Upper it
+                    # we hit the boundary condition for starting new atom
+                    # add the atom in Map
                     if count: 
                         count = int(count) * running_multiplier
                     else:
                         count = 1 * running_multiplier
                     hmap[atom] = hmap.get(atom, 0) + count
 
-                    count = ''
                     atom = ''
+                    count = ''
                 if ch.islower():
                     atom = ch + atom
+        
             elif ch.isdigit():
                 count = ch + count
 
-            elif ch is ')':
+            elif ch == ')':
                 curr_multiplier = int(count) if count else 1
-                stack.append(curr_multiplier)
+                multiplier_stack.append(curr_multiplier)
                 running_multiplier *= curr_multiplier
                 count = ""
-
             elif ch == '(':
-                # reset it 
-                running_multiplier //= stack.pop()
-        
-        return "".join([ key + str(value) if value > 1 else key for key, value in sorted(hmap.items()) ])
+                running_multiplier //= multiplier_stack.pop()
 
+        smap =[ k + str(v) if v > 1 else k for k, v in sorted(hmap.items(), key = lambda k : k[0])]
+        print(smap)
+        return "".join(smap)

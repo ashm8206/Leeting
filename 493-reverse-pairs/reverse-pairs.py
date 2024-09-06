1db24247b09c69d36949d25dc5026f15607ad4fe
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-06 03:18:06 -0700

    Time: 1376 ms (11.32%) | Memory: 24 MB (64.67%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.py
+++ 493-reverse-pairs/reverse-pairs.py
@@ -1,18 +1,15 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        if len(nums)<2:
-            return 0
-        pre = []
-        res = 0
-        for i in range(len(nums)):
-            if not pre:
-                pre.append(nums[i])
-            else:
-                index = bisect.bisect_right(pre,2*nums[i])
-                if index < len(pre):
-                    res += len(pre) - index
-                bisect.insort(pre,nums[i])
-        return res
+        elems = []
+        N = len(nums)
+        ans = 0
+        for ii in range(N-1, -1, -1):
+            at = bisect_left(elems, nums[ii])
+            if at:
+                ans += at
+            insort(elems, nums[ii] * 2)
+        return ans
         # worst case, which is O(n log n). 
         # The algorithm as a whole still has a running time of O(n2) on average because of the series of swaps required for each insertion.
         return counts
+        # https://leetcode.com/problems/reverse-pairs/solutions/355494/simple-python-bisect-solution/

52b906e53a6bd98c1d2bbf469c2c5d29ade017a7
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-05 21:01:56 -0700

    Time: 46 ms (51.23%) | Memory: 19.4 MB (76.63%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,27 +6,53 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        queue = deque([root])
-        heap = []
+        # queue = deque([root])
+        # heap = []
 
-        while queue:
-            curr = queue.popleft()
+        # while queue:
+        #     curr = queue.popleft()
             
-            if len(heap) < k:
-                heappush(heap, -curr.val)
-            elif heap[0] <= -curr.val:
-                heappop(heap)
-                heappush(heap, -curr.val)
+        #     if len(heap) < k:
+        #         heappush(heap, -curr.val)
+        #     elif heap[0] <= -curr.val:
+        #         heappop(heap)
+        #         heappush(heap, -curr.val)
 
-            if curr.left:
-                queue.append(curr.left)
+        #     if curr.left:
+        #         queue.append(curr.left)
 
-            if curr.right:
-                queue.append(curr.right)
+        #     if curr.right:
+        #         queue.append(curr.right)
 
-        if heap:
-            return heap[0] * -1
-        return -1
+        # if heap:
+        #     return heap[0] * -1
+        # return -1
+
+        
+        if not root:
+            return -1
+
+        curr = root
+        stack = [curr]
+
+        while stack or curr:
+
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+
+            curr = stack.pop()
+            k-=1 
+
+            if k==0:
+                return curr.val
+            
+            curr = curr.right
+
+
+        
+        
 
 
         # -3 - 2, -1

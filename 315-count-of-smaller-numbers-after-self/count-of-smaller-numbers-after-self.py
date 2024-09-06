89a17a9856ca2f6d52846819aded19549ebc50b4
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-05 20:46:01 -0700

    Time: 2498 ms (28.26%) | Memory: 36.1 MB (68.26%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -8,5 +8,7 @@ class Solution:
             counts.append(idx)
     
             bisect.insort(done, num)
-       
+
+        # worst case, which is O(n log n). 
+        # The algorithm as a whole still has a running time of O(n2) on average because of the series of swaps required for each insertion.
         return counts[::-1]

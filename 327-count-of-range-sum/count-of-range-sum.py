7b242c079fa2e042b911d62ea9e44b3c9881ec86
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-06 02:54:40 -0700

    Time: 1264 ms (87.68%) | Memory: 32.1 MB (55.69%) - LeetSync

--- 327-count-of-range-sum/count-of-range-sum.py
+++ 327-count-of-range-sum/count-of-range-sum.py
@@ -1,12 +1,19 @@
+from sortedcontainers import SortedList 
+
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
         ans = prefix = 0 
-        seen = [0]
+        # seen = [0]
+        sl = SortedList([0])
         for x in nums: 
             prefix += x 
-            lo = bisect_left(seen, prefix - upper)
-            hi = bisect_left(seen, prefix - lower + 1) 
-            ans += hi - lo
-            insort(seen, prefix)
+            # lo = bisect_left(seen, prefix - upper)
+            # hi = bisect_left(seen, prefix - lower + 1) 
+            lo = sl.bisect_left(prefix - upper)
+            hi = sl.bisect_left(prefix - lower + 1) - 1
+            ans += hi - lo + 1
+            # ans += hi - lo
+            # insort(seen, prefix)
+            sl.add(prefix)
         return ans 
         # https://leetcode.com/problems/count-of-range-sum/solutions/1195369/python3-4-solutions

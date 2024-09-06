468d6e8baa0e2ad92b584c98a84621c875b538ee
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-05 23:58:37 -0700

    Time: 100 ms (94.34%) | Memory: 23.6 MB (85.80%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        # Method I
+        # queue = collections.deque()
+
+        # queue.append(root)
+
+        # res = 0
+
+        # # I want to touch each node once
+        # while queue:
+        #     node = queue.popleft()
+        #     if low <= node.val <= high:
+        #         res+=node.val
+            
+        #     if node.left:
+        #         queue.append(node.left)
+            
+        #     if node.right:
+        #         queue.append(node.right)
+
+        # return res
+
+        #  Method II
+
+        def dfs(node):
+            nonlocal ans
+            if node:
+                if low <= node.val <= high:
+                    ans += node.val
+                if low < node.val:
+                    dfs(node.left)
+                if node.val < high:
+                    dfs(node.right)
+
+        ans = 0
+        dfs(root)
+        return ans

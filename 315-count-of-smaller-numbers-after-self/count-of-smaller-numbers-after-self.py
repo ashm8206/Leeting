c728deaeae9a7d90406df1ec162001856ec197c4
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-05 20:39:46 -0700

    Time: 2481 ms (30.73%) | Memory: 36.1 MB (69.50%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -2,12 +2,11 @@ class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         counts = []
         done = []
-        s=set()
+      
         for num in nums[::-1]:
             idx=bisect.bisect_left(done, num)
             counts.append(idx)
     
-            s.add(num)
             bisect.insort(done, num)
        
         return counts[::-1]

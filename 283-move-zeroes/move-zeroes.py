824b11d4fbb6551994f5c9ca45a7342ad8a323bf
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-15 00:19:53 -0700

    Time: 121 ms (78.80%) | Memory: 18.1 MB (59.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,27 +4,31 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        # j moves from i ---> n
-        # and finds a new Non-zero to write to i
-        # i moves chronologically
-        # j also moves chronological to find next Non-Zero
-        # This ensures that order is maintained
+
+        # Slow ptr keeps track of next position to place the element
+        # Fast Pointer parse the elements
+
+        # All elements before the slow pointer (lastNonZeroFoundAt) are non-zeroes.
+        # All elements between slow ptr .... fast ptr may contain Zeroes.
+        #  They may contain Zeroes if there were indeed Zeroes in the array
+        #  If there were no zeros, they will never show up
+      
         lastZeroFoundAt = 0
-        for fast in range(n):
-        #     # j = i
-        #     # while j < n and nums[j]==0:
-        #     #     j+=1
-        #     # if j < n  and nums[j]!=0:
-        #     #     nums[i], nums[j] = nums[j], nums[i]
+        for fast in range(n): # reader Pointer
+
             if nums[fast]!=0:
                 nums[lastZeroFoundAt], nums[fast] = nums[fast], nums[lastZeroFoundAt]
+
+                # you cant guarantee that "lastZeroFoundAt" will alwyas contain zero, but when  you are copying, a non-zero into this location
+                # Doesn't hurt to copy whatever is in lastzero to the "fast" ptrs location
+                # Eventually, if there are zeroes in the array. 
+                # We are bound to reach them as InsertIdx progresses, 
+                # as they were never copied, previously.
+                # It is these "Zeros" that will be copied into non-zero future "fast" positions
                 lastZeroFoundAt+=1
 
-        #     if nums[lastZeroFoundAt]!= 0:
-        #         lastZeroFoundAt+=1
 
-        #Slow ptr keeps track of next position to place the element
-        # Fast Pointer parse the elements
+ 
 
 
         # Method II

f3980eee9d0abc31c0815c2d97234cd3eb29087b
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-19 22:33:21 -0700

    Time: 38 ms (55.94%) | Memory: 17.3 MB (79.24%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -10,28 +10,28 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
 
+        # Recursive
+        # self.visited = {}
 
-        self.visited = {}
+        # def helper(node):
 
-        def helper(node):
-
-            if not node:
-                return None
+        #     if not node:
+        #         return None
             
-            if node in self.visited:
-                return self.visited[node]
+        #     if node in self.visited:
+        #         return self.visited[node]
             
-            new_node = Node(node.val, None, None)
+        #     new_node = Node(node.val, None, None)
 
-            self.visited[node] = new_node
+        #     self.visited[node] = new_node
 
-            new_node.next = helper(node.next) if node.next else None
-            new_node.random = helper(node.random) if node.random else None
+        #     new_node.next = helper(node.next) if node.next else None
+        #     new_node.random = helper(node.random) if node.random else None
 
-            # Postorder will create recursion exceeded. as Random could pt to a node already seen.
+        #     # Postorder will create recursion exceeded. as Random could pt to a node already seen.
 
-            return new_node
-        return helper(head)
+        #     return new_node
+        # return helper(head)
 
 
         #  Iterative + O(N)
@@ -101,6 +101,42 @@ class Solution:
         #     ptrB = ptrB.next
         # # We cant combine the two Random+unweaving cuz we need next ptrs intact
         # return newHead
+
+
+        if not head:
+            return None
+        
+        curr = head
+
+        # A-->A'-->B-->B'
+        while curr:
+            new_node = Node(curr.val, None, None)
+            new_node.next = curr.next
+            curr.next = new_node
+            curr = new_node.next
+        
+        # Set Random ptrs
+
+        curr = head
+        while curr and curr.next:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+
+        ptrA = head
+        ptrB = head.next
+        dummy = Node(-1)
+        dummy.next = head.next
+
+        while ptrA and ptrB:
+            ptrA.next = ptrB.next
+            ptrB.next = ptrB.next.next if ptrB.next else None
+
+            ptrA = ptrB.next
+            ptrB = ptrB.next
+        
+        return dummy.next
+
 
     
     

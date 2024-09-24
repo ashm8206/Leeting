f16db337dcac68b660dab3e4c5ce85d0cf46dbc0
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-23 21:41:07 -0700

    Time: 47 ms (30.11%) | Memory: 16.9 MB (8.22%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+
+        def getRunLen(rle_str):
+            rle_str = deque(list(rle_str))
+            stack = []
+
+            while rle_str:
+                var = rle_str.popleft()
+                if stack and stack[-1][0] == var:
+                    stack[-1][1]+=1
+                else:
+                    stack.append([var, 1])
+            RLE = "".join( str(y) + x  for x, y in stack)
+            
+            return RLE
+
+        RLE = '1'
+        while n > 1:
+            RLE = getRunLen(RLE)
+            n-=1 
+        return RLE

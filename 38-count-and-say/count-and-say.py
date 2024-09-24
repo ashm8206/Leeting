b606c2794dc6bbdf57b00025ecb0a4509d369ce8
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-23 22:08:24 -0700

    Time: 45 ms (42.52%) | Memory: 17 MB (5.14%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -2,40 +2,42 @@ class Solution:
     def countAndSay(self, n: int) -> str:
 
         # Iterative
-        # def getRunLen(rle_str):
-        #     rle_str = deque(list(rle_str))
-        #     stack = []
+        def getRunLen(rle_str):
+            # rle_str = deque(list(rle_str))
+            rle_str = list(rle_str)
+            stack = []
             
-        #     while rle_str:
-        #         var = rle_str.popleft()
-        #         if stack and stack[-1][0] == var:
-        #             stack[-1][1]+=1
-        #         else:
-        #             stack.append([var, 1])
-        #     RLE = "".join( str(y) + x  for x, y in stack)
+            while rle_str:
+                # var = rle_str.popleft()
+                var = rle_str.pop(0)
+                if stack and stack[-1][0] == var:
+                    stack[-1][1]+=1
+                else:
+                    stack.append([var, 1])
+            RLE = "".join( str(y) + x  for x, y in stack)
             
-        #     return RLE
+            return RLE
 
-        # RLE = '1'
-        # while n > 1:
-        #     RLE = getRunLen(RLE)
-        #     n-=1 
-        # return RLE
+        RLE = '1'
+        while n > 1:
+            RLE = getRunLen(RLE)
+            n-=1 
+        return RLE
 
         # Recursive
-        def rle(s):
-            count = 1
-            ret = []
-            n = len(s)-1
-            for i in range(0,n+1):
-                if i<n and s[i] == s[i+1]:
-                    count += 1
-                else:
-                    ret.append(str(count))
-                    ret.append(s[i])
-                    count = 1
-            return "".join(ret) 
+        # def rle(s):
+        #     count = 1
+        #     ret = []
+        #     n = len(s)-1
+        #     for i in range(0,n+1):
+        #         if i<n and s[i] == s[i+1]:
+        #             count += 1
+        #         else:
+        #             ret.append(str(count))
+        #             ret.append(s[i])
+        #             count = 1
+        #     return "".join(ret) 
         
-        if n == 1:
-            return "1"
-        return rle(self.countAndSay(n-1))
+        # if n == 1:
+        #     return "1"
+        # return rle(self.countAndSay(n-1))

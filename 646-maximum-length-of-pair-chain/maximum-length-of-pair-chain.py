0e96ecc4de619153e5810be2da0ff3c563fa0700
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-28 19:55:11 -0700

    Time: 1349 ms (16.94%) | Memory: 17 MB (88.89%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
@@ -1,16 +1,33 @@
 import bisect
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-        pairs.sort(key = lambda x: x[0])
+
+        # # Method I
+        # pairs.sort(key = lambda x: x[0])
       
-        lastEnd = pairs[0][1]
-        n = len(pairs)
-        ans = 1
-        for start, end in pairs:
-            if start > lastEnd:
-                ans += 1
-                lastEnd = end
-            else:
-                lastEnd = min(lastEnd, end)
+        # lastEnd = pairs[0][1]
+        # n = len(pairs)
+        # ans = 1
+        # for start, end in pairs:
+        #     if start > lastEnd:
+        #         ans += 1
+        #         lastEnd = end
+        #     else:
+        #         lastEnd = min(lastEnd, end)
             
-        return ans
+        # return ans
+
+        # Method II
+
+        pairs.sort()
+        n = len(pairs)
+        dp = [1 for i in range(n)]
+        maxLen = 1
+
+        for i in range(1, n):
+            for j in range(0, i):
+                if pairs[j][1] < pairs[i][0]:
+                    dp[i] = max(dp[i], dp[j]+1)
+            maxLen = max(maxLen, dp[i])
+     
+        return maxLen

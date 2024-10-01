6197fd71d6eea66254cd6070cfe35cf919c94f4e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-01 15:13:14 -0700

    Time: 578 ms (45.28%) | Memory: 16.8 MB (72.08%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.py
@@ -5,11 +5,11 @@ class Solution:
 
         dp = [-1 for i in range(n)]
 
-        dp[0] = 0
+        dp[0] = 0  # you can jump from 0 to 0 in 0 jumps
 
         for i in range(1, n):
             for j in range(0, i):
                 if abs(nums[i]-nums[j]) <= target and dp[j]!=-1:
                     dp[i] = max(dp[i], dp[j]+1)
-        
-        return dp[n-1]
+                    # dp[j]!=-1: means we should've reached here before
+        return dp[n-1] 

9aca83dd1ac1ed9027a02e32788aa9d337ee438e
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-09-30 21:34:38 -0700

    Time: 388 ms (17.52%) | Memory: 21.8 MB (37.54%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -1,17 +1,27 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         # https://leetcode.com/problems/maximum-sum-circular-subarray/solutions/775875/3-questions-1-approach-python/?envType=study-plan-v2&envId=top-interview-150
-        A = nums
-        if max(A) <= 0:
-            return max(A)
-            
-        max_dp = [i for i in A]
-        min_dp = [i for i in A]
-        
-        for i in range(1,len(A)):
-            if max_dp[i-1] > 0:
-                max_dp[i] += max_dp[i-1]
-            if min_dp[i-1] < 0:
-                min_dp[i] += min_dp[i-1]
 
-        return max(max(max_dp), sum(A) - min(min_dp))
+        # Do the Duplication Approach
+        # https://leetcode.com/problems/maximum-sum-circular-subarray/solutions/957969/python-dp-memo-o-n-2-o-2n-o-n/?envType=study-plan-v2&envId=top-interview-150
+        
+        A = nums
+
+        total = A[0]
+        max_sum = curr_max = min_sum = curr_min = A[0] 
+      
+        
+        for i in range(1, len(A)): 
+            curr_max = max(A[i], curr_max + A[i]) 
+            max_sum = max(max_sum, curr_max)
+            curr_min = min(A[i], curr_min + A[i]) 
+            min_sum = min(min_sum, curr_min)
+            total += A[i]
+
+        return max(max_sum, total- min_sum) if max_sum > 0 else max_sum
+      
+
+
+
+        
+        

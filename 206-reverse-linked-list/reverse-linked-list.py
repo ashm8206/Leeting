7698af3e0f53fbb8288d523a8b6627f3927e382a
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-07 13:21:26 -0700

    Time: 37 ms (56.07%) | Memory: 17.8 MB (7.15%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,64 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        newHead = None
+        
+        # if head is None or head.next is None:
+        #     return head
+        # Iterative
+        # prev, curr = None, head 
+
+        # # dummy = ListNode(-1)
+        # # dummy.next = head
+
+        # curr = head
+        # prev = None
+
+        # while curr:
+
+        #     temp = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = temp
+
+        # # dummy.next = prev
+
+        # # return dummy.next 
+        # return prev
+
+
+        #Recursion
+        # TakeYouForward https://www.youtube.com/watch?v=D2vI2DNJGd8&t=920s
+        # newHead = self.reverseList(head.next)
+        # front = head.next
+        # front.next = head
+        # head.next = None
+
+        # return newHead
+
+        def helper(head):
+            nonlocal newHead
+            if head is None or head.next is None:
+                if newHead is None:
+                    newHead = head
+                return head
+            
+            last = helper(head.next)
+            last.next = head
+            head.next = None
+            last = head
+
+            return last
+
+        helper(head)
+        return newHead
+
+
+        

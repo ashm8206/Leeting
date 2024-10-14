a05123402ce68285c3f0d4ebbab9e1ababcba6f1
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-13 19:50:18 -0700

    Time: 1617 ms (16.84%) | Memory: 42.6 MB (29.73%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -40,18 +40,21 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
+        # Print LCS
         # lcs = []
         # i = m
         # j = n
-        # while i >  0 and j > 0:
+        # while i > 0 and j > 0:
         #     # print(i-1, j-1)
         #     if s1[i-1]==s2[j-1]:
         #         lcs.append(s1[i-1])
+        #         i-=1
+        #         j-=1
             
         #     elif dp[i-1][j] > dp[i][j-1]:
         #         i-=1
         #     else:
         #         j-=1
-        # print("".join(lcs))
+        # print("".join(reversed(lcs)))
       
         return dp[m][n]

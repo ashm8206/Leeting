7b8e4437edac80e6b92c7f69bcde22d9b04580a6
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-13 18:02:37 -0700

    Time: 768 ms (10.60%) | Memory: 246.9 MB (5.05%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+       
+
+        m = len(s)
+        n = len(t)
+        memo = {}
+
+        def helper(i, j):
+
+            if i==m or j==n:
+                return 1 if j==n else 0
+
+            if (i,j) in memo:
+                return memo[(i,j)]
+          
+            ans = helper(i+1,j)
+
+            # print(ans)
+
+            if s[i]==t[j]:
+                ans += helper(i+1,j+1)
+
+            memo[(i,j)] = ans
+
+            return memo[(i,j)]
+
+        return helper(0,0)
+    

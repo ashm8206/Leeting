e7d9c0ca85341a5fad5dd071c088064649140b3d
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-16 18:24:21 -0700

    Time: 91 ms (80.77%) | Memory: 19.1 MB (97.24%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -3,7 +3,7 @@ class Solution:
 
         
         # stack = []
-        operation = '+'
+        pre_operation = '+'
         curr_num = ''
         last_num = 0
         result = 0
@@ -17,18 +17,18 @@ class Solution:
                 
                 # Excecute the previous operation 1st
                 curr_num = int(curr_num)
-                if operation == '-':
+                if pre_operation == '-':
                     # stack.append(-curr_num)
                     result += last_num
                     last_num = - curr_num
 
-                elif operation =='*':
+                elif pre_operation =='*':
                     # no check required as expression is valid
                     # op1 = stack.pop()
                     # op2 = curr_num
                     # stack.append(op1*op2)
                     last_num = last_num * curr_num
-                elif operation =='/':
+                elif pre_operation =='/':
                     # op1 = stack.pop()
                     # op2 = curr_num
                     # stack.append(int(op1/op2))
@@ -40,7 +40,7 @@ class Solution:
                 # update the operation
                 # update the curr_number
                 curr_num = ''
-                operation = ch
+                pre_operation = ch
         
        
         # while stack:

cad2fe04a84557e8c65c54a820b7f18a2d8db385
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-16 18:20:40 -0700

    Time: 96 ms (74.57%) | Memory: 19.2 MB (84.01%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -2,39 +2,51 @@ class Solution:
     def calculate(self, s: str) -> int:
 
         
-        stack = []
+        # stack = []
         operation = '+'
         curr_num = ''
-
+        last_num = 0
+        result = 0
+        s += '+'
         for i, ch in enumerate(s):
     
             if ch.isdigit():
                 curr_num = curr_num + ch
 
-            if ch in ('+','-','*','/') or i == len(s)-1 :
+            if ch in ('+','-','*','/'):
                 
                 # Excecute the previous operation 1st
                 curr_num = int(curr_num)
                 if operation == '-':
-                    stack.append(-curr_num)
+                    # stack.append(-curr_num)
+                    result += last_num
+                    last_num = - curr_num
+
                 elif operation =='*':
                     # no check required as expression is valid
-                    op1 = stack.pop()
-                    op2 = curr_num
-                    stack.append(op1*op2)
+                    # op1 = stack.pop()
+                    # op2 = curr_num
+                    # stack.append(op1*op2)
+                    last_num = last_num * curr_num
                 elif operation =='/':
-                    op1 = stack.pop()
-                    op2 = curr_num
-                    stack.append(int(op1/op2))
+                    # op1 = stack.pop()
+                    # op2 = curr_num
+                    # stack.append(int(op1/op2))
+                    last_num = int(last_num / curr_num)
                 else:
-                    stack.append(int(curr_num))
+                    # stack.append(int(curr_num))
+                    result += last_num
+                    last_num = curr_num
                 # update the operation
                 # update the curr_number
                 curr_num = ''
                 operation = ch
-            
+        
+        result += last_num
+        # while stack:
+        #     result += stack.pop()
 
-        return sum(stack) 
+        return result
         
 
 

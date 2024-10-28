e29d9512be7d57a027e8663f8aad2844dee1a295
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-28 15:36:33 -0700

    Time: 1658 ms (15.82%) | Memory: 42.7 MB (25.87%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -23,13 +23,13 @@ class Solution:
         m = len(s1)
         n = len(s2)
 
-        dp = [ [-1 for j in range(n+1)]for i in range(m+1)]
+        dp = [ [0 for j in range(n+1)]for i in range(m+1)]
 
-        for i in range(m+1):
-            dp[i][0] = 0
+        # for i in range(m+1):
+        #     dp[i][0] = 0
 
-        for j in range(n+1):
-            dp[0][j] = 0
+        # for j in range(n+1):
+        #     dp[0][j] = 0
 
         
         for i in range(1,m+1):

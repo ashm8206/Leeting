8335bf24f918c5f151da610a10faeb02e30a126a
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-29 14:13:42 -0700

    Time: 815 ms (26.40%) | Memory: 20.2 MB (78.90%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,96 +1,72 @@
 class Solution:
+    def twoSum(self, nums,firstIdx = 1, left=None,right=None,target=None):
 
-    def twoSum(self, start,end,idx,nums):
-        key = -nums[idx]
-        i = start
-        j = end
+        tempRes = []
+        while left < right:
+            
+            if left-1 > firstIdx and nums[left]==nums[left-1]:
+                # print(left, firstIdx, right)
+                left+=1
+                continue
+            if (nums[left] + nums[right]) <= target: 
+                if nums[left] + nums[right] == target:
+                    tempRes.append([-target, nums[left],nums[right]])
 
-        n = len(nums)
-        res = set()
-        while i < j:
-            if (nums[i] + nums[j]) < key :
-                i+=1
-            elif (nums[i] + nums[j]) > key:
-                j-=1
-            else:
-                res.add((nums[idx],nums[i],nums[j]))
-                i+=1
-                j-=1
-                while i < j and nums[i] == nums[i - 1]:
-                    i += 1
-                while j < i and j+1 < n-1 and nums[j] == nums[j + 1]:
-                    j -= 1
-        return res
+                left+=1
+            elif (nums[left] + nums[right]) > target :
+                right-=1
+
+        return tempRes
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res= []
-        nums.sort()
-        # print(nums)
+        res = []
         n = len(nums)
-        for i in range(n):
-            if i > 0 and nums[i-1]==nums[i]:
+        nums.sort()
+        for i in range(0, n-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
-            result = self.twoSum(i+1,n-1,i,nums)
-            # why do we pass i+1,
-            # cuz it is assumed that for i-1, nums 
-            # Triplets that statisfy the condition have already been found.
-
-            if result:
-                res.extend(list(result))
+            temp = self.twoSum(nums, firstIdx = i ,left=i+1, right=n-1, target = -nums[i])
+            # print(temp, i)
+            if temp:
+                res.extend(temp)
         return res
 
+    # def twoSum(self, start,end,idx,nums):
+    #     key = -nums[idx]
+    #     i = start
+    #     j = end
 
-#Method II
-# class Solution:
+    #     n = len(nums)
+    #     res = set()
+    #     while i < j:
+    #         if (nums[i] + nums[j]) < key :
+    #             i+=1
+    #         elif (nums[i] + nums[j]) > key:
+    #             j-=1
+    #         else:
+    #             res.add((nums[idx],nums[i],nums[j]))
+    #             i+=1
+    #             j-=1
+    #             while i < j and nums[i] == nums[i - 1]:
+    #                 i += 1
+    #     return res
 
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+    # def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # res= []
+        # nums.sort()
+        # # print(nums)
+        # n = len(nums)
+        # for i in range(n):
+        #     if i > 0 and nums[i-1]==nums[i]:
+        #         continue
 
-#         if len(nums) < 3:
-#             return []
+        #     result = self.twoSum(i+1,n-1,i,nums)
+        #     # why do we pass i+1,
+        #     # cuz it is assumed that for i-1, nums 
+        #     # Triplets that statisfy the condition have already been found.
 
-#         if len(nums) > 3 and len(set(nums))==1 and nums[0]==0:
-#             return [[0,0,0]]
-        
-#         results = set() # by making this a set, we can remove duplicate valid 3sum seqs presence in O(1)
-#         dups = set() # records args to 2sum for duplicates
-#         results_arr = [] # output format
-#         for k in range(0, len(nums)): # O(n)
-#             if nums[k] not in dups: # without this we fail case of [0,0,...]
-#                 dups.add(nums[k])
-                
-#                 validSums = self.twoSum(nums, k) # get all valid 2sum sequences in O(n)
-#                 for s in validSums:
-#                     r = sorted([s[0], s[1], nums[k]]) # (via Timsort) O(3lg(3)) = O(1) since we always sort 3 elements.
-#                     if tuple(r) not in results: # `in` operation with set() type is fast, with list() type is slow
-#                         results.add(tuple(r))
-#                         results_arr.append(r)
+        #     if result:
+        #         res.extend(list(result))
+        # return res
 
-#         return results_arr
-        
-#     # fetch all 2sums where n_j + n_k == target
-#     # n_i + n_j + n_k = 0 (problem description)
-#     # n_i + n_j = - n_k (apply 2 sum function)
-#     # n_i + n_j = target (within 2 sum)
-#     # n_j = target - n_i (n_j is complement)
-#     def twoSum(self, nums: List[int], target_index: int):
-        
-#         solutions = []
-        
-#         target = - nums[target_index]
-#         complements = defaultdict(list) # complement -> index of complement (may be multiple)
-#         for i in range(len(nums)):
-            
-#             c = target - nums[i]
-
-#             if c in complements: # complements[c] is the index of the complementary value
-#                 for index in complements[c]:
-#                     # print(target,i,index)
-#                     if index!=target_index and i!=target_index and i!=index:
-#                         solutions.append([nums[i], nums[index]]) 
-            
-                
-#             complements[nums[i]].append(i)
-#             # print(complements) # the current number may be a future compliment
-                
-#         return solutions
+#    

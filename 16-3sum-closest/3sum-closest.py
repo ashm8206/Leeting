49df1275b2ec04c64c7b51fe9046cfa02091458b
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-29 14:53:58 -0700

    Time: 315 ms (76.03%) | Memory: 16.5 MB (86.78%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,4 +1,5 @@
 class Solution:
+    
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         
         nums.sort() # --> Nlogn, O(n) with Counting Sort
@@ -6,24 +7,26 @@ class Solution:
         minDist = 10**9
         n = len(nums)
         for i in range(n):
-            # if i > 0 and nums[i-1]==nums[i]:
-            #     continue
+            if i > 0 and nums[i-1]==nums[i]:
+                continue
 
             left = i+1
             right = n-1
             while left < right:
         
                 currSum = nums[i]+ nums[left]+nums[right]
-                if abs(currSum - target) <= minDist:
+                if abs(currSum - target) < minDist:
                     minDist = abs(currSum - target)
                     result = currSum
+                
+
                 if  currSum > target:
                     right-=1
                 else:
+                    
                     left+=1
                 
             
         return result
-        #-4,-1,1,2
-        # -3 <= k  -3-1 == -4
-        #-4+1+2 = -1-1 = -2
+     
+            

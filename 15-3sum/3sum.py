f99295e2625512da64e4f6c2e51a0ee8380b8465
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-29 14:14:22 -0700

    Time: 826 ms (25.60%) | Memory: 20.3 MB (77.51%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -31,42 +31,4 @@ class Solution:
                 res.extend(temp)
         return res
 
-    # def twoSum(self, start,end,idx,nums):
-    #     key = -nums[idx]
-    #     i = start
-    #     j = end
-
-    #     n = len(nums)
-    #     res = set()
-    #     while i < j:
-    #         if (nums[i] + nums[j]) < key :
-    #             i+=1
-    #         elif (nums[i] + nums[j]) > key:
-    #             j-=1
-    #         else:
-    #             res.add((nums[idx],nums[i],nums[j]))
-    #             i+=1
-    #             j-=1
-    #             while i < j and nums[i] == nums[i - 1]:
-    #                 i += 1
-    #     return res
-
-    # def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # res= []
-        # nums.sort()
-        # # print(nums)
-        # n = len(nums)
-        # for i in range(n):
-        #     if i > 0 and nums[i-1]==nums[i]:
-        #         continue
-
-        #     result = self.twoSum(i+1,n-1,i,nums)
-        #     # why do we pass i+1,
-        #     # cuz it is assumed that for i-1, nums 
-        #     # Triplets that statisfy the condition have already been found.
-
-        #     if result:
-        #         res.extend(list(result))
-        # return res
-
-#    
+# TestCase: [0,0,0,0]

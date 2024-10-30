8799100d0bc8be7699a31e990ccc892e9df364d0
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-30 16:53:18 -0700

    Time: 99 ms (21.78%) | Memory: 20.8 MB (10.18%) - LeetSync

--- 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -4,12 +4,13 @@ class Solution:
         cnt_one = max_one = 0
         left = right = 0
         n = len(data)
+        # Fixed Window
         for right in range(n):
             # updating the number of 1's by adding the new element
             cnt_one += data[right]
             # right += 1
             # maintain the length of the window to ones
-            while right - left + 1 > ones:
+            if right - left + 1 > ones:
                 # updating the number of 1's by removing the oldest element
                 cnt_one -= data[left]
                 left += 1

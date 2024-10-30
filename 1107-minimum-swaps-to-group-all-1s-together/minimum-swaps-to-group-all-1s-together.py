20c471b8a6b1dbe6fbbcb084654b66651ea8a752
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-30 16:42:06 -0700

    Time: 83 ms (41.09%) | Memory: 20.9 MB (10.18%) - LeetSync

---
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minSwaps(self, data: List[int]) -> int:
+        ones = sum(data)
+        cnt_one = max_one = 0
+        left = right = 0
+        n = len(data)
+        for right in range(n):
+            # updating the number of 1's by adding the new element
+            cnt_one += data[right]
+            # right += 1
+            # maintain the length of the window to ones
+            if right - left + 1 > ones:
+                # updating the number of 1's by removing the oldest element
+                cnt_one -= data[left]
+                left += 1
+            # record the maximum number of 1's in the window
+            max_one = max(max_one, cnt_one)
+        return ones - max_one

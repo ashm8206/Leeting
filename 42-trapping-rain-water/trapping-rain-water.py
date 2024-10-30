5ec308e7d10ecb2cd5fc06ef032d4f1fdc466d21
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-29 17:21:50 -0700

    Time: 29 ms (27.25%) | Memory: 18.5 MB (32.34%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -27,51 +27,52 @@ class Solution:
         
         # DP Pre calculate maxHL_so_Far, maxHR_so_Far  O(N), O(N)
 
-        # maxLeft = [0]*n
-        # maxRight = [0]*n  
+        maxLeft = [0]*n
+        maxRight = [0]*n  
 
+        maxLeft[0] = height[0]
+        maxRight[n-1] = height[n-1]
+        
         # maxLVal = 0 
         # maxRVal = 0 
 
-        # for i in range(n):
-        #     maxLVal = max(maxLVal,height[i])
-        #     maxLeft[i] = maxLVal
+        for i in range(1,n):
+            # maxLVal = max(maxLVal,height[i])
+            # maxLeft[i] = maxLVal
+            maxLeft[i] = max(maxLeft[i-1], height[i])
             
-        # for j in range(n-1, -1,-1):
-        #     maxRVal = max(maxRVal,height[j])
-        #     maxRight[j] = maxRVal
-        
-        # for i in range(n):
+        for j in range(n-2, -1,-1):
+            # maxRVal = max(maxRVal,height[j])
+            # maxRight[j] = maxRVal
+            maxRight[j] = max(maxRight[j+1], height[j])
+
+        for i in range(n):
             
-        #     ans+= min(maxLeft[i], maxRight[i]) - height[i]
-        #     print(min(maxLeft[i], maxRight[i]), height[i])
+            ans+= min(maxLeft[i], maxRight[i]) - height[i]
+            # print(min(maxLeft[i], maxRight[i]), height[i])
         
-        # return ans
+        return ans
         
         # Two Ptr O(N), O(1) 
 
-        left = 0
-        right = n - 1     
+        # left = 0
+        # right = n - 1     
 
-        maxLeft = height[left]
-        maxRight = height[right]
+        # maxLeft = height[left]
+        # maxRight = height[right]
 
-        while left < right:
-            if maxLeft < maxRight:
-                left+=1 # cant store at edges, so increment first
+        # while left < right:
+        #     if maxLeft < maxRight:
+        #         left+=1 # cant store at edges, so increment first
 
-                maxLeft = max(height[left],maxLeft) 
-                ans+= maxLeft - height[left]
-    
-                
-                
-
-            else:
-                right-=1
-                maxRight = max(height[right],maxRight) 
-                ans+= maxRight - height[right]
+        #         maxLeft = max(height[left],maxLeft) 
+        #         ans+= maxLeft - height[left]
+        #     else:
+        #         right-=1
+        #         maxRight = max(height[right],maxRight) 
+        #         ans+= maxRight - height[right]
              
-        return ans
+        # return ans
 
 
 

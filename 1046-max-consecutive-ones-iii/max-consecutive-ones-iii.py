7f92b2cacb5d34725f0fe42c3d05949222d7b6c0
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-30 16:03:22 -0700

    Time: 67 ms (53.10%) | Memory: 17.4 MB (18.21%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -7,16 +7,23 @@ class Solution:
 
         for R in range(len(nums)):
 
-            if nums[R] == 1:
+            if nums[R] == 0:
                 maxCountOnes += 1
-            
-            if  R-L+1 - maxCountOnes > k:
-                if nums[L] == 1:
+
+            while maxCountOnes > k:
+                if nums[L]==0:
                     maxCountOnes-=1
-                L+= 1
-            
+                L+=1
             maxLen = max(maxLen, R-L+1)
+            
+            # if  R-L+1 - maxCountOnes > k:
+            #     if nums[L] == 1:
+            #         maxCountOnes-=1
+            #     L+= 1
+            
+            # maxLen = max(maxLen, R-L+1)
         return maxLen
 
-        # if winLen - freq[1] > k
-            #  shrink window
+    #    1,1,1,0  k = 1
+    #    1,1,1,0,0 5-3 = 2 > 1, eject
+    #       1, 1, 0, 0

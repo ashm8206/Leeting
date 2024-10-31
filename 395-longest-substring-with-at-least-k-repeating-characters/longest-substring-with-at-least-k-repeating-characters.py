335adaf1f22b3b5f760fd9e138334fd0e4eda3a1
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-31 13:06:57 -0700

    Time: 1537 ms (12.46%) | Memory: 56 MB (10.60%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,43 +1,39 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
 
-        # brute Force
-
-        # #  hmap[s[R]] >= K
-
-        # def atleastK(freq, k):
-
-        #     for i in range(26):
-        #         if freq[i]!=0 and freq[i] < k:
-        #             return False
-        #     return True
-
-        
+        # Method I - Brute force N^3
 
         # n = len(s)
+        # l = 0
+        # # hmap = defaultdict(int)
         # maxLen = 0
+        # for l in range(n):
+        #     hmap = defaultdict(int)
+        #     for r in range(l, n):
+        #         hmap[s[r]]+=1
 
-        # for L in range(n):
-
-        #     freq = [0]*26
-        #     # set Zero freq for every substring
-            
-        #     for R in range(L, n):
-        #         freq[ord(s[R]) - ord('a')]+=1
-
-        #         if atleastK(freq, k):
-        #             maxLen = max(maxLen, R-L+1)
+        #         if r-l+1 >= k*len(hmap):
+        #             # check freq of each candidate
+        #             all_greater = all([ val >= k for key, val in hmap.items()])
+        #             if all_greater:
+        #                 maxLen = max(maxLen, r-l+1)
         # return maxLen
 
+    #  Method II - Divide and conquer
 
-        # Brute Force candidate
+        if len(s) < k:
+            return 0
 
-        # for chars found in string
-        # split on char that have freq[char] < k 
-        for char, count in Counter(s).items():
-            if count < k: 
-                return max(self.longestSubstring(split_str, k) for split_str in s.split(char))
+        counter = defaultdict(int)
+
+        for char in s:
+            counter[char]+=1
+        
+        for i, char in enumerate(s):
+            if counter[char] < k:
+                left = self.longestSubstring(s[:i], k)
+                right = self.longestSubstring(s[i+1:], k)
+                return max(left, right)
+                
         return len(s)
-
-
 

317af94bda252ec41d9161a3bc112546961229d2
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-31 14:32:28 -0700

    Time: 19 ms (97.62%) | Memory: 28.3 MB (47.73%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -6,12 +6,14 @@ class Solution:
         res = set()
 
         maps = set()
+
         for start in range(n-L+1):
+
             cand_str = s[start:start+L]
-            # print(cand_str)
+          
 
             if cand_str in maps and cand_str not in res:
                 res.add(cand_str)
 
             maps.add(cand_str)
-        return res
+        return list(res)

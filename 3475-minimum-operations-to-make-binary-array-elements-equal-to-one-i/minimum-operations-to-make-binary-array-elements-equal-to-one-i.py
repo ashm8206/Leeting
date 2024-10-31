e3a32a3fd33e66c49ef8bd26fd71f58c53f1f006
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-30 17:25:19 -0700

    Time: 182 ms (12.73%) | Memory: 20.6 MB (32.65%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,6 +1,8 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        
+        """
+        The point is that flipping a position more than once does not make any sense as for even number of flips it is equivalent to not flipping at all and for odd number of flips it is the same as flipping just once, so we simply try to flip every zero element just once.
+        """
         k = 3
         cnt = 0
         n = len(nums)

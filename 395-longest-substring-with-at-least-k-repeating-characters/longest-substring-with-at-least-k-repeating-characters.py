971d92a4dac4b53bea84704bcd4dfa350f25c627
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-31 13:29:57 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (74.79%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -21,19 +21,40 @@ class Solution:
 
     #  Method II - Divide and conquer
 
-        if len(s) < k:
-            return 0
+        # if len(s) < k:
+        #     return 0
 
-        counter = defaultdict(int)
+        # counter = defaultdict(int)
 
-        for char in s:
-            counter[char]+=1
+        # for char in s:
+        #     counter[char]+=1
         
-        for i, char in enumerate(s):
-            if counter[char] < k:
-                left = self.longestSubstring(s[:i], k)
-                right = self.longestSubstring(s[i+1:], k)
-                return max(left, right)
-                
-        return len(s)
+        # for i, char in enumerate(s):
+        #     if counter[char] < k:
+        #         left = self.longestSubstring(s[:i], k)
+        #         right = self.longestSubstring(s[i+1:], k)
+        #         return max(left, right)
 
+        # return len(s)
+
+        # Method III  - Another helper
+
+        def helper(st, e, k):
+            if e - st < k:
+                return 0
+            counter = defaultdict(int)
+
+            for i in range(st, e):
+                counter[s[i]] +=1
+
+            for mid in range(st, e):
+                # [st..mid)  and [midNext+1..e) --> e in len(s)
+                if counter[s[mid]] < k:
+                    midNext = mid+1
+                
+                    while midNext < e and counter[s[midNext]] < k:
+                        midNext+=1
+                        # Keep skiping all invalid char, they are of no use to us
+                    return max(helper(st,mid, k), helper(midNext, e, k))
+            return e - st # no need to add +1 as end  is already +1 due to len      
+        return helper(0, len(s), k)

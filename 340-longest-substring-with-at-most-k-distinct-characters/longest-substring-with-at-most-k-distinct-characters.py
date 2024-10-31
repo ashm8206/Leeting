3d8f9aa0a63bc5cd90847e20cbd6a71d75c5e0cf
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-31 13:31:00 -0700

    Time: 34 ms (37.20%) | Memory: 16.8 MB (15.07%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
@@ -3,12 +3,23 @@ class Solution:
 
         L = 0
         maxLen = 0
+        hmap = defaultdict(int)
 
         for R in range(len(s)):
 
-            if len(set(s[L:R+1])) > k:
-                # print(s[L:R+1])
-                L = L+1
+            # if len(set(s[L:R+1])) > k:
+            #     # print(s[L:R+1])
+            #     L = L+1
+
+            # alternative  O(n) to create set every time
+
+            hmap[s[R]] +=1
+
+            while len(hmap) > k:
+                hmap[s[L]]-=1
+                if hmap[s[L]]==0:
+                    del hmap[s[L]]
+                L = L + 1
             
             maxLen = max(maxLen, R-L+1)
 

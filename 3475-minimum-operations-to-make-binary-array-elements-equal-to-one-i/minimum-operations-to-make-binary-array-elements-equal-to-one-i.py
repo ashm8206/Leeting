b5aec69c97c72cd63eb7415e17d87bf630f76491
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-30 17:27:01 -0700

    Time: 171 ms (14.54%) | Memory: 20.8 MB (23.47%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -8,7 +8,7 @@ class Solution:
         n = len(nums)
         for i in range(n-k+1):
             if nums[i] == 0 :
-                for j in range(3):
+                for j in range(k):
                     nums[i+j] = nums[i+j]^1
             
                 cnt+=1

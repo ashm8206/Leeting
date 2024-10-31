980b8ba3dfedffd1463c94c7ce0b56cd5e87b86f
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-31 14:43:07 -0700

    Time: 15 ms (86.58%) | Memory: 16.6 MB (72.11%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -11,17 +11,16 @@ class Solution:
         for ch in s1:
             s1_count[ord(ch) - ord('a')] += 1
 
+       
+        #  for s2 to contain permutation of s1
+        #  we check s1 len chunks of s2, where the freq[chunk] == freq[s1]
         
         for i in range(len_s2):
 
             s2_count[ord(s2[i])-ord('a')]+=1
 
-            if i >= len_s1:
+            if i >= len_s1: # i >= k, then remove  i-k from window
                 s2_count[ord(s2[i-len_s1])-ord('a')]-=1
-
-                # len(p) len window starts from i(end) - len_p + 1
-                #  2, 3, 4, (winLen 3)
-                #  1 is not included (index - len_p) to eject
 
 
             if s1_count == s2_count:

183f79c9f96526379ca7beeb4b0160fae184ccef
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-10-31 18:41:55 -0700

    Time: 144 ms (86.94%) | Memory: 30.6 MB (87.10%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -22,8 +22,20 @@ class Solution:
         res = []
         for i in range(n):
 
-            if dq and dq[0] == i-k:
+            if dq and dq[0] == i-k: 
+                # we know for window ending at "i"
+                # i-k is OOB
+                # i-k+1 or R-L+1 is idx of the first element
                 dq.popleft()
+
+           
+            # if i >= k and dq:
+                # dq.popleft()
+            # Each iteration pop although logical, doesnt work 
+            # As we pop the queue anytime we see a larger element
+            #  This random pops, mess up the window logic.
+
+                
 
             # Monotonically decreasing queue
             while dq and nums[i] > nums[dq[-1]]:

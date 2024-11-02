75516f23018ea20c42f847e4ee501b8629e143e5
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-11-02 15:14:03 -0700

    Time: 167 ms (17.35%) | Memory: 62.2 MB (23.91%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -2,16 +2,29 @@ class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         
         n = len(nums)
-        hmap = {0:-1}
+        hmap = {0:-1} 
+        # it does make sense to initialize this.
+        # Why ?
+         # Since we are morphing this problem int subarray k where k= 0
+        # we will have to store the key here
+        # [(0,1), (0,1, 1, 0)]
+        # longest subarray will be rooted at from start
+
+        # For shortest, It would be a different story, 
+        # we wouldnt want to initialize this value.
+    
         curr_sum = 0
         res = 0
-        maxLen = -10**9
+        maxLen = 0 
+
+        # currSum[j] - previous[i] = k
+        # currSum[j] - k  = previous[i] , then we found a subarray
 
         for i in range(n):
             curr_sum += nums[i]
 
-            if curr_sum == k:
-                maxLen  = max(maxLen, i+1)
+            # if curr_sum == k:
+            #     maxLen  = max(maxLen, i+1)
                 # maxLen = i+1
 
             if curr_sum - k in hmap.keys():
@@ -19,11 +32,13 @@ class Solution:
             
             if curr_sum not in hmap.keys():
                 # seeing for the first time
-                hmap[curr_sum] = i
+                hmap[curr_sum] = i  # Don't place it in every iteration
 
-            # print(hmap)
-        return maxLen if maxLen > -10**9 else 0
-            # -2, -3, 1, 2
+            # print(curr_sum, curr_sum-k, maxLen)
+    
+        return maxLen
+        # 1,-4, 1 = 
+        
         
 
 

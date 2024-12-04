359277c5f4aea9e0312888a5d6d29be3bcd876f0
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-03 16:30:31 -0800

    Time: 4 ms (94.50%) | Memory: 17.5 MB (6.02%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -4,16 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        def numBits(x):
-            count = 0
-            while x != 0:
-                x = x & (x - 1) # zeroing out the least significant set bit
-                #kernighans
-                count += 1
-            return count
+        # def numBits(x):
+        #     count = 0
+        #     while x != 0:
+        #         x = x & (x - 1) # zeroing out the least significant set bit
+        #         #kernighans
+        #         count += 1
+        #     return count
             
-        ans = [0] * (n + 1)
-        for x in range(n + 1):
-            ans[x] = numBits(x)
+        # ans = [0] * (n + 1)
+        # for x in range(n + 1):
+        #     ans[x] = numBits(x)
     
-        return ans                                
+        # return ans                                
+
+ 
+        ans = [0] * (n + 1)
+        for x in range(1, n + 1):
+            ans[x] = ans[x & (x - 1)] + 1
+        return ans 

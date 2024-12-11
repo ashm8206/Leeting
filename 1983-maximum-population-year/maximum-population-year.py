b520e8fc125f77e81c5481d8c5aac100f20c4f23
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-10 20:46:55 -0800

    Time: 0 ms (100.00%) | Memory: 17.2 MB (29.34%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -12,19 +12,28 @@ class Solution:
             d_idx = d - 1950
             prefixSum[b_idx]+=1
             prefixSum[d_idx]-=1
-        prefixSum = list(accumulate(prefixSum))
+        
+        # prefixSum = list(accumulate(prefixSum))
+        # print(prefixSum)
+        # year = 0
+        # maxPopulation = 0
+        # for i, val in enumerate(prefixSum):
+        #     if val > maxPopulation:
+        #         maxPopulation = val
+        #         year = i+1950
+        # return year
 
-        print(prefixSum)
+        # You can do this in One Go
+        # No need to pre-accumulate for this case
+
         year = 0
         maxPopulation = 0
-        for i, val in enumerate(prefixSum):
-            if val > maxPopulation:
-                maxPopulation = val
-                year = i+1950
-        return year
-    
-        
-        
-        return 1
 
+        for i in range(101):
+            if i > 0:
+                prefixSum[i]+=prefixSum[i-1]
+            if prefixSum[i] > maxPopulation:
+                maxPopulation = prefixSum[i]
+                year = 1950+i
+        return year
 

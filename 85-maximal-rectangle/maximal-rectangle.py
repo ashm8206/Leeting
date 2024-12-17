05917922634dc6b55800a4c6908f7e5f9fd32d79
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-16 19:25:08 -0800

    Time: 41 ms (54.17%) | Memory: 23.7 MB (11.46%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -10,37 +10,38 @@ class Solution:
 
         n, m = len(matrix), len(matrix[0])
         
-        max_area = -2**31
+        max_area = 0
 
         def largest_rec_hist(heights):
-            # monotonic stack
+            #  Monotonotonically increasing stack
+            maxAreaLocal = 0
             stack = []
-            nonlocal max_area
-            for curr_idx, h in enumerate(heights):
-                start_idx = curr_idx
-                while stack and stack[-1][1] > h:
-                    old_idx, old_height = stack.pop()
-                    max_area = max(max_area, old_height * (curr_idx - old_idx)) 
-                    start_idx = old_idx # till wherever the previous could be extended
-                stack.append((start_idx, h))
-            if stack:
-                for curr_idx, h in stack:
-                     max_area = max(max_area, h * (m - curr_idx))
-            return max_area
 
+            for i, h in enumerate(heights+[0]):
+                
+                while stack and heights[stack[-1]] >= h:
+                    H = heights[stack.pop()]
+                    # all elements inbetween
+                    W = i if not stack else i - stack[-1] - 1
 
+                    maxAreaLocal = max(maxAreaLocal, H*W)
+                stack.append(i)
 
-        dp = [0] * m
-        
+            return maxAreaLocal
+
+        # this is Dp that builds on the previous
+        dp = [0] * m 
+
         for r in range(n):
+            # for every row
             for c in range(m):
                 
                 # re-initialize to 0 if the matrix is 0, dont count
                 dp[c] = dp[c] + 1 if matrix[r][c]=='1' else 0
 
-            
+            # print(dp)
             # Now Columns have heights, Call largest histogram func
-            # print(dp) 
+        
             max_area = max(max_area, largest_rec_hist(dp))
         return max_area
   

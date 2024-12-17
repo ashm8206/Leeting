468fe112dd5da5abedaf5d17ccff82d1772d2032
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-16 19:09:25 -0800

    Time: 146 ms (76.34%) | Memory: 31 MB (46.81%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,40 +1,45 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
 
-        max_area = -2**31
+        max_area = 0 
         n = len(heights)
-        # stack = []
+        stack = []
+
+        # Build a monotonically increasing stack
+        # (Y) ht increases as X (width) increases will maximize the area.
+
+        # if we are building a monotonically increasing array
+        # we need a NextSmaller 
+
+        """
+        """
+        # for the last 
+        for i, h in enumerate(heights+[0]):
+
+            while stack and heights[stack[-1]] > h:
+                H = heights[stack.pop()]
+                W = i if not stack else i - stack[-1]-1
+                # width calculation
+                # right bound where next smaller is found 
+                # - left bound --> (previous smaller) 
+                # - 1 (don't imclude either)
+                max_area = max(max_area, H*W)
+            stack.append(i)
+        return max_area 
+
+        #  1. we add h[0] to flush out the stack
+        # Without that [0] to flush out the stack, we would need to basically repeat what we do in the inner while loop once at the very end of the program in case there are bars left in the stack,
+
+        # 2. if stackEmpty W = i
+        # we saw "i" larger bars before this one in the stack
+        # Width = i
+
+
+        # Remember width is caluclated from NextSmaller 
+        # pt without account for it
+
+
+
+
+
         
-
-        # for curr_idx, h in enumerate(heights):
-            
-        #     start_idx = curr_idx
-
-        #     while stack and stack[-1][1] > h:
-        #         old_idx, old_height = stack.pop()
-        #         max_area = max(max_area, old_height * (curr_idx - old_idx))
-        #         start_idx = old_idx 
-                
-        #         # where the previous one ended becomes start indx of the smaller building/historgram
-        #     stack.append((start_idx,h))
-        
-        # if stack: # we extend it ahead.
-        #     for  curr_idx, h in stack:
-        #         max_area = max(max_area, h * (n - curr_idx))
-        # return max_area
-
-        stack = [(-1,0)]
-
-        for right_idx , h in enumerate(heights):
-
-            while stack[-1][0]!=-1 and stack[-1][1] > h:
-                _ , curr_height = stack.pop()
-                curr_width = right_idx - stack[-1][0] - 1
-                max_area = max(max_area, curr_height*curr_width)
-            stack.append((right_idx, h))
-        
-        while stack[-1][0]!=-1:
-            _ , curr_height = stack.pop()
-            curr_width = n - stack[-1][0] - 1
-            max_area = max(max_area, curr_height*curr_width)
-        return max_area

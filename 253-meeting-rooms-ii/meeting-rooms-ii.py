9772ba795a525d177a33e1adf7ff2112f1cb10f3
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-24 12:17:10 -0800

    Time: 3 ms (85.53%) | Memory: 19.9 MB (22.27%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -2,22 +2,22 @@ import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 
-        events = SortedList()
+        # events = SortedList()
     
-        # Add start and end events
-        for start, end in intervals:
-            events.add([start, 1])
-            events.add([end, -1])
+        # # Add start and end events
+        # for start, end in intervals:
+        #     events.add([start, 1])
+        #     events.add([end, -1])
     
-        rooms = 0
-        max_rooms = 0
+        # rooms = 0
+        # max_rooms = 0
         
-        # Process events in chronological order
-        for _, event_type in events:
-            rooms += event_type
-            max_rooms = max(max_rooms, rooms)
+        # # Process events in chronological order
+        # for _, event_type in events:
+        #     rooms += event_type
+        #     max_rooms = max(max_rooms, rooms)
         
-        return max_rooms
+        # return max_rooms
 
 
         # events = SortedDict()
@@ -37,23 +37,24 @@ class Solution:
         # return maxRoomCount
 
         # Method II
-        # free_rooms = []
-        # intervals.sort(key = lambda x: x[0])
+        free_rooms = []
+        intervals.sort(key = lambda x: x[0])
 
-        # heapq.heappush(free_rooms, intervals[0][1]) # ending time
+        heapq.heappush(free_rooms, intervals[0][1]) # ending time of first interval
         
-        # n = len(intervals)
+        n = len(intervals)
 
-        # for i in range(1,n):
-        #     s2, e2 = intervals[i]
-        #     if free_rooms and s2 >= free_rooms[0]: 
-        #             heapq.heappop(free_rooms)
-        #             heapq.heappush(free_rooms, e2)
-        #     else:
-        #         # Empty Heap or  Meeting has start time  < end time of First
-        #         # Just push
-        #         heapq.heappush(free_rooms, e2)
-        # return len(free_rooms)
+        for i in range(1,n):
+            s2, e2 = intervals[i]
+            if free_rooms and s2 >= free_rooms[0]: 
+                    # start time >= smallest endTime
+
+                    heapq.heappop(free_rooms)
+                    heapq.heappush(free_rooms, e2)
+            else:
+                # Empty Heap or  Meeting has s2 < e1: Overlap: Start Room
+                heapq.heappush(free_rooms, e2)
+        return len(free_rooms)
 
         
         

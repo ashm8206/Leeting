bfb57b0dbf41917079582620b9d2d6a146bfacc4
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-23 22:04:31 -0800

    Time: 1435 ms (19.87%) | Memory: 18.8 MB (6.27%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -0,0 +1,37 @@
+class MyCalendarTwo:
+    def __init__(self):
+        # self.events = {}
+        self.events = SortedDict()
+    
+    def book(self, start: int, end: int) -> bool:
+        """
+        Book an event from start time to end time.
+        
+        Args:
+            start (int): Start time of the event
+            end (int): End time of the event
+            
+        Returns:
+            bool: True if the event can be booked, False if there's a conflict
+        """
+        # Increment count at start time, decrement at end time
+        self.events[start] = self.events.get(start, 0) + 1
+        self.events[end] = self.events.get(end, 0) - 1
+        
+        # Check for conflicts by scanning through all times
+        count = 0
+        for value in self.events.values():
+            count += value
+            if count > 2:
+                # Revert the changes if there's a conflict
+                self.events[start] -= 1
+                self.events[end] += 1
+                return False
+                
+        return True
+        
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(startTime,endTime)

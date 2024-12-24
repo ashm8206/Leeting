691bcba1662cabf0bd73f081da5d6b31dced38ba
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-24 14:43:51 -0800

    Time: 27 ms (8.51%) | Memory: 21.8 MB (9.48%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,65 +1,78 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
 
-            # i=0
-            # end=1
-            # start=0
-            # merged=[] # monotonic Array
-            
-            # # find position of new interval, skip/append to new list all intervals
-            # # whose end time < new_interval_start time
-            # while i < len(intervals) and intervals[i][start] < newInterval[start]:
-            #     merged.append(intervals[i])
-            #     i+=1
+            # Line Sweep:
+            count = 0
+            events = SortedList()
+            res = []
 
-            # # merge / append new interval
+            for s, e in intervals + [newInterval]:
+                events.add((s,1))
+                events.add((e,-1))
 
-            # if merged and merged[-1][end] >= newInterval[start]:
-            #     newInterval[start] = min(merged[-1][start],newInterval[start])
-            #     newInterval[end] = max(merged[-1][end],newInterval[end])
-            #     merged.pop()
-            # merged.append(newInterval)
+            n = len(events)
+            start = -1
 
-            # # add all the remaining intervals to the output
-            # while i < len(intervals):
-
-            #     if merged and merged[-1][end] >= intervals[i][start]:
-            #         intervals[i][start] = min(merged[-1][start],intervals[i][end])
-            #         intervals[i][end] = max(merged[-1][end],intervals[i][end])
-            #         merged.pop()
+            print(events)
+            for i in range(n):
+                if count == 0:
+                    start = events[i][0]
+                count += events[i][1]
+                if count == 0:
+                    if res and res[-1][1]== start:
+                        res[-1] = [min(res[-1][0], start), max(res[-1][1], events[i][0])]
+                    else:
+                        res.append([start, events[i][0]])
+            return res
                 
-            #     merged.append(intervals[i])
+
+            # Linear Scan
+
+            # n = len(intervals)
+            # i = 0
+            # res = []
+
+            # # Case 1: No overlapping before merging intervals
+            # while i < n and intervals[i][1] < newInterval[0]:
+            #     res.append(intervals[i])
             #     i += 1
-            # #print(i)
-            # return merged
 
-            if not intervals:
-                return [newInterval]
-            
-            target = newInterval[0]
-            left = 0
-            right = len(intervals)
+            # # Case 2: Overlapping and merging intervals
+            # while i < n and newInterval[1] >= intervals[i][0]:
+            #     newInterval[0] = min(newInterval[0], intervals[i][0])
+            #     newInterval[1] = max(newInterval[1], intervals[i][1])
+            #     i += 1
+            # res.append(newInterval)
 
-            insert_idx = bisect.bisect(intervals,[target])
+            # # Case 3: No overlapping after merging newInterval
+            # while i < n:
+            #     res.append(intervals[i])
+            #     i += 1
+
+            # return res
+
+            # BS
+            # if not intervals:
+            #     return [newInterval]
             
-            intervals.insert(insert_idx, newInterval)
-            # Inserting this might need but one previous to be merged
-            # Following the insert to be merged
-            # print(intervals)
-            merged = []
-            for interval in intervals:
-                if merged and interval[0] <= merged[-1][1]:
-                    start = merged[-1][0] # we did BS so the start remains same
-                    new_end = max(merged[-1][1], interval[1])
-                    merged.pop()
-                    merged.append([start,new_end])
-                else:
-                    merged.append(interval)
+            # target = newInterval[0]
+            # left = 0
+            # right = len(intervals)
+
+            # insert_idx = bisect.bisect(intervals,[target])
+            
+            # intervals.insert(insert_idx, newInterval)
+            # # Inserting this might need but one previous to be merged
+            # # Following the insert to be merged
+            # # print(intervals)
+            # merged = []
+            # for interval in intervals:
+            #     if merged and interval[0] <= merged[-1][1]:
+            #         start = merged[-1][0] # we did BS so the start remains same
+            #         new_end = max(merged[-1][1], interval[1])
+            #         merged.pop()
+            #         merged.append([start,new_end])
+            #     else:
+            #         merged.append(interval)
                 
-            return merged
-
-        
-
-
-            return []
-
+            # return merged

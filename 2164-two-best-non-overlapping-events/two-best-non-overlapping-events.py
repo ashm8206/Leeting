4384efb79d42310b085fb747cb0caf45f5cc44e8
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-24 17:28:01 -0800

    Time: 343 ms (38.05%) | Memory: 70.7 MB (17.40%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -2,38 +2,63 @@ import heapq
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         
-        events.sort(key = lambda x: x[0])
-        maxProfit = 0
-        maxVal = 0 
-        # track the most optimal Value Sofar
+      
+        # maxProfit = 0
+        # maxVal = 0 
+        # # track the most optimal Value Sofar
 
-        # even if previous event is Popped out, if it gave the maxVal,
-        # that will remain constant, until a greater maxVal shows up
+        # # even if previous event is Popped out, if it gave the maxVal,
+        # # that will remain constant, until a greater maxVal shows up
 
-        # inwhich case it will be counted towards maxVal + currEvent'sProfit
+        # # inwhich case it will be counted towards maxVal + currEvent'sProfit
 
-        # if the previous pair, non-overlapping was indeed so high !
-        # maxProfit will be unchanged for (newMaxVal+newCurrentEventProfit)
+        # # if the previous pair, non-overlapping was indeed so high !
+        # # maxProfit will be unchanged for (newMaxVal+newCurrentEventProfit)
 
-        pq = []
+        # pq = []
 
-        for start, end, profit in events:
-            # Among all Overlapping options available
-            # pop all non-overlapping intervals with curr
-            # and extend curr with Most profitable Non-overlapping interval
+        # for start, end, profit in events:
+        #     # Among all Overlapping options available
+        #     # pop all non-overlapping intervals with curr
+        #     # and keep track of the MaxVal Non-overlapping interval
 
-            while pq and start > pq[0][0]: # start >= end Non-overlap condi
-                _, val = heappop(pq)
-                maxVal = max(val, maxVal)
+        #     while pq and start > pq[0][0]: # start >= end Non-overlap condi
+        #         _, val = heappop(pq)
+        #         maxVal = max(val, maxVal)
             
-            # At most 2 Overlapping, 
-            # we just want to find a Pair or Single MaxVal
+        #     # At most 2 Overlapping, 
+        #     # we just want to find a Pair or Single MaxVal
 
-            maxProfit = max(maxProfit, maxVal+profit)
+        #     maxProfit = max(maxProfit, maxVal+profit)
 
-            heappush(pq,(end, profit))
+        #     heappush(pq,(end, profit))
 
-            # loop invariant: After each iteration heap will have all overlapping interval with their profits
+        #     # loop invariant: After each iteration heap will have all overlapping interval with their profits
 
             
-        return maxProfit
+        # return maxProfit
+
+        times = []
+        for e in events:
+            # 1 denotes start time.
+            times.append([e[0], 1, e[2]])
+            # 0 denotes end time.
+            times.append([e[1] + 1, -1, e[2]])
+
+        times.sort()
+
+        ans = 0
+        max_value = 0
+
+        for t, cmd, profit in times:
+            if cmd > 0:
+                ans = max(ans, profit+ max_value)
+            else:
+                max_value = max(max_value, profit)
+        return ans
+
+
+
+
+   
+        

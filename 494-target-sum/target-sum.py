011b63f2bee570a6faf0c514c7ee2489b11e79aa
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-25 23:45:19 -0800

    Time: 125 ms (74.41%) | Memory: 18.1 MB (61.85%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -2,31 +2,49 @@
 class Solution:
     
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-
-        count = 0
         n = len(nums)
+        # dp = [ 0 for _ in range(sum(nums)) for i in range(n+1)]
+        # since we will get negative sums, and cant have -ve indices
+        # replace inner array with default(int)
 
-        memo = {}
+        dp = [defaultdict(int) for i in range(n+1)]
+        dp[0][0] = 1 # 1 way of getting 0 
 
-        def helper(running_sum, idx):
-            nonlocal count
+        for i in range(n):
+            for k,v in dp[i].items():
+                curr_sum = k
+                countWays = v
+                # you are carrying forward the 1 way from dp[0][0]
+                dp[i+1][curr_sum-nums[i]]+=countWays 
+                dp[i+1][curr_sum+nums[i]]+=countWays 
+                # numberways of reaching curr_sum ± nums[i]
+                # add +1 way way or the previous count
+        return dp[n][target]
+
+        # count = 0
+        # n = len(nums)
+
+        # memo = {}
+
+        # def helper(running_sum, idx):
+        #     nonlocal count
             
-            if idx == n:
-                return 1 if running_sum==target else 0
+        #     if idx == n:
+        #         return 1 if running_sum==target else 0
 
-            if (running_sum, idx) in memo:
-                return memo[(running_sum, idx)]
+        #     if (running_sum, idx) in memo:
+        #         return memo[(running_sum, idx)]
             
-            memo[(running_sum, idx)] = (
-                helper(running_sum + nums[idx], idx+1) 
-                + helper(running_sum - nums[idx] , idx+1)
-            )
-            return memo[(running_sum, idx)]
+        #     memo[(running_sum, idx)] = (
+        #         helper(running_sum + nums[idx], idx+1) 
+        #         + helper(running_sum - nums[idx] , idx+1)
+        #     )
+        #     return memo[(running_sum, idx)]
 
-        # for loop is not not for tradition combinations
-        # we only want Combindations of len(N) start from idx 0... end N
-        # Each idx has two option, take / don't take
-        return helper(0, 0)
+        # # for loop is not not for tradition combinations
+        # # we only want Combindations of len(N) start from idx 0... end N
+        # # Each idx has two option, take / don't take
+        # return helper(0, 0)
         
 
         

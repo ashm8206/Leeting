e74a71126e9bb9ffc54b2e883450726f61a665d7
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-30 11:25:19 -0800

    Time: 15 ms (56.49%) | Memory: 22.7 MB (24.03%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -7,30 +7,30 @@ class Solution:
     
         def dfs(curr_num):
             
-            if curr_num > n:
+            if curr_num > n:  
+                # for singal digit numbers, execeeding limit
+                #  limit is 2, but Outer loop is [1.....10]
                 return 
-                
+
             result.append(curr_num)
 
             for i in range(10):
                 next_num = curr_num*10 + i
+
                 if next_num <= n:
                     dfs(next_num)
                 else:
-                    break
+                    return 
 
-        for i in range(1, 10):
+        for i in range(1, 10): 
+            # this optimization
             dfs(i)
         return result
 
-        # result = []
-
-        # def dfs(i):
-
+       
         
 
-        # for i in range(1, 10):
-        #     dfs(i)
+       
 
         
         

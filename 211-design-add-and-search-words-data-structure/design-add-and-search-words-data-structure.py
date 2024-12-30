e40dbcb0d46f4bb336c06fb5053f4e1cdf8a2fd2
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-30 15:12:41 -0800

    Time: 1281 ms (51.14%) | Memory: 65.6 MB (30.11%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,49 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.is_end = False
+
 class WordDictionary:
 
     def __init__(self):
-        self.root = {}
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         node = self.root
-        for char in word:
-            if char not in node:
-                node[char] = {}
-            node = node[char]
-        node["$"] = True
-
-    def search_node(self, word: str, node : dict) -> bool:
-
-        for i, char in enumerate(word):
-            
-            if char not in node:
-                if char == ".":
-                    for key in node.keys():
-                        """
-                        For every key at this level, excluding "$" ending char
-                        as it is not a valid char in input, but a stub created 
-                        by us.
-
-                        for every key, pass their mapping node[key] and 
-                        the word[i+1: ], as it is the next char after "."
-
-                        Start a new search(word, node[key]) for all key
-                        mappings at this level, if any of them match return True
-
-                        if None of they match, return True
-
-                        """
-                        if key!='$' and self.search_node(word[i+1:],node[key]):
-                            return True
-                return False
-
-            else:
-                node = node[char]
-           
-        return "$" in node
-
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_end = True
 
     def search(self, word: str) -> bool:
-            return self.search_node(word, self.root)
+        return self.searchWord(word, self.root)
+    
+    def searchWord(self, word, root):
+        node = root
+        for i, c in enumerate(word):
+            if c not in node.children: 
+                if c == ".":
+                    for c in node.children:
+                        if self.searchWord(word[i+1:], node.children[c]):
+                            return True
+                return False # any other character, or match not found
+            else:
+                node = node.children[c]
+        return node.is_end==True
         
 
 

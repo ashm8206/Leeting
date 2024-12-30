b0c372f9a8b465d4bac28fcc0b3ca795c949a796
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-30 10:30:41 -0800

    Time: 185 ms (13.08%) | Memory: 43.7 MB (6.91%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,17 +1,67 @@
+# class Solution:
+#     def removeSubfolders(self, folder: List[str]) -> List[str]:
+#         folder.sort()
+#         res = [folder[0]]
+#         n = len(folder)
+
+#         for i in range(1, n):
+#             last_folder = res[-1]
+#             last_folder+= '/'
+
+#             if not folder[i].startswith(last_folder):
+#                 res.append(folder[i])
+            
+#         return res
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder.sort()
-        res = [folder[0]]
-        n = len(folder)
 
-        for i in range(1, n):
-            last_folder = res[-1]
-            last_folder+= '/'
+    class TrieNode:
+        def __init__(self):
+            self.is_end_of_folder = False
+            self.children = {}
 
-            if not folder[i].startswith(last_folder):
-                res.append(folder[i])
-            
-        return res
+    def __init__(self):
+        self.root = self.TrieNode()
 
-            
+    def removeSubfolders(self, folder):
+        # Build Trie from folder paths
+        for path in folder:
+            current_node = self.root
+            folders = path.split("/")
+
+            for folder_name in folders:
+                if folder_name == "":
+                    continue
+
+                # Create new node if it doesn't exist
+                if folder_name not in current_node.children:
+                    current_node.children[folder_name] = self.TrieNode()
+                current_node = current_node.children[folder_name]
+
+            # Mark the end of the folder path
+            current_node.is_end_of_folder = True
+
+        # Check each path for subfolders
+        result = []
+        for path in folder:
+            current_node = self.root
+            folders = path.split("/")
+            is_subfolder = False
+
+            for i, folder_name in enumerate(folders):
+                if folder_name == "":
+                    continue
+                next_node = current_node.children[folder_name]
+                # Check if the current folder path is a subfolder of an existing folder
+                # print(path, next_node.is_end_of_folder, folder_name,i)
+                if next_node.is_end_of_folder and i != len(folders) - 1:
+                    is_subfolder = True
+                    break  # Found a subfolder
+                current_node = next_node
+
+            # If not a subfolder, add to the result
+            if not is_subfolder:
+                result.append(path)
+
+        return result
+
             

56581651efc05b5b24317124935afac15672ec5a
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-30 11:44:06 -0800

    Time: 9 ms (81.18%) | Memory: 21.2 MB (58.58%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -3,30 +3,46 @@ class Solution:
 
     def lexicalOrder(self, n: int) -> List[int]:
         
-        result = []
+        # result = []
     
-        def dfs(curr_num):
-            
-            if curr_num > n:  
-                # for singal digit numbers, execeeding limit
-                #  limit is 2, but Outer loop is [1.....10]
-                return 
+        # def dfs(curr_num):
+        #     if curr_num > n:  
+        #         # for singal digit numbers, execeeding limit
+        #         #  limit is 2, but Outer loop is [1.....10]
+        #         return 
 
-            result.append(curr_num)
+        #     result.append(curr_num)
 
-            for i in range(10):
-                next_num = curr_num*10 + i
+        #     for i in range(10):
+        #         next_num = curr_num*10 + i
 
-                if next_num <= n:
-                    dfs(next_num)
-                else:
-                    return 
+        #         if next_num <= n:
+        #             dfs(next_num)
+        #         else:
+        #             return 
 
-        for i in range(1, 10): 
-            # this optimization
-            dfs(i)
-        return result
+        # for i in range(1, 10): 
+        #     # this optimization
+        #     dfs(i)
+        # return result
 
+
+        # Iterative Neetcode
+        
+        res = []
+        curr = 1
+
+        while len(res) < n:
+            res.append(curr)
+
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+
+                while curr == n or curr%10==9:
+                    curr //= 10
+                curr+=1
+        return res
        
         
 

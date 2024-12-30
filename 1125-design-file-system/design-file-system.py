ba8fdd6098d2070028831981ccf3549a42df2ace
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-30 13:42:16 -0800

    Time: 47 ms (54.58%) | Memory: 25.5 MB (7.89%) - LeetSync

---
+++ 1125-design-file-system/design-file-system.py
@@ -0,0 +1,45 @@
+class TrieNode:
+        def __init__(self, value = 0):
+            self.value = value
+            self.children = {}
+        
+class FileSystem:
+    
+    def __init__(self):
+        self.root = TrieNode()
+
+    def createPath(self, path: str, value: int) -> bool:
+
+        node = self.root
+        folders = path.split("/")[1:]
+
+        for i, f in enumerate(folders):
+        
+            if f not in node.children:
+                if i < len(folders) - 1:
+                    return False
+                else:
+                    # last Value
+                    node.children[f] = TrieNode(value)
+                    return True
+            else:
+                node = node.children[f]
+                # keep  going down
+        return False
+    
+    def get(self, path: str) -> int:
+        folders = path.split("/")
+        node = self.root
+        for i, f in enumerate(folders[1:]):
+            if f in node.children:
+                node = node.children[f]
+            else:
+                return -1
+        return node.value
+        
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)

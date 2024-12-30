0488bda5b91b4fa569247e204fe87c5e7b4823f2
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-30 13:48:33 -0800

    Time: 59 ms (31.34%) | Memory: 25.6 MB (7.89%) - LeetSync

--- 1125-design-file-system/design-file-system.py
+++ 1125-design-file-system/design-file-system.py
@@ -11,9 +11,12 @@ class FileSystem:
     def createPath(self, path: str, value: int) -> bool:
 
         node = self.root
-        folders = path.split("/")[1:]
+        folders = path.split("/")
 
         for i, f in enumerate(folders):
+
+            if f == "":
+                continue
         
             if f not in node.children:
                 if i < len(folders) - 1:
@@ -25,12 +28,15 @@ class FileSystem:
             else:
                 node = node.children[f]
                 # keep  going down
-        return False
+        return False  # if it already exist # return False
     
     def get(self, path: str) -> int:
         folders = path.split("/")
         node = self.root
-        for i, f in enumerate(folders[1:]):
+        for i, f in enumerate(folders):
+            if f == "":
+                continue
+
             if f in node.children:
                 node = node.children[f]
             else:

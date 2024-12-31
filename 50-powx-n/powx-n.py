844e0a60e0c976d66cb728445413133c57e71e65
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-31 13:50:12 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (8.57%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -5,27 +5,23 @@ class Solution:
             n = abs(n) 
 
         ans = 1
-        nn = n
-        # def helper(n) -> float:
-        #     if n == 0:
-        #         return 1
-
-        #     half = helper(n//2)
-
-        #     if n%2:
-        #        return half * half * x
-        #     else:
-        #         return half * half
-
-        # return helper(n)
-
-
         
-        # 2**4
-        #   n = 4, x = 2 ==> x = 4 (x = x * x)
-        #   n = 2  x = 4 ==> x = 16 (x = x * x)
+        def helper(n) -> float:
+            if n == 0:
+                return 1
 
-        
+            half = helper(n//2)
+
+            if n%2:
+               return half * half * x
+            else:
+                return half * half
+
+        return helper(n)
+
+
+
+        # nn = n
         while nn:
 
             if nn%2:
@@ -35,4 +31,7 @@ class Solution:
                 x = x * x
                 nn = nn // 2
         return ans
+
+        
+
       

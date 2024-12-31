6bbef19cb9c5384ecfe0a41eef80b2c9eb3715b9
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-31 13:44:20 -0800

    Time: 24 ms (19.77%) | Memory: 25.4 MB (11.34%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        
+        # allFBT(7)
+        # # 0, 6. 1, 5 2, 4 3, 3 4, 2 5, 1, 6,0
+        # what happens for 2 ?  or 4 or 6 ? -> []
+
+        # res = []
+
+        def helper(n):
+            # base case:
+            if n%2==0:
+                return []
+            if n==1:
+                return [TreeNode(0)]
+
+            res = []
+
+            for l in range(1, n, 2):
+                r = n - 1 - l
+                leftTrees, rightTrees = helper(l), helper(r)
+                
+                
+                for left in leftTrees:
+                    for right in rightTrees:
+                        root = TreeNode(0, left=left, right=right)
+                        res.append(root)
+            return res
+
+        return helper(n)

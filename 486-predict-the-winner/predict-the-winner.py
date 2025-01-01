e7b6f47983388037a1b719b27c14d83ced093fdf
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-31 18:51:50 -0800

    Time: 3 ms (66.64%) | Memory: 18.3 MB (5.09%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,28 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        dp = {}
+        def helper(left,right, turn):
+            if left > right:
+                return 0
+            
+            if (left,right, turn) in dp:
+                return dp[(left,right, turn)]
+
+            res = 0
+            if turn ==1:
+                res = max(nums[left]+ helper(left+1,right,2), nums[right]+ helper(left,right-1,2))
+            else:
+                # turn 2
+                # we want to try and minimize turn 1 score
+                # so mininum is returned
+
+                res = min(helper(left+1,right,1), helper(left,right-1,1))
+            dp[(left,right, turn)] = res
+            return res
+
+        n = len(nums)
+        p1_score = helper(0,n-1, 1)
+        p2_score = sum(nums) - p1_score
+        return p2_score <= p1_score
+          

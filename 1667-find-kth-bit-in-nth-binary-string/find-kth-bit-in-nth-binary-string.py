2d868cb256d09c011a785b0e42fd96f9584286e6
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-31 16:06:38 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (50.46%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,19 +1,35 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
 
-        def revInv(bin_string):
-            new_string = ""
-            for bit in bin_string:
-                if bit=="1":
-                    new_string += "0"
-                else:
-                    new_string += "1"
-            return new_string[::-1]
+        def helper(n, k):
+            if n==1:
+                return "0"
+            
+            length = 2**n - 1
+            half = length // 2 
+            if k <= half:
+                return helper(n-1, k)
+            elif k > half + 1:
+                res = helper(n-1, 1 + length -  k)
+                return "0" if res== "1" else  "1"
+            else:
+                return "1" # at half + 1, center
+
+        return helper(n, k)
+
+        # def revInv(bin_string):
+        #     new_string = ""
+        #     for bit in bin_string:
+        #         if bit=="1":
+        #             new_string += "0"
+        #         else:
+        #             new_string += "1"
+        #     return new_string[::-1]
         
-        def helper(i, prev):
+        # def helper(i, prev):
 
-            if i==n:
-                return prev[k-1]
-            return helper(i+1, prev + "1" + revInv(prev))
+        #     if i==n:
+        #         return prev[k-1]
+        #     return helper(i+1, prev + "1" + revInv(prev))
 
-        return helper(1, "0")
+        # return helper(1, "0")

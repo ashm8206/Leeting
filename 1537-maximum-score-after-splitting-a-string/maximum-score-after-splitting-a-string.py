75635f8f14ac0d7463f76011b5d9aa863a4078c1
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-31 16:50:07 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (9.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,12 +1,31 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         
-        maxSum = 0
+        # maxSum = 0
 
+        # n = len(s)
+        # for i in range(n-1):
+        #     left = s[:i+1].count("0")
+        #     right = s[i+1:].count("1")
+        #     maxSum = max(left+right, maxSum)
+        # return maxSum
+
+        # optimized
+        ones = s.count("1")
+        zeros = 0
+        ans = 0 
         n = len(s)
+
         for i in range(n-1):
-            left = s[:i+1].count("0")
-            right = s[i+1:].count("1")
-            print(left,right)
-            maxSum = max(left+right, maxSum)
-        return maxSum
+            if s[i] == "1":
+                ones -= 1
+                # this s[i] was part of right
+                # but now adding to left, we lose it
+            else:
+                # this s[i] was part of left
+                # and we add the zeros as we gain
+                zeros += 1
+        
+            ans = max(ans, zeros + ones)
+        
+        return ans

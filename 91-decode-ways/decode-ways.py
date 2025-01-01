31983f3caf94d46d4bca3a667b4031e42ec8d0c1
Aishwarya Mali <aishwaryamali94@gmail.com>
2024-12-31 20:00:55 -0800

    Time: 2 ms (34.70%) | Memory: 17.9 MB (11.49%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -2,65 +2,61 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         # Recursion
         # @lru_cache(maxsize=None)
-        # def helper(s, index):
-
-
-        #     if index == len(s):
-                
-        #         return 1
-
-        #     if s[index] == "0":
-        #         # catches last element!=0 
-        #         # and other element, if they are leading zeros
-        #         return 0
-
-
-        #     if index == len(s)-1:
-        #         # when idx+2
-
-        #         # Len(S) -2 +1 = Len(S)-1
-        #         # Stopping at above idx, ensures Window-Size - 1 element ahead of it remains to make the complete window 
-
-        #         # we only come here if the last element is !=0 
-
-        #         return 1
-            
-
-        #     ans = helper(s, index+1)
-        #     if int(s[index: index+2])<=26:
-        #         ans+= helper(s, index+2)
-            
-        #     return ans
-        # return helper(s, 0)
-
-        # DP Tabluation
 
         n = len(s)
-        dp = [0 for _ in range(n+1)]
-       
-       
-        # if s[0]!="0"
+        dp = {}
+        def helper(index):
+            if index in dp:
+                return dp[index]
+            if index == n:
+                return 1
+                # if we reach n from index+1
+
+            if s[index]=="0":
+                # check any element for leading zeros
+                return 0
+            
+            if index == n-1:
+                # you reached here from two digit element
+                return 1
+            
+            ans = helper(index+1) # all single number counted except 0
+            if index + 1 < n and (s[index]=="1" or s[index] == "2" and s[index+1] in "0123456"):
+                ans+= helper(index+2)
+            dp[index] = ans
+            return ans
+
+        return helper(0)
+
         
-        # Ways to decode empty string = 1 
-        dp[0] = 1
+        # DP Tabluation
 
-        # Ways to decode a string of size 1 is 1. Unless the string is '0'.
-        # '0' doesn't have a single digit decode.
+        # n = len(s)
+        # dp = [0 for _ in range(n+1)]
+       
+       
+        # # if s[0]!="0"
+        
+        # # Ways to decode empty string = 1 
+        # dp[0] = 1
 
-        dp[1] = 0 if s[0] == "0" else 1
+        # # Ways to decode a string of size 1 is 1. Unless the string is '0'.
+        # # '0' doesn't have a single digit decode.
 
-        for i in range(2, n+1):
+        # dp[1] = 0 if s[0] == "0" else 1
+
+        # for i in range(2, n+1):
             
-            if s[i-1]!="0":
-                dp[i] += dp[i-1] 
+        #     if s[i-1]!="0":
+        #         dp[i] += dp[i-1] 
             
-            two_digit = int(s[i - 2 : i]) 
-            if two_digit >= 10 and two_digit<=26:
+        #     two_digit = int(s[i - 2 : i]) 
+        #     if two_digit >= 10 and two_digit<=26:
                 
-                    dp[i]+= dp[i - 2]
+        #             dp[i]+= dp[i - 2]
                 
-        # print(dp)
-        return dp[n]
+        # # print(dp)
+        # return dp[n]
         
 
         

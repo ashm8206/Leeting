c4c5b5661039143ddc6cf0eac2dc64b0d32f43e3
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-02 19:05:19 -0800

    Time: 43 ms (95.74%) | Memory: 17.8 MB (67.71%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -6,14 +6,16 @@ class Solution:
     #   * sold[number_of_transactions] is max(sold[n_of_t], bought[n_of_t] + price)
     #   * sold[i-1] - price --> bought[i] because it's a new transaction
 
-        bought = [-inf] * (k + 1)
-        sold = [0] * (k + 1)
-        maxProfitVal = 0
+        if not k:
+            return k
+        t_cost = [float('inf')] * k
+        t_profit = [0] * k
 
         for price in prices:
-            for i in range(1, k + 1):
-                bought[i] = max(bought[i], sold[i - 1] - price)
-                sold[i] = max(sold[i], bought[i] + price)
-                maxProfitVal = max(maxProfitVal, sold[i])
-
-        return maxProfitVal
+            t_cost[0] = min(t_cost[0], price)
+            t_profit[0] = max(t_profit[0], price - t_cost[0])
+            for i in range(1, k):
+                t_cost[i] = min(t_cost[i], price - t_profit[i - 1])
+                t_profit[i] = max(t_profit[i], price - t_cost[i])
+    
+        return t_profit[-1]

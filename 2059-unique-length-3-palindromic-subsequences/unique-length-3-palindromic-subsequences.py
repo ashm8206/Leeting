49b9b0db49539eee67cfbb38e60596b04e063a1c
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-03 20:56:48 -0800

    Time: 2330 ms (26.37%) | Memory: 18.3 MB (27.25%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -2,30 +2,42 @@ class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         
 
-        # n = len(s)
-        # res = 0
-        # seen_pairs = set()
-        # for i in range(n):
-        #     for j in range(i+1,n):
-        #         for k in range(j+1,n):
-        #             if s[k]==s[i] and s[i]+s[j] +s[k] not in seen_pairs:
+        first_index = [-1]*26
+        last_index = [-1]*26
+        n = len(s)
+        for i in range(n):
+            if first_index[ord(s[i]) - ord("a")]==-1:
+                first_index[ord(s[i]) - ord("a")] = i
 
-        #                 seen_pairs.add(s[i]+s[j] +s[k])
-        #                 res+=1
-        # return res
+            if last_index[ord(s[n-i-1]) - ord("a")]==-1:
+                last_index[ord(s[n-i-1]) - ord("a")] = n-i-1
 
-
-        letters = set(s)
-        ans = 0
         
-        for letter in letters:
-            i, j = s.index(letter), s.rindex(letter) # O(n)
-            between = set()
-            
-            for k in range(i + 1, j): #O(n)
-                between.add(s[k])
-            
-            ans += len(between)
+        unique_3_len = 0
+        seen = set()
+        for j, ch in enumerate(s):
+            for i in range(26):
+                l_r_char = chr(i + ord("a"))
+                
+                if (l_r_char,ch) not in seen:
+                    if 0 <= first_index[i]  < j < last_index[i]:
+                        # print(l_r_char,ch,l_r_char)
+                        unique_3_len+=1
+                        seen.add((l_r_char,ch))
+        return unique_3_len
 
-        return ans # O(n**2) but can be seprated in 2 Loops.
+
+        # letters = set(s)
+        # ans = 0
+        
+        # for letter in letters:
+        #     i, j = s.index(letter), s.rindex(letter) # O(n)
+        #     between = set()
+            
+        #     for k in range(i + 1, j): #O(n)
+        #         between.add(s[k])
+            
+        #     ans += len(between)
+
+        # return ans # O(n**2) but can be seprated in 2 Loops.
 

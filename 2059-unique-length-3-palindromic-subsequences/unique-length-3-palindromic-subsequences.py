0a0f872b9e8d3e8384aeaa29fe1ba1945009eaa9
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-03 21:13:14 -0800

    Time: 193 ms (76.26%) | Memory: 18.2 MB (32.97%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -11,20 +11,29 @@ class Solution:
 
             if last_index[ord(s[n-i-1]) - ord("a")]==-1:
                 last_index[ord(s[n-i-1]) - ord("a")] = n-i-1
-
-        
-        unique_3_len = 0
-        seen = set()
-        for j, ch in enumerate(s):
-            for i in range(26):
-                l_r_char = chr(i + ord("a"))
+        res = 0
+        for i in range(26):
+            
+            if first_index[i]!= last_index[i]:
+                # they are distinct occurence, not just 1 occurrence
+                res += len(set(s[first_index[i]+1:last_index[i]]))
+                # count how many distinct chars between these two indices?
+        return res
+            
+        # treat J as the Middle character
+        # iterate every char in 26 chars to find first_idx[char] < charMid < last_idx[char]
+        # unique_3_len = 0
+        # seen = set()
+        # for j, ch in enumerate(s):
+        #     for i in range(26):
+        #         l_r_char = chr(i + ord("a"))
                 
-                if (l_r_char,ch) not in seen:
-                    if 0 <= first_index[i]  < j < last_index[i]:
-                        # print(l_r_char,ch,l_r_char)
-                        unique_3_len+=1
-                        seen.add((l_r_char,ch))
-        return unique_3_len
+        #         if (l_r_char,ch) not in seen:
+        #             if 0 <= first_index[i]  < j < last_index[i]:
+        #                 # print(l_r_char,ch,l_r_char)
+        #                 unique_3_len+=1
+        #                 seen.add((l_r_char,ch))
+        # return unique_3_len
 
 
         # letters = set(s)

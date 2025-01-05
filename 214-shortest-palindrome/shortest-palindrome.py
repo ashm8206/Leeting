c5a9109fecb0a57ee34fcc0543471946cd82becf
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-05 15:21:13 -0800

    Time: 99 ms (35.60%) | Memory: 18.4 MB (47.55%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,26 +1,34 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         length = len(s)
-        if length == 0:
-            return s
+        reversed_string = s[::-1]  # Reverse the string
 
-        # Find the longest palindromic prefix
-        left = 0
-        for right in range(length - 1, -1, -1):
-            if s[right] == s[left]:
-                left += 1
+        # Iterate through the string to find the longest palindromic prefix
+        for i in range(length):
+            if s[: length - i] == reversed_string[i:]:
+                return reversed_string[:i] + s
+        return ""
+        # length = len(s)
+        # if length == 0:
+        #     return s
 
-        # If the whole string is a palindrome, return the original string
-        if left == length:
-            return s
+        # # Find the longest palindromic prefix
+        # left = 0
+        # for right in range(length - 1, -1, -1):
+        #     if s[right] == s[left]:
+        #         left += 1
 
-        # Extract the suffix that is not part of the palindromic prefix
-        non_palindrome_suffix = s[left:]
-        reverse_suffix = non_palindrome_suffix[::-1]
+        # # If the whole string is a palindrome, return the original string
+        # if left == length:
+        #     return s
 
-        # Form the shortest palindrome by prepending the reversed suffix
-        return (
-            reverse_suffix
-            + self.shortestPalindrome(s[:left])
-            + non_palindrome_suffix
-        )
+        # # Extract the suffix that is not part of the palindromic prefix
+        # non_palindrome_suffix = s[left:]
+        # reverse_suffix = non_palindrome_suffix[::-1]
+
+        # # Form the shortest palindrome by prepending the reversed suffix
+        # return (
+        #     reverse_suffix
+        #     + self.shortestPalindrome(s[:left])
+        #     + non_palindrome_suffix
+        # )

1523eb863222cd14b4895c853853fcf089f63254
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-05 22:37:59 -0800

    Time: 262 ms (73.94%) | Memory: 18 MB (22.38%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,24 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        r = s[::-1]
+
         n = len(s)
-        # Tabulation
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
-        
-        max_length = 0
-        max_end = 0
-        
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                # Storing longest common substring in dp Array
-                if s[i - 1] == r[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
-                # Code to check i and j
-                if dp[i][j] > max_length:
-                    # print(i, dp[i][j], n, j)
-                    if i - dp[i][j] == n - j:
-                    
-                        max_length = dp[i][j]
-                        max_end = i
-                        # print(max_length, max_end)
-        return s[max_end - max_length:max_end]
+        ans = s[0]
+        maxLen = 1
+
+        # odd length palindromes
+        # chose a middle and expand in both direction
+        for i in range(n):
+            l, r = i, i
+            while l >=0 and r < n and s[l] == s[r]:
+                if r-l+1 > maxLen:
+                    maxLen = r-l+1
+                    ans = s[l:r+1]
+
+                l-=1
+                r+=1
+            
+            # even length palindrome
+            # start with length 2 and expand in both directions
+
+            l, r = i, i+1 # 2 len
+            while l >=0 and r < n and s[l] == s[r]:
+                if r-l+1 > maxLen:
+                    maxLen = r-l+1
+                    ans = s[l:r+1]
+                l-=1
+                r+=1
+        return ans

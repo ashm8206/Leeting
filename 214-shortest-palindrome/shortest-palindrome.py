419ec1b814ab1d66ecc4290f5f8b81c9b50596c1
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-06 13:51:53 -0800

    Time: 63 ms (43.04%) | Memory: 18.2 MB (57.03%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -9,30 +9,7 @@ class Solution:
         # return reversed(S[R+1:N]) + X
         #  dc abacd
 
-        # if not s:
-        #     return ""
-            
-        # def isPalindrome(s, end):
-        #     # Check if s[0:end+1] is a palindrome
-        #     i, j = 0, end
-        #     while i < j:
-        #         if s[i] != s[j]:
-        #             return False
-        #         i += 1
-        #         j -= 1
-        #     return True
-        
-        # # Find the longest palindromic prefix
-        # n = len(s)
-        # longest_pal = 0
-        
-        # # Try each position as potential end of palindromic prefix
-        # for i in range(n-1, -1, -1):
-        #     if isPalindrome(s, i):
-        #         longest_pal = i
-        #         break
-        
-        # return s[longest_pal + 1:][::-1] + s
+       
 
         if not s:
             return ""
@@ -47,7 +24,7 @@ class Solution:
             if s.startswith(rev[i:]):
                 return rev[:i] + s
         
-        return rev + s[1:]
+        return ""
 
 
       

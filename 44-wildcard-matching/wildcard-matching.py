9f8901060dc11ea3c0e13e4a6dc457762ba701eb
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-08 22:52:57 -0800

    Time: 435 ms (29.80%) | Memory: 125.7 MB (17.49%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,17 +1,26 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # https://leetcode.com/problems/wildcard-matching/solutions/1336621/python-dfs-with-memoization-clean-concise/
-        @lru_cache()
+        dp  = {}
         def dfs(i, j):
+
             if j == len(p):
                 return i == len(s)
             
+            if (i,j) in dp:
+                return dp[(i,j)]
+            
             if i < len(s) and (s[i]==p[j] or p[j]=="?"):
-                return dfs(i+1,j+1)
+                dp[(i,j)] = dfs(i+1,j+1)
+                return dp[(i,j)]
             
             if p[j]=="*":
-                return dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
+               
+                dp[(i,j)]  =  dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
+                return  dp[(i,j)]
 
-            return False
+            # if it  doesnt match return False
+            dp[(i,j)]= False
+            return  dp[(i,j)]
     
         return dfs(0,0)

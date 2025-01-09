d4b8fa4ff298f455a72d3acb28499ea4130a340b
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-08 23:04:40 -0800

    Time: 455 ms (24.75%) | Memory: 125.3 MB (18.37%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,7 +1,9 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # https://leetcode.com/problems/wildcard-matching/solutions/1336621/python-dfs-with-memoization-clean-concise/
-        @lru_cache()
+        # @lru_cache()
+
+        dp = {}
         def dfs(i, j):
             if j == len(p):
                 return i == len(s)
@@ -11,18 +13,23 @@ class Solution:
                 # then are the rest "*"
                 return all(x == '*' for x in p[j:])
             
+            if (i,j) in dp:
+                return dp[(i,j)]
+            
             if (s[i]==p[j] or p[j]=="?"):
-                return dfs(i+1,j+1)
+                dp[(i,j)] = dfs(i+1,j+1)
+                return dp[(i,j)]
 
             if p[j]=="*":
-                return dfs(i, j+1) or dfs(i+1, j)
+                dp[(i,j)] = dfs(i, j+1) or dfs(i+1, j)
+                return dp[(i,j)]
             
             # if i < len(s) and (s[i]==p[j] or p[j]=="?"):
             #     return dfs(i+1,j+1)
             
             # if p[j]=="*":
             #     return dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
-
-            return False
+            dp[(i,j)] = False
+            return dp[(i,j)]
     
         return dfs(0,0)

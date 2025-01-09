603267e58f0b8ad6203440291d12760bb4d77104
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-08 20:00:09 -0800

    Time: 219 ms (9.06%) | Memory: 32.6 MB (12.56%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -1,21 +1,42 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        m = len(word1)
-        n = len(word2)
+        n = len(word1)
+        m = len(word2)
 
-        memo = [ [0 for j in range(n+1)] for i in range(m+1)]
-        
-        for i in range(m+1):
-            memo[i][0] = i
-        
-        for j in range(n+1):
-            memo[0][j] = j
+        dp = {}
+        def helper(i, j):
+            if i>=n or j >=m:
+                return 0
 
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if word1[i-1]==word2[j-1]:
-                    memo[i][j] = memo[i-1][j-1]
-                else:
-                    memo[i][j] = 1 + min(memo[i-1][j], memo[i][j-1])
-        return memo[m][n]
+            if (i,j) in dp:
+                return dp[(i,j)]
+            
+            if word1[i]==word2[j]:
+                dp[(i,j)] =  1 + helper(i+1, j+1)
+            else:
+                dp[(i,j)] = max(helper(i+1, j), helper(i, j+1))
+
+            return dp[(i,j)]
+        
+  
+       
+        return (m + n - 2*  helper(0, 0)) 
+
+
+
+        # memo = [ [0 for j in range(n+1)] for i in range(m+1)]
+        
+        # for i in range(m+1):
+        #     memo[i][0] = i
+        
+        # for j in range(n+1):
+        #     memo[0][j] = j
+
+        # for i in range(1, m+1):
+        #     for j in range(1, n+1):
+        #         if word1[i-1]==word2[j-1]:
+        #             memo[i][j] = memo[i-1][j-1]
+        #         else:
+        #             memo[i][j] = 1 + min(memo[i-1][j], memo[i][j-1])
+        # return memo[m][n]

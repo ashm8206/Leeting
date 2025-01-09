6d7a96e1dc57a58bab2acb213a7867ba497ee811
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-08 23:02:54 -0800

    Time: 1560 ms (5.02%) | Memory: 19.1 MB (71.17%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,27 +1,28 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # https://leetcode.com/problems/wildcard-matching/solutions/1336621/python-dfs-with-memoization-clean-concise/
-        dp  = {}
+        @lru_cache()
         def dfs(i, j):
-
             if j == len(p):
                 return i == len(s)
-            
-            if (i,j) in dp:
-                return dp[(i,j)]
-            
-            if i < len(s) and (s[i]==p[j] or p[j]=="?"):
-                dp[(i,j)] = dfs(i+1,j+1)
-                return dp[(i,j)]
-            
-            if p[j]=="*":
-               
-                dp[(i,j)]  =  dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
-                return dp[(i,j)]
 
-          
-            # if it  doesnt match return False
-            dp[(i,j)]= False
-            return  dp[(i,j)]
+            if i == len(s): 
+                # end of string to match
+                # then are the rest "*"
+                return all(x == '*' for x in p[j:])
+            
+            if (s[i]==p[j] or p[j]=="?"):
+                return dfs(i+1,j+1)
+
+            if p[j]=="*":
+                return dfs(i, j+1) or dfs(i+1, j)
+            
+            # if i < len(s) and (s[i]==p[j] or p[j]=="?"):
+            #     return dfs(i+1,j+1)
+            
+            # if p[j]=="*":
+            #     return dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
+
+            return False
     
         return dfs(0,0)

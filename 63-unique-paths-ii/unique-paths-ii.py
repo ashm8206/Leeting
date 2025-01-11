46aad9ea6c41527accd5a66adc6a6e1346162510
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-11 14:57:55 -0800

    Time: 1 ms (38.45%) | Memory: 17.9 MB (22.10%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -3,28 +3,25 @@ class Solution:
         
         
         n, m = len(obstacleGrid), len(obstacleGrid[0])
-        dp =  [[0 for _ in range(m)] for _ in range(n)]
+        dp =  [[0 for _ in range(m+1)] for _ in range(n+1)]
 
         
 
-        if obstacleGrid[0][0] == 1:
+        if obstacleGrid[n-1][m-1] == 1 or obstacleGrid[0][0] == 1:
             return 0
         else:
-            dp[0][0] = 1
+            dp[n-1][m-1] = 1
 
-        for row in range(n):
-            for col in range(m):
+        for row in range(n-1, -1, -1):
+            for col in range(m-1, -1, -1):
                 
                 if obstacleGrid[row][col] == 1:
                     dp[row][col] = 0
 
-                if row > 0 and obstacleGrid[row][col] == 0:
-                    dp[row][col]+= dp[row-1][col]
+                if obstacleGrid[row][col] == 0:
+                    dp[row][col]+= (dp[row+1][col] + dp[row][col+1])
+        return dp[0][0]
 
-                if col > 0 and obstacleGrid[row][col] == 0:
-                    dp[row][col]+= dp[row][col-1]
-        
-        return dp[n-1][m-1]
         # @lru_cache / Recursive
         # def helper(r,c):
         #     print(r,c)

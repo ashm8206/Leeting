bdb27e009b8a6a676b52b71349f1245af304282e
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-11 23:07:20 -0800

    Time: 51 ms (5.87%) | Memory: 18 MB (24.62%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -16,29 +16,29 @@ class Solution:
 
         # Alternative - Build parent pointers
         graph = collections.defaultdict(list)
-        def build_graph(cur, parent):
-            if cur and parent:
-                graph[cur.val].append(parent.val)
-                graph[parent.val].append(cur.val)
-            if cur.left:
-                build_graph(cur.left, cur)
-            if cur.right:
-                build_graph(cur.right, cur) 
+        def build_graph(curr, parent):
+            if curr:
+                graph[curr].append(parent)
+                graph[parent].append(curr)
+            if curr.left:
+                build_graph(curr.left, curr)
+            if curr.right:
+                build_graph(curr.right, curr) 
         build_graph(root, None)
 
-        while queue:
-            curr = queue.popleft()
-            if curr.left:
-                queue.append(curr.left)
-                adjSet[curr.val].add(curr.left.val)
-                adjSet[curr.left.val].add(curr.val)
-            if curr.right:
-                queue.append(curr.right)
-                adjSet[curr.val].add(curr.right.val)
-                adjSet[curr.right.val].add(curr.val)
+        # while queue:
+        #     curr = queue.popleft()
+        #     if curr.left:
+        #         queue.append(curr.left)
+        #         adjSet[curr.val].add(curr.left.val)
+        #         adjSet[curr.left.val].add(curr.val)
+        #     if curr.right:
+        #         queue.append(curr.right)
+        #         adjSet[curr.val].add(curr.right.val)
+        #         adjSet[curr.right.val].add(curr.val)
         
         queue = deque()
-        queue.append((target.val,0))
+        queue.append((target,0))
         visited = set()
         while queue:
             curr, level = queue.popleft()
@@ -46,9 +46,11 @@ class Solution:
             visited.add(curr)
 
             if level==k:
-                res.append(curr)
+                if curr:
+                    res.append(curr.val)
                 continue
-            for nei in adjSet[curr]:
+            # for nei in adjSet[curr]:
+            for nei in graph[curr]:
                 if nei not in visited:
                     queue.append((nei, level+1))
         return res

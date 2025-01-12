f50a7aec07dbe1b74fb0faf87c52d65d9360bd57
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-12 12:44:32 -0800

    Time: 174 ms (8.42%) | Memory: 22.5 MB (10.23%) - LeetSync

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -7,21 +7,68 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
         self.ans = None
-        self.dfs(root, p, q)
+
+        def helper(root, p, q): 
+
+            if not root:
+                return None, None
+
+            pseen, qseen = False, False
+
+            if root==p:
+                pseen = True
+            
+            if root == q:
+                qseen = True
+
+            pLeft, qLeft = helper(root.left, p, q)
+            pRight, qRight = helper(root.right, p, q)
+
+            if pLeft or pRight:
+                pseen = True
+            
+            if qLeft or qRight:
+                qseen = True
+
+            # if pLeft or pRight or root == p:
+            #     pseen = True
+            
+            # if qLeft or qRight or root == q:
+            #     qseen = True
+
+
+            if pseen and qseen:
+                if self.ans is None:
+                    self.ans = root
+            
+            return pseen, qseen
+    
+        helper(root, p, q)
+        
         return self.ans
 
-    def dfs(self, node, p, q):
-        if node is None:
-            return False, False
-        
-        pleft, qleft = self.dfs(node.left, p, q)
-        pright, qright = self.dfs(node.right, p, q)
+           
 
-        pseen = pleft or pright or node == p
-        qseen = qleft or qright or node == q
+
+
+
+    #     self.ans = None
+    #     self.dfs(root, p, q)
+    #     return self.ans
+
+    # def dfs(self, node, p, q):
+    #     if node is None:
+    #         return False, False
         
-        if pseen and qseen:
-            if self.ans is None:
-                self.ans = node
-        return pseen, qseen
+    #     pleft, qleft = self.dfs(node.left, p, q)
+    #     pright, qright = self.dfs(node.right, p, q)
+
+    #     pseen = pleft or pright or node == p
+    #     qseen = qleft or qright or node == q
+        
+    #     if pseen and qseen:
+    #         if self.ans is None:
+    #             self.ans = node
+    #     return pseen, qseen

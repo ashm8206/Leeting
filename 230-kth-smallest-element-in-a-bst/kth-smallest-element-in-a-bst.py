88fd01509bed8b2978fd383b0f802fe7ff57f539
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-12 13:20:28 -0800

    Time: 0 ms (100.00%) | Memory: 21.2 MB (12.78%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,55 +6,48 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # queue = deque([root])
-        # heap = []
-
-        # while queue:
-        #     curr = queue.popleft()
-            
-        #     if len(heap) < k:
-        #         heappush(heap, -curr.val)
-        #     elif heap[0] <= -curr.val:
-        #         heappop(heap)
-        #         heappush(heap, -curr.val)
-
-        #     if curr.left:
-        #         queue.append(curr.left)
-
-        #     if curr.right:
-        #         queue.append(curr.right)
-
-        # if heap:
-        #     return heap[0] * -1
-        # return -1
+        
+        self.ans = 0
+        def helper(root):
+            nonlocal k
+            if not root:
+                return None
+            helper(root.left)
+            k-=1
+            if k==0:
+                self.ans = root.val
+            helper(root.right)
+        
+        helper(root)
+        return self.ans
+        
 
         
-        if not root:
-            return -1
+        # if not root:
+        #     return -1
 
-        curr = root
-        stack = [curr]
+        # curr = root
+        # stack = [curr]
 
-        while True:
+        # while True:
 
-            if curr:
-                stack.append(curr)
-                curr = curr.left
+        #     if curr:
+        #         stack.append(curr)
+        #         curr = curr.left
             
-            elif stack:
-                curr = stack.pop()
-                k-=1 
-                if k==0:
-                    return curr.val
+        #     elif stack:
+        #         curr = stack.pop()
+        #         k-=1 
+        #         if k==0:
+        #             return curr.val
                 
             
-                curr = curr.right
-            else:
-                break
+        #         curr = curr.right
+        #     else:
+        #         break
 
 
         
         
 
 
-        # -3 - 2, -1

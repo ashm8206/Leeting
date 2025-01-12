ca862666167b6bfd1712c4b548c0ab4f889b0cf8
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-12 12:42:15 -0800

    Time: 54 ms (46.74%) | Memory: 22.6 MB (5.29%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,15 +7,49 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        # Method I
-        if not root or p == root or q == root:
-            return root
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-
-        if left and right:
-            return root
         
-        return left or right
+        # # Method I
+        # if not root:
+        #     return None
+
+        # if p == root or q == root:
+        #     return root # if we find either first, they are the LCA
+
+        # left = self.lowestCommonAncestor(root.left, p, q)
+        # right = self.lowestCommonAncestor(root.right, p, q)
+
+        # if left and right: # split
+        #     return root
+        
+        # return left or right # one of them comes before case II
+
+
+        self.ans = None
+
+        def helper(root, p, q): 
+
+            if not root:
+                return None, None
+
+            pLeft, qLeft = helper(root.left, p, q)
+            pRight, qRight = helper(root.right, p, q)
+
+            pseen, qseen = False, False
+
+            if pLeft or pRight or root == p:
+                pseen = True
+            
+            if qLeft or qRight or root == q:
+                qseen = True
+
+
+            if pseen and qseen:
+                if self.ans is None:
+                    self.ans = root
+            
+            return pseen, qseen
+    
+        helper(root, p, q)
+        
+        return self.ans

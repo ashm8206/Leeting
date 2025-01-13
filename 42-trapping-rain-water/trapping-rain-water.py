c9a131e23640eb90f84d3e6f5202dc741f779097
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-12 22:21:13 -0800

    Time: 10 ms (76.78%) | Memory: 19.2 MB (45.45%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,7 +1,54 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+
+        n = len(height)
+        l = 0
+        r = n-1
+        ans = 0
+        maxLeft = 0
+        maxRight = 0
+        while l < r:
+            if height[l] < height[r]:
+                maxLeft = max(maxLeft, height[l])
+                ans+= maxLeft - height[l] 
+                l+=1
+            else:
+                maxRight = max(maxRight, height[r])
+                ans+= maxRight- height[r] 
+                r-=1
+        return ans
+
+
+
+
+        n = len(height)
+
         
+        ans = 0
+        left_max, right_max = [0]*n, [0]*n
+
+       
+        for i in range(n):
+            if i > 0:
+                left_max[i] = max(left_max[i-1], height[i])
+            else:
+                left_max[i] = height[i]
         
+        for j in range(n-1, -1, -1):
+            if j+1 < n :
+                right_max[j] = max(right_max[j+1], height[j])
+            else:
+                right_max[j] = height[j]
+        
+        print(left_max, right_max)
+        
+        for i in range(1,n-1):
+            ans+= min(right_max[i], left_max[i]) - height[i]
+        
+        return ans
+
+
+
         # How does the calculation work?:
         # At each point min(maxHL_so_Far,maxHR_so_far) - Height[i]/elevation
         
@@ -11,19 +58,6 @@ class Solution:
         ans = 0
         n = len(height)
 
-        # for i in range(n):
-        #     maxLeft = height[i]
-        #     maxRight = height[i]
-        #     for j in range(i, -1, -1):
-        #         maxLeft = max(maxLeft,height[j])
-            
-        #     for j in range(i, n):
-        #         maxRight = max(maxRight,height[j])
-            
-        #     ans+= min(maxLeft, maxRight) - height[i]
-        
-        # return ans
-    
         
         # DP Pre calculate maxHL_so_Far, maxHR_so_Far  O(N), O(N)
 
@@ -39,13 +73,12 @@ class Solution:
         for j in range(n-2, -1,-1):
        
             maxRight[j] = max(maxRight[j+1], height[j])
-
+            
         for i in range(n):
             
             ans+= min(maxLeft[i], maxRight[i]) - height[i]
-            # print(min(maxLeft[i], maxRight[i]), height[i])
-        
         return ans
+
         
         # Two Ptr O(N), O(1) 
 

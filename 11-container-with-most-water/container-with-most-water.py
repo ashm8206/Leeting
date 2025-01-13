4f493cded92f49f95435a250e2161308264972aa
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-12 22:18:03 -0800

    Time: 105 ms (42.57%) | Memory: 28.6 MB (11.28%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,11 +1,29 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+
+        n = len(height)
+        l = 0
+        r = len(height) - 1
+        maxArea = 0
+
+        while l < r:
+            if height[l] < height[r]:
+                maxArea = max(maxArea, (r-l) * min(height[l],height[r]))
+                l+=1
+         
+            else:
+                maxArea = max(maxArea, (r-l) * min(height[l],height[r]))
+                r-=1
+                
+        return maxArea
+        # [1,8,6,2,5, 4, 8, 3,7]
+    #    7*7
         
         # Brute force
         # Compare Pair wise : O n^2 and O(1)
 
-        n = len(height)
-        max_area = -10**10
+        # n = len(height)
+        # max_area = -10**10
         # for i in range(n):
         #     for j in range(i+1,n):
         #         # area  = width * min(heights[l], heights[r])
@@ -15,18 +33,17 @@ class Solution:
 
         # Greedy
         #  Maximize the width * min (Max L, Max R)
+        #  If we try to move the pointer at the longer line inwards, 
+        #          we won't gain any increase in area, 
+        #          since it is limited by the shorter line.
+        # However, if we move the shorter line, inward
+        #       we might make an increase in area, despite the reduction in width
 
         L = 0
         R = n-1
 
-        maxHeightL = height[L]
-        maxHeightR = height[R]
-
         while L < R:
-            # maxHeightL = max(height[L], maxHeightL)
-            # maxHeightR = max(height[R], maxHeightR)
-
-            # area = (R - L) * min(maxHeightL, maxHeightR)
+        
             area = (R - L) * min(height[L], height[R])
             max_area = max(area,max_area)
 

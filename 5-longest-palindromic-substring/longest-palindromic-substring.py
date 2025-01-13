42a2f64bc1bf8c9a914dbd9dba1a57a3e3852c4d
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-12 20:32:01 -0800

    Time: 259 ms (78.49%) | Memory: 17.9 MB (38.25%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,30 +1,59 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
+        maxLen = 0 
         n = len(s)
-        ans = s[0]
-        maxLen = 1
-
-        # odd length palindromes
-        # chose a middle and expand in both direction
+        
+        longestString = ""
         for i in range(n):
+            # odd Len
             l, r = i, i
-            while l >=0 and r < n and s[l] == s[r]:
-                if r-l+1 > maxLen:
-                    maxLen = r-l+1
-                    ans = s[l:r+1]
-
+            # print(i,s[i])
+            while  r < n  and l >=0 and s[l]==s[r]:
+                if r - l + 1 > maxLen:
+                    maxLen = r - l + 1 
+                    longestString = s[l:r+1]
+                    # print(maxLen, s[l], s[r])
                 l-=1
                 r+=1
             
-            # even length palindrome
-            # start with length 2 and expand in both directions
-
-            l, r = i, i+1 # 2 len
-            while l >=0 and r < n and s[l] == s[r]:
-                if r-l+1 > maxLen:
-                    maxLen = r-l+1
-                    ans = s[l:r+1]
+            # even
+            l, r = i, i+1
+            while  r < n  and l >=0 and  s[l]==s[r]:
+                if r - l + 1 > maxLen:
+                    maxLen = r - l + 1 
+                    longestString = s[l:r+1]
                 l-=1
                 r+=1
-        return ans
+        return longestString
+        
+        
+            #even Len
+
+        # n = len(s)
+        # ans = s[0]
+        # maxLen = 1
+
+        # # odd length palindromes
+        # # chose a middle and expand in both direction
+        # for i in range(n):
+        #     l, r = i, i
+        #     while l >=0 and r < n and s[l] == s[r]:
+        #         if r-l+1 > maxLen:
+        #             maxLen = r-l+1
+        #             ans = s[l:r+1]
+
+        #         l-=1
+        #         r+=1
+            
+        #     # even length palindrome
+        #     # start with length 2 and expand in both directions
+
+        #     l, r = i, i+1 # 2 len
+        #     while l >=0 and r < n and s[l] == s[r]:
+        #         if r-l+1 > maxLen:
+        #             maxLen = r-l+1
+        #             ans = s[l:r+1]
+        #         l-=1
+        #         r+=1
+        # return ans

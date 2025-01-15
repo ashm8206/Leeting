406618f96c652904c287fa2a441f6e789d5007a4
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-15 13:16:48 -0800

    Time: 7 ms (91.34%) | Memory: 20.1 MB (52.10%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -7,46 +7,68 @@
 import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        pq =[]
+        n = len(lists)
+        for i in range(n):
+            if lists[i]:
+                heapq.heappush(pq, (lists[i].val, i))
+                lists[i] = lists[i].next
+        
+        prevHead = ListNode(-1)
+        curr = prevHead
+        while pq:
+            # print(pq) 
+            minVal, idx = heapq.heappop(pq)
+
+            curr.next = ListNode(minVal)
+            curr = curr.next
+            
+            if lists[idx]:
+                heapq.heappush(pq, (lists[idx].val, idx))
+                lists[idx] = lists[idx].next
+
+        return prevHead.next
+
+
         
        
-        pq = []
-        curr = head = ListNode()
-        prev = ListNode(-int(10**5))
+        # pq = []
+        # curr = head = ListNode()
+        # prev = ListNode(-int(10**5))
         
-        k = len(lists)
-        if k < 1:
-            return None
+        # k = len(lists)
+        # if k < 1:
+        #     return None
 
-        for i in range(k):
-            if lists[i]:
-                # value and index of the list
-                pq.append((lists[i].val, i))
-                lists[i] = lists[i].next
+        # for i in range(k):
+        #     if lists[i]:
+        #         # value and index of the list
+        #         pq.append((lists[i].val, i))
+        #         lists[i] = lists[i].next
         
    
     
-        heapq.heapify(pq)
-        while pq:
-            next_num, idx = heapq.heappop(pq)
-            if lists[idx]:
-                heapq.heappush(pq,(lists[idx].val, idx))
-                lists[idx] = lists[idx].next
+        # heapq.heapify(pq)
+        # while pq:
+        #     next_num, idx = heapq.heappop(pq)
+        #     if lists[idx]:
+        #         heapq.heappush(pq,(lists[idx].val, idx))
+        #         lists[idx] = lists[idx].next
             
             
-            curr = ListNode(next_num)
-            prev.next = curr
+        #     curr = ListNode(next_num)
+        #     prev.next = curr
 
-            if prev.val == -10**5 and prev.next:
-                head.next = prev.next
+        #     if prev.val == -10**5 and prev.next:
+        #         head.next = prev.next
             
-            prev = curr
+        #     prev = curr
 
-            
-            
 
-        if head.next is None:
-            return None
-        else:
-            return head.next
+        # if head.next is None:
+        #     return None
+        # else:
+        #     return head.next
             
       

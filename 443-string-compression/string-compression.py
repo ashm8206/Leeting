0c1e1ee9c2b8d20c41b4b770a0dae40e13d2733e
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-14 23:31:25 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (12.76%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,40 +1,59 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        slow = 0
-        n = len(chars)
 
-        if n==1:
-            return 1
-        
+        count = 1
+        left = 0
 
-        stack = []
-
-        fast = 1
-        count = 1 # one char
-
-        while fast < n:
-            if chars[slow]==chars[fast]:
-                fast+=1
-                count+=1
+        for right in range(1,len(chars)+1):
+            if right < len(chars) and chars[right-1] == chars[right]:
+                count += 1
             else:
-                stack.append(chars[slow])
-
-                if 1 < count <= 9 :
-                    stack.append(str(count))
-                elif count > 9:
-                    count = str(count)
-                    while len(count) > 0:
-                        stack.append(count[0]) # on repeat
-                        count = count[1:]
-                # print(slow, fast, stack)
-                slow = fast
-                fast = slow + 1
+                chars[left] = chars[right-1]
+                left += 1
+                if count > 1:
+                    for c in str(count):
+                        chars[left] = c
+                        left += 1
                 count = 1
 
-        stack.append(chars[slow])
-        if count > 1:
-            stack.extend(list(str(count)))
+        return left
+
+
+        # slow = 0
+        # n = len(chars)
+
+        # if n==1:
+        #     return 1
+        
+
+        # stack = []
+
+        # fast = 1
+        # count = 1 # one char
+
+        # while fast < n:
+        #     if chars[slow]==chars[fast]:
+        #         fast+=1
+        #         count+=1
+        #     else:
+        #         stack.append(chars[slow])
+
+        #         if 1 < count <= 9 :
+        #             stack.append(str(count))
+        #         elif count > 9:
+        #             count = str(count)
+        #             while len(count) > 0:
+        #                 stack.append(count[0]) # on repeat
+        #                 count = count[1:]
+        #         # print(slow, fast, stack)
+        #         slow = fast
+        #         fast = slow + 1
+        #         count = 1
+
+        # stack.append(chars[slow])
+        # if count > 1:
+        #     stack.extend(list(str(count)))
 
         
-        chars[:] = stack
-        return len(stack)
+        # chars[:] = stack
+        # return len(stack)

5d750904be871b29c23ed94c75dfb90be4886bf8
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-16 13:48:37 -0800

    Time: 46 ms (63.47%) | Memory: 32.1 MB (62.28%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,36 +1,85 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.is_end = False
+
 
 class Trie:
-
     def __init__(self):
-        self.root = {}
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+             #curr.count+=1 
+        curr.is_end = True
 
-    def insert(self, word: str) -> None:
-        node = self.root
+    
+    def search(self, word):
+        curr = self.root
 
-        for char in word:
-            if char not in node:
-                node[char]= {}
-            node = node[char]
-        node["*"] = 'end'
-        # print(self.root)
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if char in node:
-                node = node[char]
-            else:
+        for ch in word:
+            if ch not in curr.children:
                 return False
-        return "*" in node
+            else:
+                curr = curr.children[ch]
+             #curr.count+=1 
+        return curr.is_end == True
 
+        # check the word is "$" : searchword
+        # search prefix "$" is not end
+        pass 
+    
     def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for char in prefix:
-            if char in node:
-                node = node[char]
-            else:
+        curr = self.root
+
+        for ch in prefix:
+            if ch not in curr.children:
                 return False
-        return True
+            else:
+                curr = curr.children[ch]
+             #curr.count+=1 
+        return True # ends here as well as startswith
+    
+
+
+
+    
+# class Trie:
+
+#     def __init__(self):
+#         self.root = {}
+
+#     def insert(self, word: str) -> None:
+#         node = self.root
+
+#         for char in word:
+#             if char not in node:
+#                 node[char]= {}
+#             node = node[char]
+#         node["*"] = 'end'
+
+
+#     def search(self, word: str) -> bool:
+#         node = self.root
+#         for char in word:
+#             if char in node:
+#                 node = node[char]
+#             else:
+#                 return False
+#         return "*" in node
+
+#     def startsWith(self, prefix: str) -> bool:
+#         node = self.root
+#         for char in prefix:
+#             if char in node:
+#                 node = node[char]
+#             else:
+#                 return False
+#         return True
         
 
 

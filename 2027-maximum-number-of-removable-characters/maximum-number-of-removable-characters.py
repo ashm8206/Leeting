6a1de852c9951250d165891cbf1960627f291096
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-16 22:14:05 -0800

    Time: 2172 ms (5.22%) | Memory: 38.3 MB (17.27%) - LeetSync

---
+++ 2027-maximum-number-of-removable-characters/maximum-number-of-removable-characters.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+
+        remove_dict = {r : i for i, r in enumerate(removable)}
+        def check(last_removable):
+            i,j = 0,0
+            while i < len(s) and j < len(p):
+                if i in remove_dict and remove_dict[i] <= last_removable:
+                    i += 1
+                    continue
+                if s[i] == p[j]:
+                    i += 1
+                    j += 1
+                else:
+                    i += 1
+            if j == len(p):
+                return True
+            return False
+        lo = 0
+        hi = len(removable)
+        while lo < hi:
+            mi = (lo+hi) // 2
+            if check(mi):
+                lo = mi+1
+            else:
+                hi = mi
+        return lo
+                

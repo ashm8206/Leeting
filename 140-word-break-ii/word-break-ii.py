bcb4d0edcd9ed2ab2769d0e12c1d7aca6a48847e
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-16 20:34:41 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (21.71%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -13,68 +13,82 @@ class Trie:
         for char in word:
             if char not in node.children:
                 node.children[char] = TrieNode()
-            # node = node.next[idx]
             node = node.children[char]
         node.isEnd = True
 
 class Solution:
-    def __init__(self):
-        self.trie = Trie()
-        self.dp = []
-        self.n = 0
-
-    def dfs(self, s: str, start: int):
-        # If we have reached the end of the string, return a list containing an empty string
-        if start == self.n:
-            return [""]
-        
-        # If the solution is already computed for this start index, return the stored result
-        if self.dp[start]:
-            return self.dp[start]
-
-        ans = []
-        node = self.trie.root
-        current_prefix = ""
-
-        # Iterate through the string from the current start index
-        for i in range(start, self.n):
-            # idx = ord(s[i]) - ord('a')
-            char = s[i]
-            # If the current character is not in the Trie, break the loop
-            if char not in node.children:
-                break
-
-            node = node.children[char]
-            current_prefix += s[i]
-
-            # If we find a valid word (end of a word in the Trie)
-            if node.isEnd:
-                rest = self.dfs(s, i + 1)
-                for r in rest:
-                    ans.append(current_prefix + (" " if r else "") + r)
-
-        # Store the result for this start index in dp
-        self.dp[start] = ans
-        return ans
-
+    
     def wordBreak(self, s: str, wordDict: list) -> list:
-        self.n = len(s)
+        n = len(s)
+        trie = Trie()
         
         # Insert all words from wordDict into the Trie
         for word in wordDict:
-            self.trie.insert(word)
+            trie.insert(word)
 
-        # Initialize dp for memoization
-        self.dp = [[] for _ in range(self.n + 1)]
+        dp = {}
+        def dfs(s, start):
+            # End, return list containing empty string
+            if start == n:
+                return [""]
+            
+    
+            if start in dp:
+                return dp[start]
 
-        # Start the dfs from index 0 of the string
-        return self.dfs(s, 0)
+            ans = []
+            node = trie.root
+            current_prefix = ""
+
+            
+            for i in range(start, n):
+                char = s[i]
+                # If char not not in the Trie, break the loop
+                if char not in node.children:
+                    break
+
+                node = node.children[char]
+                current_prefix += s[i]
+
+                # If we find a valid word (end of a word in the Trie)
+                if node.isEnd:
+                    rest = dfs(s, i + 1)
+                    for r in rest:
+                        ans.append(current_prefix + (" " if r else "") + r)
+
+            # Store the result for this start index in dp
+            dp[start] = ans
+            return ans
 
 
+        return dfs(s,0)
+        
+
+        # OPTION 1: EASIEST: BACKTRACKING
+        # n = len(s)
+        # res = []
+        # wordDict = set(wordDict)
 
 
+        # # helper
+        # def backtrack(slate, start):
+        #     if start >= n:
+        #         res.append(" ".join(slate[:]))
+        #         return
 
-        # Option 2: Memoization
+
+        #     for end in range(start, n):
+        #         if s[start:end+1] in wordDict:
+        #             # print(s[start:end+1])
+        #             slate.append(s[start:end+1])
+        #             backtrack(slate, end+1)
+        #             slate.pop()
+        
+        # backtrack([],0)
+        # return res
+
+
+        # Option 3: Memoization
 
         # res = []
         # n = len(s)
@@ -103,29 +117,4 @@ class Solution:
 
         # return helper(0)
     
-
-        
-
-        # OPTION 1: EASIEST: BACKTRACKING
-        # n = len(s)
-        # res = []
-        # wordDict = set(wordDict)
-
-
-        # # helper
-        # def backtrack(slate, start):
-        #     if start >= n:
-        #         res.append(" ".join(slate[:]))
-        #         return
-
-
-        #     for end in range(start, n):
-        #         if s[start:end+1] in wordDict:
-        #             # print(s[start:end+1])
-        #             slate.append(s[start:end+1])
-        #             backtrack(slate, end+1)
-        #             slate.pop()
-        
-        # backtrack([],0)
-        # return res
         

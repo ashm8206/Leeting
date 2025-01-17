e44bb48fd515095a79bee880901cd3d7075b7b78
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-16 19:38:26 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (35.50%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,25 +1,44 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        
-        n = len(s)
+
         res = []
+        n = len(s)
         wordDict = set(wordDict)
 
+        def helper(idx, slate):
 
-        # helper
-        def backtrack(slate, start):
-            if start >= n:
+            if idx == n:
                 res.append(" ".join(slate[:]))
                 return
-
-
-            for end in range(start, n):
-                if s[start:end+1] in wordDict:
-                    # print(s[start:end+1])
-                    slate.append(s[start:end+1])
-                    backtrack(slate, end+1)
+            
+            for i in range(idx,n):
+                if s[idx:i+1] in wordDict:
+                    slate.append(s[idx:i+1])
+                    helper(i+1, slate)
                     slate.pop()
-        
-        backtrack([],0)
+                    
+        helper(0, [])
         return res
+        
+        # n = len(s)
+        # res = []
+        # wordDict = set(wordDict)
+
+
+        # # helper
+        # def backtrack(slate, start):
+        #     if start >= n:
+        #         res.append(" ".join(slate[:]))
+        #         return
+
+
+        #     for end in range(start, n):
+        #         if s[start:end+1] in wordDict:
+        #             # print(s[start:end+1])
+        #             slate.append(s[start:end+1])
+        #             backtrack(slate, end+1)
+        #             slate.pop()
+        
+        # backtrack([],0)
+        # return res
         

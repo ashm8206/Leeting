e425852b2ac123422ffdcc76f0a665d743057337
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-19 18:08:16 -0800

    Time: 26 ms (48.27%) | Memory: 19.7 MB (7.14%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,44 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        # seen = set()
+        # def area(r, c):
+        #     if not (0 <= r < len(grid) and 0 <= c < len(grid[0])
+        #             and (r, c) not in seen and grid[r][c]):
+        #         return 0
+        #     seen.add((r, c))
+        #     return (1 + area(r+1, c) + area(r-1, c) +
+        #             area(r, c-1) + area(r, c+1))
+
+        # return max(area(r, c)
+        #            for r in range(len(grid))
+        #            for c in range(len(grid[0])))
+
+        N = len(grid)
+        M = len(grid[0])
+        seen = set()
+
+        def get_area(r,c):
+            if not (0 <= r < len(grid) and 0 <= c < len(grid[0])
+                    and (r, c) not in seen and grid[r][c]==1):
+                return 0
+
+            seen.add((r, c))
+
+            ans = 1
+            ans+=get_area(r+1, c)
+            ans+=get_area(r-1, c)
+            ans+=get_area(r, c-1) 
+            ans+= get_area(r, c+1)
+            return ans
+
+            # return (1 + get_area(r+1, c) + get_area(r-1, c) +
+            #         get_area(r, c-1) + get_area(r, c+1))
+
+
+        max_area = 0
+        for r in range(N):
+            for c in range(M):
+                if grid[r][c]==1 and (r,c) not in seen:
+                    max_area = max(max_area,get_area(r,c))
+        return max_area

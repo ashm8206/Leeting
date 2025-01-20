7738fd1ef4f6c41a31a8dd2a93b25e8cd60756e4
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-19 16:30:40 -0800

    Time: 95 ms (53.77%) | Memory: 19.4 MB (82.08%) - LeetSync

--- 2701-subsequence-with-the-minimum-score/subsequence-with-the-minimum-score.py
+++ 2701-subsequence-with-the-minimum-score/subsequence-with-the-minimum-score.py
@@ -1,45 +1,40 @@
 class Solution:
     def minimumScore(self, s: str, t: str) -> int:
-        suffix = [-1]*len(s)
-        j = len(t)-1
-        for i in reversed(range(len(s))): 
-            if 0 <= j and s[i] == t[j]: 
-                j -= 1
-            suffix[i] = j 
-
+       
+        # suffix[i] stores the position in t that matches with s[i:] suffix part
+        # initialized with -1 for all positions
+        suffix = [-1] * len(s)
         
-        ans = j + 1
-        j = 0 
-        for i, ch in enumerate(s): 
-            ans = min(ans, max(0, suffix[i] - j + 1))
-            if j < len(t) and s[i] == t[j]: j += 1
-        return min(ans, len(t)-j)
+        # First pass: build suffix array going from right to left
+        right = len(t) - 1  # pointer for string t
+        for i in reversed(range(len(s))):
+            if 0 <= right and s[i] == t[right]:
+                right -= 1  # found a match, move t pointer left
+            suffix[i] = right  
+            # store position in t that we need to match
+        
+        # Initial answer: characters left unmatched after first pass
+        ans = right + 1
+        # its importnat to do this as j = -1 for matching the first character
+        
+        # Second pass: go left to right
+        left = 0  # reset t pointer to start
+        for i, ch in enumerate(s):
+            # Try removing characters between current j and suffix[i]
+            # This represents removing characters between current match and next suffix match
+            ans = min(ans, max(0, suffix[i] - left + 1))
+            
+            if left < len(t) and s[i] == t[left]:
+                left += 1  # found a match, move t pointer right
+                
+        # Return minimum of current answer and remaining unmatched characters
+        return min(ans, len(t) - left)
+        
+        '''In essence, this final comparison ensures we take the minimum between:
+
+The best score we found by removing characters in the middle of t
+Simply removing all remaining unmatched characters at the end of t
+
+This is crucial because sometimes it's better to just remove all remaining unmatched characters rather than trying to find a window of characters to remove in the middle of the string.'''
+        
     
-    # def minimumScore(self, s: str, t: str) -> int:
-
-    #     ss, st = len(s), len(t)
-    #     k = st - 1
-    #     dp = [-1] * st
-        
-    #     # Step 1: Fill dp array by tracking the positions of matching characters from the end
-    #     for i in range(ss - 1, -1, -1):
-    #         if k >= 0 and s[i] == t[k]:
-    #             dp[k] = i
-    #             k -= 1
-        
-    #     # Step 2: Find the minimum score
-    #     res = k + 1
-    #     if res == 0:
-    #         return 0
-
-    #     j = 0
-    #     for i in range(ss):
- 
-    #         if j < st and s[i] == t[j]:
-    #             while k < st and dp[k] <= i:
-    #                 k += 1
-    #             res = min(res, k - (j + 1))
-    #             j += 1
-        
-    #     return res
-      

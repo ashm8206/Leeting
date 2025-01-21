20cde5bdf39329405af3a41242ad94dfe4934907
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-21 00:45:49 -0800

    Time: 387 ms (85.52%) | Memory: 63.7 MB (55.90%) - LeetSync

--- 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.py
+++ 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.py
@@ -1,18 +1,29 @@
 class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
-        graph = collections.defaultdict(list)
-        for node, parent in enumerate(parents):  # build graph
-            graph[parent].append(node)
-        n = len(parents)                         # total number of nodes
-        d = collections.Counter()
-        def count_nodes(node):                   # number of children node + self
-            p, s = 1, 0                          # p: product, s: sum
-            for child in graph[node]:            # for each child (only 2 at maximum)
-                res = count_nodes(child)         # get its nodes count
-                p *= res                         # take the product
-                s += res                         # take the sum
-            p *= max(1, n - 1 - s)               # times up-branch (number of nodes other than left, right children ans itself)
-            d[p] += 1                            # count the product
-            return s + 1                         # return number of children node + 1 (self)
-        count_nodes(0)                           # starting from root (0)
-        return d[max(d.keys())]                  # return max count
+        children = defaultdict(list)
+        
+        for node, parent in enumerate(parents):
+            children[parent].append(node)
+        
+        freq = defaultdict(int)
+        NODES = len(parents)
+        
+        # Idea is when we remove a node, we split it into, three parts,
+        # 1. The left subtree(left) 
+        # 2. The right subtree (right) 
+        # 3. All other nodes connected to it's parent (up)
+        def dfs(node):
+            left = right = 0
+            if children[node]: # Count left subtree, it is binary tree so we can index 0/1 for left/right
+                left = dfs(children[node][0])
+            if len(children[node]) > 1: # Count right subtree
+                right = dfs(children[node][1])
+            up = NODES - left - right - 1 # Count nodes connected to parent
+            # short-circuiting, becomes 1 if expr evaluates to 0
+            score = (left or 1) * (right or 1) * (up or 1)
+            freq[score] += 1
+            return 1 + left + right
+        
+        dfs(0)
+        # max to get frequency of max score
+        return freq[max(freq)] 

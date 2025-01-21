25d8fc87010458df0182d74bfa128337d0267c49
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-21 00:39:01 -0800

    Time: 130 ms (13.17%) | Memory: 21.1 MB (64.56%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -6,74 +6,117 @@
 #         self.right = None
 
 class Codec:
-
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        
-        if not root:
-            return ''
-
         res = []
-        queue = collections.deque()
-        queue.append(root)
-
-        while queue:
-            node = queue.popleft()
-            if not node:
-                res.append('None')
-            else:
-                res.append(str(node.val))
-                queue.append(node.left)
-                queue.append(node.right)
-            
-        while res[-1]=='None':
-            res.pop()
-        return ','.join(res)
-
+        def ser(root):
+            if not root:
+                res.append("None,")
+                return
+            res.append(str(root.val)+ ",")
+            ser(root.left)
+            ser(root.right)
+        ser(root)
+        # print(res)
+        return "".join(res)
+    
     def deserialize(self, data):
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
-        if not data:
-            return None
+        def deser(nodes):
+            if not nodes:
+                return None
+            if nodes and nodes[0] == 'None':
+                nodes.pop(0)
+                return None
+            
+            root = TreeNode(int(nodes[0]))
+            nodes.pop(0)
+            root.left = deser(nodes)
+            root.right = deser(nodes)
+            return root
+        
 
-        ls = data.split(',')
-        root = ls.pop(0)
+        nodes = data.split(",")
+        # print(data)
+        return deser(nodes)
+
+
+
+    # def serialize(self, root):
+    #     """Encodes a tree to a single string.
         
-        new_root = TreeNode(int(root))
+    #     :type root: TreeNode
+    #     :rtype: str
+    #     """
         
-        queue = collections.deque()
-        queue.append(new_root)
+    #     if not root:
+    #         return ''
+
+    #     res = []
+    #     queue = collections.deque()
+    #     queue.append(root)
+
+    #     while queue:
+    #         node = queue.popleft()
+    #         if not node:
+    #             res.append('None')
+    #         else:
+    #             res.append(str(node.val))
+    #             queue.append(node.left)
+    #             queue.append(node.right)
+            
+    #     while res[-1]=='None':
+    #         res.pop()
+    #     return ','.join(res)
+
+    # def deserialize(self, data):
+    #     """Decodes your encoded data to tree.
+        
+    #     :type data: str
+    #     :rtype: TreeNode
+    #     """
+    #     if not data:
+    #         return None
+
+    #     ls = data.split(',')
+    #     root = ls.pop(0)
+        
+    #     new_root = TreeNode(int(root))
+        
+    #     queue = collections.deque()
+    #     queue.append(new_root)
     
 
-        while queue:
-            node = queue.popleft()
+    #     while queue:
+    #         node = queue.popleft()
         
             
-            if len(ls) > 0:
-                if ls[0]!= 'None':
-                    node.left = TreeNode(int(ls.pop(0))) 
-                    queue.append(node.left)
-                else:
-                    ls.pop(0)
-                    node.left = None
+    #         if len(ls) > 0:
+    #             if ls[0]!= 'None':
+    #                 node.left = TreeNode(int(ls.pop(0))) 
+    #                 queue.append(node.left)
+    #             else:
+    #                 ls.pop(0)
+    #                 node.left = None
 
-            if len(ls) > 0:
-                if ls[0]!= 'None':
-                    node.right = TreeNode(int(ls.pop(0))) 
-                    queue.append(node.right)
-                else:
-                    ls.pop(0)
-                    node.right = None
+    #         if len(ls) > 0:
+    #             if ls[0]!= 'None':
+    #                 node.right = TreeNode(int(ls.pop(0))) 
+    #                 queue.append(node.right)
+    #             else:
+    #                 ls.pop(0)
+    #                 node.right = None
 
-            # print(ls, queue)
-        return new_root
+    #         # print(ls, queue)
+    #     return new_root
 
 
         

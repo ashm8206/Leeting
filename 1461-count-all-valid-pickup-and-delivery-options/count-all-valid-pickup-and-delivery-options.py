3f229c082876d1ca4c6800370b5a595c605954a4
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-21 08:54:04 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (41.16%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
@@ -1,9 +1,14 @@
 class Solution:
      def countOrders(self, n: int) -> int:
-        ans, MOD = 1, 10**9 + 7
-        for i in range(2, n+1):
-            spaceNum = (i - 1) * 2 + 1
-            ans *= spaceNum * (spaceNum + 1) // 2
+        MOD = 1_000_000_007
+        ans = 1
+
+        for i in range(1, n + 1):
+            # Ways to arrange all pickups, 1*2*3*4*5*...*n
+            ans = ans * i
+            # Ways to arrange all deliveries, 1*3*5*...*(2n-1)
+            ans = ans * (2 * i - 1)
             ans %= MOD
+        
         return ans
         

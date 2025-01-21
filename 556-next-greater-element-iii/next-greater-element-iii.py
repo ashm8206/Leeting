c9ad22b71b6df825f5f2ea22d05eea4e7d584f1e
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-20 23:39:34 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.15%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.py
+++ 556-next-greater-element-iii/next-greater-element-iii.py
@@ -4,26 +4,41 @@ class Solution:
 
         digits = list(str(n))
         n = len(digits)
-        i = n - 1
-        while i-1>=0 and digits[i-1]>=digits[i]:
-            i-=1
 
-        if i == 0:
+        i = j = n-1
+
+        while i > 0 and digits[i-1] >= digits[i]:
+            i -= 1
+
+        if i == 0:   # nums are in descending order
             return -1
         
-        j = i
-        # i....n
-        # last digit > digits[i-1]
-        # last digit > 5
+        k = i - 1    # find the last "ascending" position
+        while digits[j] <= digits[k]:
+            j -= 1
 
-        while j+1 < n and digits[j+1] > digits[i-1]:
-            j+=1
-        digits[i-1], digits[j] =   digits[j], digits[i-1]
+        digits[k], digits[j] = digits[j], digits[k] 
+
+        # i = n - 1
+        # while i-1>= 0 and digits[i-1]>=digits[i]:
+        #     i-=1
+
+        # if i == 0:
+        #     return -1
+        
+        # j = i
+        # # i....n
+        # # last digit > digits[i-1]
+        # # last digit > 5
+
+        # while j+1 < n and digits[j+1] > digits[i-1]:
+        #     j+=1
+        # digits[i-1], digits[j] =   digits[j], digits[i-1]
         digits[i:] = digits[i:][::-1]
         ret = int(''.join(digits))
 
         return ret if ret < 1<<31 else -1
-        
+
         # s = str(n)
         # lenS = len(s)
 

45e0c71f780140914bd6585e0a8d0717144e47f5
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-20 23:16:25 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.26%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.py
+++ 556-next-greater-element-iii/next-greater-element-iii.py
@@ -2,27 +2,49 @@ from collections import defaultdict
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
 
-        s = str(n)
-        lenS = len(s)
+        digits = list(str(n))
+        n = len(digits)
+        i = n - 1
+        while i-1>=0 and digits[i-1]>=digits[i]:
+            i-=1
 
-        res = []
-        countMap = Counter(s)
-        def helper(slate):
-            if len(slate) == lenS:
-                res.append(int("".join(slate[:])))
-                return
+        if i == 0:
+            return -1
+        
+        j = i
+        # i....n
+        # last digit > digits[i-1]
+        # last digit > 5
+
+        while j+1 < n and digits[j+1] > digits[i-1]:
+            j+=1
+        digits[i-1], digits[j] =   digits[j], digits[i-1]
+        digits[i:] = digits[i:][::-1]
+        ret = int(''.join(digits))
+
+        return ret if ret < 1<<31 else -1
+        
+        # s = str(n)
+        # lenS = len(s)
+
+        # res = []
+        # countMap = Counter(s)
+        # def helper(slate):
+        #     if len(slate) == lenS:
+        #         res.append(int("".join(slate[:])))
+        #         return
             
-            for val in countMap.keys() :
-                if countMap[val] <= 0 :
-                    continue
-                slate.append(val)
-                countMap[val]-=1
-                helper(slate)
-                slate.pop()
-                countMap[val]+=1
+        #     for val in countMap.keys():
+        #         if countMap[val] <= 0 :
+        #             continue
+        #         slate.append(val)
+        #         countMap[val]-=1
+        #         helper(slate)
+        #         slate.pop()
+        #         countMap[val]+=1
         
-        helper([])
-        res.sort()
-        idx = res.index(n) + 1
+        # helper([])
+        # res.sort()
+        # idx = res.index(n) + 1
         
-        return  -1  if idx >= len(res) or res[idx] >= 2**31 else res[idx]
+        # return  -1  if idx >= len(res) or res[idx] >= 2**31 else res[idx]

13bbe790abf0ed62935cc2456625638b8acea45a
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 14:47:51 -0800

    Time: 1 ms (53.54%) | Memory: 18 MB (5.74%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,30 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        res = []
+        sign = 1
+        sign_seen = False
+        for i, ch in enumerate(s):
+            if not ch.isdigit():
+                if ch in {"+","-"} and not sign_seen and len(res)==0:
+                    sign_seen = True
+                    sign = int(ch+"1") 
+                elif ch==" " and len(res)==0 and not sign_seen:
+                    continue
+                else:
+                    break
+            else:
+                res.append(ch)
+            # print(res)
+        # how do you handle sign ?
+        
+        # print(res)
+        if len(res) > 0:
+            ans = sign*int("".join(res)) 
+            if ans < -2**31:
+                return -2**31
+            elif ans > 2**31 - 1:
+                return 2**31-1
+            else:
+                return ans
+        else:
+            return 0

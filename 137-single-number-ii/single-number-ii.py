d21ad8fc416f217e28e0d9b5489e2b029d2d87f1
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 15:42:56 -0800

    Time: 1 ms (80.52%) | Memory: 19.1 MB (52.97%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -5,7 +5,10 @@ class Solution:
         twos = 0
 
         for num in nums:
-            ones ^= (num & ~twos)
-            twos ^= (num & ~ones)
+            ones ^= (num & ~twos) # (ones ^ i) & (~twos)
+            
+            twos ^= (num & ~ones) # (twos ^ i) & (~ones)
 
         return ones
+
+        # https://leetcode.com/problems/single-number-ii/solutions/3714928/bit-manipulation-c-java-python-beginner-friendly

795163b10a45a172a275282b25a41ceb7480295f
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-25 18:08:41 -0800

    Time: 907 ms (21.06%) | Memory: 247.8 MB (6.78%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -2,29 +2,48 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
        
+        n = len(s)
+        m = len(t)
+        dp = {}
 
-        m = len(s)
-        n = len(t)
-        memo = {}
+        def helper(i,j):
+            if i == n:
+                return int(j == m)
+            
+            if j== m:
+                return 1
+            if (i,j) in dp :
+                return dp[(i,j)]
 
-        def helper(i, j):
-
-            if i==m or j==n or  m - i < n - j:
-                return 1 if j==n else 0
-
-            if (i,j) in memo:
-                return memo[(i,j)]
-          
-            ans = helper(i+1,j)
-
-            # print(ans)
-
+            res = helper(i+1, j)
             if s[i]==t[j]:
-                ans += helper(i+1,j+1)
-
-            memo[(i,j)] = ans
-
-            return memo[(i,j)]
-
+                res += helper(i+1, j+1)
+            dp[(i,j)] = res
+            return dp[(i,j)]
         return helper(0,0)
+
+        # m = len(s)
+        # n = len(t)
+        # memo = {}
+
+        # def helper(i, j):
+
+        #     if i==m or j==n or  m - i < n - j:
+        #         return 1 if j==n else 0
+
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+          
+        #     ans = helper(i+1,j)
+
+        #     # print(ans)
+
+        #     if s[i]==t[j]:
+        #         ans += helper(i+1,j+1)
+
+        #     memo[(i,j)] = ans
+
+        #     return memo[(i,j)]
+
+        # return helper(0,0)
     

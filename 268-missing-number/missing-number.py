d4a3b2c6a40ce032fcec1b571c2dde92a0a94dd8
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 15:33:05 -0800

    Time: 0 ms (100.00%) | Memory: 18.9 MB (25.59%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,22 +1,11 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-
         mask = n
-        xor = 0
-
+        x = 0
         for i in range(n):
             mask^=i
-            xor^=nums[i]
-        
-        return xor^mask
-           
-                # this is the dup, first time you get it set it
+            x^=nums[i]
             
-          
+        return x ^ mask
         

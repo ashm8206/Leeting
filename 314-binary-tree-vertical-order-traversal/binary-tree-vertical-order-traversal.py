1f6a1ab20204b074f658e3e9d53206bb47f5332b
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 21:33:12 -0800

    Time: 1 ms (29.51%) | Memory: 17.8 MB (38.95%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -7,38 +7,75 @@ from collections import deque, defaultdict
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # 9, 3, 15 20 7
-        column_map = defaultdict(list)
 
-        min_col = 10**5
+        colMap = defaultdict(list)
+        if not root:
+            return[]
 
-        max_col = -10**5
+        q = deque()
+        q.append((root,0)) #01st col
+        maxCol = -2**31 - 1
+        minCol = 2**31 + 1
 
-        if root is None:
-            return []
+        while q:
+            node, col = q.popleft()
 
-        queue = deque()
-        queue.append((root, 0))
+            if node.left:
+                q.append((node.left, col-1))
+            
+            if node.right:
+                q.append((node.right, col+1))
+            
+            colMap[col].append(node.val)
+            
 
-        while queue:
-            len_of_queue = len(queue)
+            maxCol = max(maxCol, col)
+            minCol = min(minCol, col)
 
-            for _ in range(len_of_queue):
+        res = []
+        for cIdx in range(minCol, maxCol+1):
+            res.append(colMap[cIdx])
+        
+        return res
+            
 
-                node, col_idx = queue.popleft()
 
-                if node.left:
-                    queue.append((node.left, col_idx-1))
 
-                if node.right:
-                    queue.append((node.right, col_idx+1))
+
+
+
+        # # 9, 3, 15 20 7
+        # column_map = defaultdict(list)
+
+        # min_col = 10**5
+
+        # max_col = -10**5
+
+        # if root is None:
+        #     return []
+
+        # queue = deque()
+        # queue.append((root, 0))
+
+        # while queue:
+        #     len_of_queue = len(queue)
+
+        #     for _ in range(len_of_queue):
+
+        #         node, col_idx = queue.popleft()
+
+        #         if node.left:
+        #             queue.append((node.left, col_idx-1))
+
+        #         if node.right:
+        #             queue.append((node.right, col_idx+1))
                 
-                min_col = min(min_col, col_idx)
-                max_col = max(max_col, col_idx)
+        #         min_col = min(min_col, col_idx)
+        #         max_col = max(max_col, col_idx)
 
-                column_map[col_idx].append(node.val)
+        #         column_map[col_idx].append(node.val)
 
-        return [column_map[x] for x in range(min_col, max_col+1)]
+        # return [column_map[x] for x in range(min_col, max_col+1)]
 
                 
 

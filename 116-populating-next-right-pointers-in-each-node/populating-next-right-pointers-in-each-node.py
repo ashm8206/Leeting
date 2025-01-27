2ebd9bd68b19809db0a73ac529e361d3d85baa34
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 21:39:54 -0800

    Time: 43 ms (90.48%) | Memory: 19.2 MB (29.69%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -10,6 +10,7 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+
         
         if not root:
             return root
@@ -21,12 +22,10 @@ class Solution:
         while queue:
             length = len(queue)
 
-            # print(length)
             for i in range(length):
                 
                 node = queue.popleft()
 
-                # print(node.val)
                 
                 if node.left:
                     queue.append(node.left)
@@ -34,14 +33,9 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
 
-                if  i < length - 1:
+                if  i < length - 1: #0...n-2
                     next_node = queue[0]
                 
                     node.next = next_node
-                #     node = node.next
-            # node.next = None
-
-            
-        
         return root
 

4e55a19d07a346ca71bda06738e56b840470dc56
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 20:32:27 -0800

    Time: 6 ms (74.28%) | Memory: 18 MB (22.08%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -4,10 +4,15 @@ class Solution:
         # Option 1# https://leetcode.com/problems/regular-expression-matching/solutions/883719/python-top-down-dp-clean-concise-o-m-n/
 
         m, n = len(s), len(p)
-        @lru_cache()
+        # @lru_cache()
+        dp = {}
         def dfs(i, j):
+
             if j == n:
                 return i == m
+
+            if (i,j) in dp:
+                return dp[(i,j)]
             
             match = i < m and (s[i] == p[j] or p[j] == ".")
             # use * only if prev is match
@@ -15,12 +20,20 @@ class Solution:
             # But we are only interested in  upto i < m
 
             if (j + 1) < n and p[j + 1] == "*":
-                return (dfs(i, j + 2) or          # don't use * 
-                       (match and dfs(i + 1, j))) # use *
-                # use * only if prev is match
-            if match:
-                return dfs(i + 1, j + 1)
-            return False
+
+                dp[(i,j)] = dfs(i, j+2) or (match and dfs(i + 1, j))
+
+                # return (dfs(i, j + 2) or          # don't use * 
+                #        (match and dfs(i + 1, j))) # use *
+                # # use * only if prev is match
+            
+            elif match:
+                dp[(i,j)] =  dfs(i + 1, j + 1)
+            else:
+                dp[(i,j)] = False
+            
+            return dp[(i,j)]
         
-        return dfs(0, 0)
+        dfs(0,0)
+        return dp[(0,0)]
         

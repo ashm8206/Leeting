3cf2c98b5dcd5bbec882f3b24257e5b8632247e5
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 20:42:45 -0800

    Time: 1572 ms (5.01%) | Memory: 19.2 MB (73.29%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -6,22 +6,26 @@ class Solution:
             if j == len(p):
                 return i == len(s)
 
-            if i == len(s): 
-                # end of string to match
-                # then are the rest "*"
-                return all(x == '*' for x in p[j:])
+            # if i == len(s): 
+            #     # end of string to match
+            #     # then are the rest "*"
+            #     return all(x == '*' for x in p[j:])
             
 
-            if (s[i]==p[j] or p[j]=="?"):
+            if (i < len(s) and s[i]==p[j] or p[j]=="?"):
                 return dfs(i+1,j+1)
 
             elif p[j]=="*":
-                return dfs(i, j+1) or dfs(i+1, j)
-                
+                return dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
+
             else:
                 return False
             
         return dfs(0,0)
+
+        # https://leetcode.com/problems/wildcard-matching/solutions/1336621/python-dfs-with-memoization-clean-concise/
+        
+        
 
         dp  = {}
         def dfs(i, j):

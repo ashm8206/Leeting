cce04239c58b303f5bea1be77a9fc28c2dd02684
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 20:38:46 -0800

    Time: 1542 ms (5.01%) | Memory: 19.4 MB (73.27%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,9 +1,7 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # https://leetcode.com/problems/wildcard-matching/solutions/1336621/python-dfs-with-memoization-clean-concise/
-        # @lru_cache()
-
-        dp = {}
+        @lru_cache()
         def dfs(i, j):
             if j == len(p):
                 return i == len(s)
@@ -13,18 +11,39 @@ class Solution:
                 # then are the rest "*"
                 return all(x == '*' for x in p[j:])
             
-            if (i,j) in dp:
-                return dp[(i,j)]
 
             if (s[i]==p[j] or p[j]=="?"):
-                dp[(i,j)] = dfs(i+1,j+1)
-                return dp[(i,j)]
+                return dfs(i+1,j+1)
 
-            if p[j]=="*":
-                dp[(i,j)] = dfs(i, j+1) or dfs(i+1, j)
+            elif p[j]=="*":
+                return dfs(i, j+1) or dfs(i+1, j)
+                
+            else:
+                return False
+            
+        return dfs(0,0)
+
+        dp  = {}
+        def dfs(i, j):
+
+            if j == len(p):
+                return i == len(s)
+            
+            if (i,j) in dp:
                 return dp[(i,j)]
             
-            dp[(i,j)] = False
-            return dp[(i,j)]
+            if i < len(s) and (s[i]==p[j] or p[j]=="?"):
+                dp[(i,j)] = dfs(i+1,j+1)
+                return dp[(i,j)]
+            
+            if p[j]=="*":
+               
+                dp[(i,j)]  =  dfs(i, j+1) or ( i < len(s) and dfs(i+1, j))
+                return dp[(i,j)]
+
+          
+            # if it  doesnt match return False
+            dp[(i,j)]= False
+            return  dp[(i,j)]
     
         return dfs(0,0)

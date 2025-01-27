cec152089521c7b61755668cb8d6c2d95679b81f
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 20:48:03 -0800

    Time: 4 ms (82.92%) | Memory: 18.2 MB (12.79%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,10 +1,9 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
 
-        # Option 1# https://leetcode.com/problems/regular-expression-matching/solutions/883719/python-top-down-dp-clean-concise-o-m-n/
 
         m, n = len(s), len(p)
-        # @lru_cache()
+
         dp = {}
         def dfs(i, j):
 
@@ -13,27 +12,61 @@ class Solution:
 
             if (i,j) in dp:
                 return dp[(i,j)]
+
             
             match = i < m and (s[i] == p[j] or p[j] == ".")
-            # use * only if prev is match
-            # this condition can cause the pattern to keep growing
-            # But we are only interested in  upto i < m
 
             if (j + 1) < n and p[j + 1] == "*":
-
                 dp[(i,j)] = dfs(i, j+2) or (match and dfs(i + 1, j))
-
-                # return (dfs(i, j + 2) or          # don't use * 
-                #        (match and dfs(i + 1, j))) # use *
-                # # use * only if prev is match
-            
-            elif match:
-                dp[(i,j)] =  dfs(i + 1, j + 1)
+            elif i < len(s) and match:
+                dp[(i,j)] = dfs(i+1,j+1)
             else:
                 dp[(i,j)] = False
-            
             return dp[(i,j)]
+            
+            # # use * only if prev is match
+            # # this condition can cause the pattern to keep growing
+            # # But we are only interested in  upto i < m
+
+            # if (j + 1) < n and p[j + 1] == "*":
+            #     # j+1= "*", so start at j+2
+            #     dp[(i,j)] = dfs(i, j+2) or (match and dfs(i + 1, j))
+            #     # zero match or match 1 more of preceeding 
+            #     # match current and send (i+1, j )  i+1 with same j
+            # elif match:
+            #     dp[(i,j)] =  dfs(i + 1, j + 1)
+            # else:
+            #     dp[(i,j)] = False
+            # return dp[(i,j)]
         
         dfs(0,0)
         return dp[(0,0)]
+
+
+
+        # Memoized implicit
+
+
+        # Option 1# https://leetcode.com/problems/regular-expression-matching/solutions/883719/python-top-down-dp-clean-concise-o-m-n/
+
+        # m, n = len(s), len(p)
+        # @lru_cache()
+        # def dfs(i, j):
+        #     if j == n:
+        #         return i == m
+            
+        #     match = i < m and (s[i] == p[j] or p[j] == ".")
+        #     # use * only if prev is match
+        #     # this condition can cause the pattern to keep growing
+        #     # But we are only interested in  upto i < m
+
+        #     if (j + 1) < n and p[j + 1] == "*":
+        #         return (dfs(i, j + 2) or          # don't use * 
+        #                (match and dfs(i + 1, j))) # use *
+        #         # use * only if prev is match
+        #     if match:
+        #         return dfs(i + 1, j + 1)
+        #     return False
+        
+        # return dfs(0, 0)
         

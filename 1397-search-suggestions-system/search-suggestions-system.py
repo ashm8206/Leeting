f2d28db363fc86b4b057cb01b0a43f202ed1f821
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-27 15:59:28 -0800

    Time: 79 ms (48.88%) | Memory: 23.6 MB (22.52%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,106 +1,44 @@
 class TrieNode:
-    def __init__(self):
-        self.children = dict()
-        self.words = list()
-        self.n = 0
-    # def __str__(self):
-    #     return str(self.children) + str(self.words)
-        
+    def __init__(self, atMost=3):
+        self.children = defaultdict(dict)
+        self.wordList = []
+
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
-        
-    def add_word(self, word):
-        node = self.root
-
-        """At root Level
-        We have empty key, the first char in the Trie, is a child of Empty key
-        { : children { m: children: {o}} , words=[]} At root Level"""
-        
-        for c in word:
-            if c not in node.children: 
-                node.children[c] = TrieNode()
-
-            node = node.children[c] 
-
-            if node.n < 3:
-                node.words.append(word)
-                node.n += 1
-        
-    def find_word_by_prefix(self, prefix):
-        node = self.root
-        for c in prefix:
-            if c in node.children:
-                node = node.children[c]
-            else:
-                return []
-            """
-            Another way of writing the above is:
-            
-            if c not in node.children:
-                return []
-            node = node.children[char] --> every time!!
-
-            This saves the if / else as 
-            its saves time for us from checking common condition again and again
-            through multiple 'ifss'
-            """
-        return node.words  # only return at after entire prefix is matched
-            
-class Solution:
-    def suggestedProducts(self, A: List[str], searchWord: str) -> List[List[str]]:
-        # Method  I
-        A.sort()
-        """
-        Sorting is important, to return lexographically ordered suggestion
-
-        It is important to process words in order, as we add words at each level
-        upto size 3
-
-        To make sure, the correct lexographically words are added as the Top 3 suggestions, (similar to oxford dict, it helps to have the input sorted)
-        """
-        trie = Trie()
-        for word in A:
-            trie.add_word(word)
-
+        self.root = TrieNode(atMost=3)
     
-        result, prefix = [], ''
-        for c in searchWord:
-            prefix += c 
-            result.append(trie.find_word_by_prefix(prefix))
-        return result    
+    def add(self, word):
+        root = self.root
+        
+        for ch in word:
+            if ch not in root.children:
+                root.children[ch] = TrieNode()
+            root = root.children[ch]
 
-        """A Note on Time Complexity
+            root.wordList.append(word)
+            # root.wordList.sort()
+            # if len(root.wordList) > 3:
+            #    root.wordList.pop()
 
-        # Time : 
-            1. Sort : Nlogn
+        
 
-            2. Building trie:   N words of Len M == O(N*M)
-            If there are many words that are prefixes of each other --Tend to --> O(M), where M is Len of Prefix
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
 
-        # Space : Space will also be same as build, with the same constraints/qualifiers incase of common prefixes (m), vs No common prefixes (n*m)
-        """
+        trie = Trie()
+        for product in products:
+            trie.add(product)
+        
+        curr = trie.root
+        res = []
+        for ch in searchWord:
+            if ch in curr.children:
+                curr = curr.children[ch]
+                res.append(sorted(curr.wordList)[:3])
+            else:
+                curr.children = {}
+                res.append([])
+        return res
+   
 
-        # Method II - LeftMost Binary Search
 
-        # A.sort()
-        # res, prefix = [], ''
-        # i = 0
-        # for c in searchWord: 
-        #     prefix += c
-        #     i = bisect.bisect_left(A, prefix, lo=i) # M*LogN
-        #     # we get left most index of prefix in the search
-        #     # and ad next 3 product that match prefix
-            
-        #     #  An optimization: 
-        #     #  passing lo : start index
-        #     #  In each Binary serach, it is not required to start lo = 0
-        #     #  Lo = atleast as small as the "i" from previous prefix matched
-
-        #     res.append([w for w in A[i:i+3] if w.startswith(prefix)]) #--> o(3)*O(k)
-        # return res
-
-        # TC : 
-        # 1. Sort : NlogN 
-        # 2  PrefixSearch : O(M len of Prefix)*Log(N) Binsearch
-        # 3 startswith

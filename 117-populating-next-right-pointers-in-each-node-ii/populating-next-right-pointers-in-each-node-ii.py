cc5a37facd9b01dfe9e0ef41ee87e012ac558fa3
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-26 21:52:07 -0800

    Time: 38 ms (92.27%) | Memory: 18.8 MB (53.73%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -11,83 +11,32 @@ class Node:
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
         
-        #Method I Queue
-        # if not root:
-        #     return root
-        
-        # q = collections.deque()
-        # q.append(root)
-
-        # while q:
-        #     lenQ = len(q)
-        
-        #     for i in range(lenQ):
-        #         node = q.popleft()
-
-        #         if i < lenQ-1:
-        #             node.next = q[0]
-                
-        #         if node.left:
-        #             q.append(node.left)
-                
-        #         if node.right:
-        #             q.append(node.right)
-        # return root
-                
-        # Level2curr.next = if L1right then L1Right else : # Level1curr = Level1.next 
-        # Level2curr= Level2.next
-
-        # Method 2 
-        # Pointers, Space O(1)
-        
-        def process(childNode, prev, leftmost):
-            if childNode:
-                if prev is None:
-                    leftmost = childNode
-                else:
-                    prev.next = childNode
-
-                prev = childNode
-
-            return prev, leftmost
-
         if not root:
-            return root
+            return None
         
-        leftmost = root
-
-        while leftmost: 
-
-            # leftmost  Always comes from previous level.
-            # for Leaf Node, leftmost never gets set 
-            #  so after curr --> reach None
-
-            #  we break out of Outer leftmost Node too
         
+        dummy = Node(-1)
+        head = root
 
-            curr = leftmost
-            # Now Leftmost becomes Head of Parent List
+        while head:
+            curr  = head
+            prev = dummy #dummy = -1
 
-            # before every level, prev is None, and leftmost is None
-            prev = None 
-            leftmost = None
             while curr:
+                if curr.left:
+                    prev.next = curr.left
+                    prev = prev.next
+                if curr.right:
+                    prev.next = curr.right
+                    prev = prev.next
 
-                prev, leftmost = process(curr.left, prev, leftmost)
-                
-                # if leftmost:
-                #     print(curr.val, leftmost.val)
-                # else:
-                #     print(curr.val, None)
-                prev, leftmost = process(curr.right, prev, leftmost)
-                # left most can be right child'd leftmost
-                #  So it is Required!
-                
-            
                 curr = curr.next
-                # move through the parent level with next pointer
-                # parent level is already linked
-
+            head = dummy.next
+            # dummy.next =  pts to start of next level
+            # at each level prev.next 
+            #      = first present node in next level
+            dummy.next = None
+            #unlink this
         return root
         
 

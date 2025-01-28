74ae96f38b0d206119efa4fd151af1449d7931ef
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-27 18:41:12 -0800

    Time: 839 ms (64.92%) | Memory: 35 MB (54.69%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -2,23 +2,43 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         # lcs with string and reverse string
 
-        t = s[::-1]
+        # t = s[::-1]
         
+        # n = len(s)
+        # dp = [ [ 0 for j in range(n+1) ] for i in range(n+1)]
+
+        
+        # for i in range(n-1, -1, -1):
+        #     for j in range(n-1, -1, -1):
+                
+        #         if s[i]==t[j]:
+
+        #             dp[i][j] = 1 + dp[i+1][j+1]
+
+        #         else:
+        #             dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+
+        # return dp[0][0] 
+
         n = len(s)
-        dp = [ [ 0 for j in range(n+1) ] for i in range(n+1)]
+        dp = [ [ 0 for j in range(n) ] for i in range(n)]
 
         
         for i in range(n-1, -1, -1):
-            for j in range(n-1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i+1, n):
                 
-                if s[i]==t[j]:
+                if s[i]==s[j]:
 
-                    dp[i][j] = 1 + dp[i+1][j+1]
+                    dp[i][j] = dp[i+1][j-1] + 2
 
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
 
-        return dp[0][0] 
+        return dp[0][n-1] 
+
+
+        
                 
 
 

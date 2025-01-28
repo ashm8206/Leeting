f53990b9dd22f315a460064921533de183876017
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-27 18:33:50 -0800

    Time: 1462 ms (29.64%) | Memory: 43.6 MB (33.07%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,44 +1,27 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         # lcs with string and reverse string
-        # s1 = s
-        # s2 = s[::-1]
-        # n = len(s)
 
-        # dp = [[ -1 for j in range(n+1)] for i in range(n+1)]
+        t = s[::-1]
         
-        # for i in range(n+1):
-        #     for j in range(n+1):
+        n = len(s)
+        dp = [ [ 0 for j in range(n+1) ] for i in range(n+1)]
 
-        #         if i==0 or j==0:
-        #             dp[i][j] = 0
+        
+        for i in range(n-1, -1, -1):
+            for j in range(n-1, -1, -1):
                 
-        #         if s1[i-1]==s2[j-1]:
-        #             dp[i][j] = 1 + dp[i-1][j-1]
-        #         else:
-        #             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        # return dp[n][n]
-        s1 = s
-        s2 = s[::-1]
-        m = len(s1)
-        n = len(s2)
+                if s[i]==t[j]:
 
-        dp = [ [0 for j in range(n+1)]for i in range(m+1)]
+                    dp[i][j] = 1 + dp[i+1][j+1]
 
-        # for i in range(m+1):
-        #     dp[i][0] = 0
-
-        # for j in range(n+1):
-        #     dp[0][j] = 0
-
-        
-        for i in range(1,m+1):
-            for j in range(1, n+1):
-
-                if s1[i-1]==s2[j-1]:
-                    dp[i][j]= 1 + dp[i-1][j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+
+        return dp[0][0] 
+                
+
+
 
         # Print LCS
         # lcs = []
@@ -57,4 +40,4 @@ class Solution:
         #         j-=1
         # print("".join(reversed(lcs)))
       
-        return dp[m][n]
+        # return dp[m][n]

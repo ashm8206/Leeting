1cb7b597908e3f7893f9167d2709975562e7be2a
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-01-30 19:36:33 -0800

    Time: 47 ms (15.55%) | Memory: 18.5 MB (63.49%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -10,56 +10,6 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
 
-        # Recursive
-        # self.visited = {}
-
-        # def helper(node):
-
-        #     if not node:
-        #         return None
-            
-        #     if node in self.visited:
-        #         return self.visited[node]
-            
-        #     new_node = Node(node.val, None, None)
-
-        #     self.visited[node] = new_node
-
-        #     new_node.next = helper(node.next) if node.next else None
-        #     new_node.random = helper(node.random) if node.random else None
-
-        #     # Postorder will create recursion exceeded. as Random could pt to a node already seen.
-
-        #     return new_node
-        # return helper(head)
-
-
-        #  Iterative + O(N)
-
-        # dummy = ListNode(-1)
-        # dummy.next = head
-        # curr = head
-        # hmap = {}
-        # Create Node 1st
-
-        # while curr: 
-        #     _curr = Node(curr.val)
-        #     hmap[curr] = _curr
-        #     curr = curr.next
-
-        
-        
-        # LinkNodes
-        # curr = head
-        # while curr:
-
-        #     _curr = hmap[curr]
-        #     _curr.next = hmap[curr.next] if curr.next is not None else None
-        #     _curr.random = hmap[curr.random] if curr.random is not None else None
-        #     curr = curr.next
-
-        # return hmap.get(head,None)
-
         if not head:
             return None
         
@@ -82,17 +32,18 @@ class Solution:
 
         ptrA = head
         ptrB = head.next
-        dummy = Node(-1)
-        dummy.next = head.next
+        newHead = head.next
+        # dummy = Node(-1)
+        # dummy.next = head.next
 
-        while ptrA and ptrB:
+        while ptrA.next and ptrB.next:
             ptrA.next = ptrB.next
-            ptrB.next = ptrB.next.next if ptrB.next else None
+            ptrA = ptrA.next
 
-            ptrA = ptrB.next
+            ptrB.next = ptrA.next
             ptrB = ptrB.next
-        # We cant combine the two Random+unweaving cuz we need next ptrs intact
-        return dummy.next
+           
+        return newHead
 
 
     

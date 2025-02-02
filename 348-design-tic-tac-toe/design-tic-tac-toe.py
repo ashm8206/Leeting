c37296f74138ed47e9981ee89b94966a1ec5fc46
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-01 21:54:53 -0800

    Time: 136 ms (14.22%) | Memory: 20.3 MB (37.54%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/design-tic-tac-toe.py
@@ -1,31 +1,28 @@
 class TicTacToe:
 
     def __init__(self, n: int):
+        self.board =[ [0 for _ in range(n)] for _ in range(n)]
         self.n = n
-        self.rows = [0] * n
-        self.cols = [0] * n
-        self.diagonal = 0
-        self.anti_diagonal = 0
-
-
+        
+    # def isWinner(self, player:int ) -> bool:
+        
     def move(self, row: int, col: int, player: int) -> int:
         p = 1 if player == 1 else -1
-        
-        self.rows[row] += p
-        self.cols[col] += p
-        
+        self.board[row][col] = p
+
+        isDiagonal = 0
+        isAntiDiag = 0
+
+        isHorizonal = sum(self.board[row])
+        isVertical = sum([self.board[i][col] for i in range(len(self.board))])
         if row == col:
-            self.diagonal += p
+            isDiagonal =sum([self.board[i][i] for i in range(len(self.board))])
+        if row+col == self.n - 1:
+            isAntiDiag =sum([self.board[i][self.n-i-1] for i in range(len(self.board))])
         
-        if row + col == self.n - 1:
-            self.anti_diagonal += p
-
-        ## After adding Row and COL
-        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diagonal) == self.n or abs(self.anti_diagonal) == self.n:
+        if abs(isHorizonal) == self.n  or abs(isVertical) == self.n  or abs(isAntiDiag)==self.n or abs(isDiagonal)==self.n:
             return player
-        
         return 0
-
 
         
 
@@ -33,7 +30,3 @@ class TicTacToe:
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
 # param_1 = obj.move(row,col,player)
-
-# [o,x,o]
-# [0,o,x]
-# [x,x,o]

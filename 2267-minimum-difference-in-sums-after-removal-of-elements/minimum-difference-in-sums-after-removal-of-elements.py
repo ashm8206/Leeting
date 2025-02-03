8383abd3dc6d834dfcb6419a975cf024203dc84b
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-03 10:38:19 -0800

    Time: 234 ms (97.10%) | Memory: 50.5 MB (45.64%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
@@ -11,10 +11,12 @@ class Solution:
         
         
         for i in range(N, 2*N):
+           
             curr = heappushpop(left, -nums[i])
-            # print(sum_left) Why ?
+            # remove the max value from the sum soFar
+            # add curr_value to it
             sum_left.append(sum_left[-1] + curr + nums[i])
-            # print(sum_left, curr, nums[i])
+            
             
         right = nums[2*N:]
         heapify(right)
@@ -22,6 +24,9 @@ class Solution:
         
         for i in reversed(range(N, 2*N)):
             curr = heappushpop(right, nums[i])
+            # you want to maximize the right
+            # remove the mininum value so far
+            # add curr value
             sum_right.append(sum_right[-1] - curr + nums[i])
             
         return min(l - r for l, r in zip(sum_left, sum_right[::-1]))

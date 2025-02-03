c30d47a4e61599dc14c9d8ef2624f4a456db5dd6
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-03 07:13:14 -0800

    Time: 22 ms (69.40%) | Memory: 18.4 MB (62.65%) - LeetSync

---
+++ 536-construct-binary-tree-from-string/construct-binary-tree-from-string.py
@@ -0,0 +1,76 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def str2tree(self, s: str) -> Optional[TreeNode]:
+
+        # Iterative:
+        i = 0
+        n = len(s)
+        stack = []
+        while i < n:
+
+            if s[i]=="-" or s[i].isdigit():
+                curr = s[i]
+                i+=1
+
+                while i < n and s[i].isdigit():
+                    curr += s[i]
+                    i += 1
+                 
+
+                node = TreeNode(int(curr))
+
+                if stack:
+                    parent = stack[-1]
+                    if parent.left is None:
+                        parent.left = node
+                    else:
+                        parent.right = node
+                stack.append(node)
+                # print(i)
+
+            elif s[i]=="(":
+                i+=1
+
+            else:
+                stack.pop() # pop this level
+                i+=1
+        
+        return stack[0] if stack and n > 0 else None
+        
+
+
+
+        # Recursive
+        # ix = s.find('(')
+        # if ix < 0:
+        #     return TreeNode(int(s)) if s else None
+            
+        # bal = 0
+        # for jx, u in enumerate(s):
+        #     if u == '(': bal += 1
+        #     if u == ')': bal -= 1
+        #     if jx > ix and bal == 0:
+        #         break
+
+        openIdx = s.find("(")
+        if openIdx < 0:
+            return TreeNode(int(s)) if s else None
+        
+        bal = 0
+        closeIdx = 0
+        for i in range(len(s)):
+            if s[i]=="(": bal+=1
+            if s[i]==")": bal-=1
+            if bal==0 and i > openIdx and s[i]==")":
+                closeIdx = i
+                break
+
+        root = TreeNode(int(s[:openIdx]))
+        root.left = self.str2tree(s[openIdx+1:closeIdx]) # don't include "outmost )"
+        root.right = self.str2tree(s[closeIdx+2:-1]) # last but one 
+        return root

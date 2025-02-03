ce611d31721dd19ff818f10dbbc928888b4dae8b
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-03 09:54:51 -0800

    Time: 127 ms (38.36%) | Memory: 18.3 MB (45.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,72 +1,98 @@
+from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
         # https://www.youtube.com/watch?v=jSto0O4AJbM
-        len_s , len_t = len(s), len(t)
+
+
+        len_s, len_t = len(s), len(t)
 
         if len_s < len_t:
             return ""
 
-        ans = ""
-
-        t_count =  defaultdict(int)
-        
-        for char in t:
-            t_count[char]+=1
-
-        
-        minLen = 10**10
-        
-        # METHOD I - BRUTE FORCE
-        
-        # for i in range(len_s):
-
-        #     s_count = defaultdict(int)
-
-        #     for j in range(i, len_s):
-
-        #         s_count[s[j]]+=1
-
-        #         if all(True if char_t in s_count.keys() and s_count[char_t] >= t_count[char_t] else False for char_t in t_count.keys()):
-
-        #             if j-i+1 < minLen:
-        #                 minLen = j-i+1
-        #                 ans = s[i:j+1]
-        # return ans
-
-        # O(N*2*K)
-
-        L = 0
-        have = 0
+        t_count = Counter(t)
+        have = 0 
         need = len(t_count)
-
+        L = 0
+        n = len(s)
         s_count = defaultdict(int)
 
-        for R in range(len_s): #(O(len(s)))
 
+        min_len = 10**9
+        min_win = ''
+        for R in range(n):
             s_count[s[R]]+=1
+
             if s_count[s[R]] == t_count[s[R]]:
-                # increase only when counts match, not when count is greater
-                # greater doesnt help us get minmuz window substring
+                have+=1 
 
-                have+=1
-            
-            while have == need: # O(len(t)) --> Total
-               
-                if R-L+1 < minLen:
-                    minLen = R-L+1 
-                    ans = s[L:R+1]
+            while have == need:
+                if R-L+1 < min_len:
+                    min_len = R-L+1
+                    min_win = s[L:R+1]
                 
-                # Try to minimize it further, # Shrink Window
                 s_count[s[L]]-=1
-            
-                if s[L] in t_count and s_count[s[L]] < t_count[s[L]]:
+                
+                if s[L] in t_count and s_count[s[L]]< t_count[s[L]]:
                     have-=1
-                    # will cause to break
-                    
-                L = L+1
+                     # >= is ok, less is not
 
-        return ans
+                L+=1
+            
+        return min_win
+
+
+
+
+
+        # len_s , len_t = len(s), len(t)
+
+        # if len_s < len_t:
+        #     return ""
+
+        # ans = ""
+
+        # t_count =  defaultdict(int)
+        
+        # for char in t:
+        #     t_count[char]+=1
+
+        
+        # minLen = 10**10
+        
+        
+
+        # L = 0
+        # have = 0
+        # need = len(t_count)
+
+        # s_count = defaultdict(int)
+
+        # for R in range(len_s): #(O(len(s)))
+
+        #     s_count[s[R]]+=1
+        #     if s_count[s[R]] == t_count[s[R]]:
+        #         # increase only when counts match, not when count is greater
+        #         # greater doesnt help us get minmuz window substring
+
+        #         have+=1
+            
+        #     while have == need: # O(len(t)) --> Total
+               
+        #         if R-L+1 < minLen:
+        #             minLen = R-L+1 
+        #             ans = s[L:R+1]
+                
+        #         # Try to minimize it further, # Shrink Window
+        #         s_count[s[L]]-=1
+            
+        #         if s[L] in t_count and s_count[s[L]] < t_count[s[L]]:
+        #             have-=1
+        #             # will cause to break
+                    
+        #         L = L+1
+
+        # return ans
 
 
 

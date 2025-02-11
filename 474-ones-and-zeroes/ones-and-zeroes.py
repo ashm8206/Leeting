b8a327091e3efcece4590c916a3783cc8ed28333
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-10 21:05:49 -0800

    Time: 1880 ms (46.39%) | Memory: 296.9 MB (18.04%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.py
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -6,12 +6,16 @@ class Solution:
                 return 0
             if (ind, m, n) in cache:
                 return cache[ind, m, n]
-            res = 0
+           
             count_0 = strs[ind].count("0")
             count_1 = len(strs[ind]) - count_0
+
+            res = helper(ind + 1, m, n)
+
             if m >= count_0 and n >= count_1:
                 res = max(res, 1 + helper(ind + 1, m - count_0, n - count_1))
-            res = max(res, helper(ind + 1, m, n))
+            
             cache[ind, m, n] = res
-            return res
+            return cache[ind, m, n]
+            
         return helper(0, m, n)

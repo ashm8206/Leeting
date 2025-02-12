30b8892965b5d04b0eef1b628098fcc8d5c0f859
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-12 14:51:50 -0800

    Time: 7 ms (35.54%) | Memory: 17.9 MB (48.90%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,21 +1,25 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        
+    
         n = len(days)
         dp = {}
+        
         def dfs(i):
             if i == n:
                 return 0
+
             if i in dp:
                 return dp[i]
             
             dp[i] = float("inf")
-
-            for d, c in zip([1,7,30],costs):
-                j = i
-                while j < n and days[j] < days[i]+d:
+            j = i
+            for duration, cost in zip([1,7,30],costs):
+                while j < n and days[j] < days[i] + duration:
                     j+=1
-                dp[i] = min(dp[i], c+dfs(j)) 
+                
+                dp[i]= min(dp[i], cost + dfs(j))
+                # print(i, days[i], cost + dfs(j), j ,duration)
+            # print("---")
             return dp[i]
-
+        
         return dfs(0)

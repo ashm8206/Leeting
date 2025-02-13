fe52af21cb83b65b0a5da0426e7c27f5cfe7b28e
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-13 11:53:09 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (14.16%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -3,38 +3,51 @@ class Solution:
         res = []
         n = len(nums)
 
-        def backtrack(slate):
-            if len(slate) == n:
-                # we  only append when len(slate) == n
+        def backtrack(slate,arr):
+            if not arr:
                 res.append(slate[:])
                 return
-
-            for i in range(0,n):
-                # start point is all numbers
-                # This is different from subset,
-                #  each next iteration only cares bout [ i.. n]
-                #  pick or not pick at each i
-
-                # Wheras for permuation
-                # we get to choose from all [:i] [i+1: ]
-                #  constraint of not choosing same [i] again
-
-                if nums[i] in slate:
-                    continue
-                #else
-                slate.append(nums[i])
-                backtrack(slate)
+            
+            for i, num in enumerate(arr):
+                slate.append(num)
+                backtrack(slate, arr[:i]+arr[i+1:])
                 slate.pop()
+        backtrack([],nums)
+        return res
+
+        # def backtrack(slate):
+        #     if len(slate) == n:
+        #         # we  only append when len(slate) == n
+        #         res.append(slate[:])
+        #         return
+
+        #     for i in range(0,n):
+        #         # start point is all numbers
+        #         # This is different from subset,
+        #         #  each next iteration only cares bout [ i.. n]
+        #         #  pick or not pick at each i
+
+        #         # Wheras for permuation
+        #         # we get to choose from all [:i] [i+1: ]
+        #         #  constraint of not choosing same [i] again
+
+        #         if nums[i] in slate:
+        #             continue
+        #         #else
+        #         slate.append(nums[i])
+        #         backtrack(slate)
+        #         slate.pop()
+        # backtrack([])
+        # return res
             
 
 
 
-            # for i in range(first, n):
-            #     nums[i], nums[first] = nums[first], nums[i]
-            #     backtrack(first + 1)
-            #     #They are swapped, i.e  i==first and first==i so we swap back
-            #     nums[i], nums[first] = nums[first], nums[i]
-            #     #nums[i], nums[first] = nums[i], nums[first]
-        # if nums:
-        backtrack([])
-        return res
+        #     # for i in range(first, n):
+        #     #     nums[i], nums[first] = nums[first], nums[i]
+        #     #     backtrack(first + 1)
+        #     #     #They are swapped, i.e  i==first and first==i so we swap back
+        #     #     nums[i], nums[first] = nums[first], nums[i]
+        #     #     #nums[i], nums[first] = nums[i], nums[first]
+        # # if nums:
+        

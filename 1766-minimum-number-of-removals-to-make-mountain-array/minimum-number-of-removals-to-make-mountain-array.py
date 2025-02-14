2627e825323b10dd1d108a4ce9cf698286c5162a
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-14 11:20:04 -0800

    Time: 19 ms (85.75%) | Memory: 17.9 MB (83.61%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+       
+        n = len(nums)
+        if n < 3:
+            return float('inf')  # Not possible with less than 3 elements
+        
+        # Find longest increasing subsequence from left
+        def getLIS(arr):
+            sub = []
+            dp = [1] * len(arr)  # Length of LIS ending at each index
+            
+            for i, num in enumerate(arr):
+                pos = bisect_left(sub, num)
+                if pos == len(sub):
+                    sub.append(num)
+                else:
+                    sub[pos] = num
+                dp[i] = pos + 1  # Store length of LIS ending at i
+            return dp
+        
+        # Get LIS from left to right and right to left
+        left_lis = getLIS(nums)
+        right_lis = getLIS(nums[::-1])[::-1]  # Reverse array for decreasing sequence
+    
+        min_removals = float('inf')
+        # Try each index as peak
+        for i in range(1, n-1):  # Peak can't be at ends
+            # Only consider if both sides have increasing sequences
+            if left_lis[i] > 1 and right_lis[i] > 1:
+                total_length = left_lis[i] + right_lis[i] - 1  # -1 because peak is counted twice
+                removals = n - total_length
+                min_removals = min(min_removals, removals)
+                
+        return min_removals if min_removals != float('inf') else -1

9e247647ed12ed1ab5dfd581996110fc6d63f3ae
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-14 09:55:28 -0800

    Time: 5 ms (83.56%) | Memory: 17.8 MB (82.64%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,8 +1,26 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
 
+        # Recursive
+        def rle(s):
+            count = 1
+            ret = []
+            n = len(s)-1
+            for i in range(0,n+1):
+                if i<n and s[i] == s[i+1]:
+                    count += 1
+                else:
+                    ret.append(str(count))
+                    ret.append(s[i])
+                    count = 1
+            return "".join(ret) 
+        
+        if n == 1:
+            return "1"
+        return rle(self.countAndSay(n-1))
+
         # Iterative
-        def getRunLen(rle_str):
+        # def getRunLen(rle_str):
 
             #  Method I
             # rle_str = deque(list(rle_str))
@@ -16,41 +34,25 @@ class Solution:
             #         stack[-1][1]+=1
             #     else:
             #         stack.append([var, 1])
-            # RLE = "".join( str(y) + x  for x, y in stack)
+        #     # RLE = "".join( str(y) + x  for x, y in stack)
 
-            stack = []
-            n = len(rle_str)
-            cnt = 1
-            for i in range(0, n):
-                if i < n-1 and rle_str[i]==rle_str[i+1]:
-                    cnt+=1
-                else:
-                    stack.append(str(cnt))
-                    stack.append(rle_str[i])
-                    cnt = 1
-            
-            return "".join(stack)
-
-        RLE = '1'
-        while n > 1:
-            RLE = getRunLen(RLE)
-            n-=1 
-        return RLE
-
-        # Recursive
-        # def rle(s):
-        #     count = 1
-        #     ret = []
-        #     n = len(s)-1
-        #     for i in range(0,n+1):
-        #         if i<n and s[i] == s[i+1]:
-        #             count += 1
+        #     stack = []
+        #     n = len(rle_str)
+        #     cnt = 1
+        #     for i in range(0, n):
+        #         if i < n-1 and rle_str[i]==rle_str[i+1]:
+        #             cnt+=1
         #         else:
-        #             ret.append(str(count))
-        #             ret.append(s[i])
-        #             count = 1
-        #     return "".join(ret) 
+        #             stack.append(str(cnt))
+        #             stack.append(rle_str[i])
+        #             cnt = 1
+            
+        #     return "".join(stack)
+
+        # RLE = '1'
+        # while n > 1:
+        #     RLE = getRunLen(RLE)
+        #     n-=1 
+        # return RLE
+
         
-        # if n == 1:
-        #     return "1"
-        # return rle(self.countAndSay(n-1))

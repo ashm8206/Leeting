5e33a50fb65c9a23f54353863c7736fcdf71a4d7
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-14 11:06:10 -0800

    Time: 244 ms (6.01%) | Memory: 19.2 MB (10.29%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,4 +1,15 @@
 class Solution:
+
+    # How is this Minimize the Maximum hence BS ?
+
+    # Well we ned to return minimum integer k
+    # k = # Bananas per Hour
+
+    # This k can a number b/w 1 and  max(piles) = [1,11]: eg 1
+    # it can maximum possible to finish before guard come at hour "h"
+
+    # But Q: Asks for minimum Posible K
+    
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
         left = 1
@@ -20,9 +31,14 @@ class Solution:
         hour_spent = 0
 
         for pile in piles:
-            hour_spent += math.ceil(pile/k_candidate)
+            hour_spent += math.ceil(int(pile)/k_candidate)
             # total_hours += pile_hr
         
         if hour_spent <= h:
             return True
         return False
+    
+    #Let n be the length of the input array piles and m be the maximum number of bananas in a single pile from piles.
+
+#Time complexity: O(n⋅logm)
+

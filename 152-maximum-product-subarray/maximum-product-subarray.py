5d455582f0b2b257e05499760ee5a33671633a05
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-02-14 07:50:25 -0800

    Time: 8 ms (71.01%) | Memory: 18.6 MB (16.91%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -5,36 +5,37 @@ class Solution:
         # minSoFar = 10**9
 
         n = len(nums)
-        # dp =[0]*n
-        # dp_min = [0] * n
 
-        # dp[0] = nums[0]
-        # dp_min[0] = nums[0]
 
-        # for i in range(n):
-        #     if i > 0:
-        #         dp[i] = max([dp[i-1]*nums[i], dp_min[i-1]*nums[i], nums[i]])
-        #         dp_min[i] = min([dp[i-1]*nums[i],dp_min[i-1]*nums[i], nums[i]])
-        #     else:
-        #         dp[i] = nums[i]
-        #         dp_min[i] = nums[i]
-
-        #     maxProduct = max([maxProduct, dp[i]])
-        # print(dp)
-        # print(dp_min)
-        # return maxProduct
+        prefix, suffix, max_so_far = 0, 0, float('-inf')
+        for i in range(n):
+            prefix = (prefix or 1) * nums[i]
+            suffix = (suffix or 1) * nums[n-1-i] # same as ~i
+            max_so_far = max(max_so_far, prefix, suffix)
+        return max_so_far
+        
         
         maxSoFar = nums[0]
         minSoFar = nums[0]
+
         maxProduct = nums[0]
 
         for i in range(1,n):
-            tempProd = max(nums[i], max(maxSoFar*nums[i],minSoFar*nums[i]))
-            maxProduct = max(tempProd,maxProduct)
-            tempMax= maxSoFar*nums[i]
-            tempMin = minSoFar*nums[i]
-            maxSoFar = max(nums[i], max(tempMax,tempMin))
-            minSoFar = min(nums[i], min(tempMax,tempMin))
+            # -11, 11 ,  22
+            # nums[i] = -11
+            # maxSoFar = -1
+            # minSoFar = -2
+            currProd = max(nums[i], max(maxSoFar*nums[i],minSoFar*nums[i]))
+            maxProduct = max(currProd,maxProduct)
+
+            currMax= maxSoFar*nums[i] # assumed currMax
+            currMin = minSoFar*nums[i]
+
+            # by multiplying with curr nums[i]. both maxSofar and minSofar can change.
+            # take the following comparison between 3 values to get NExt maxSoFar and minSoFar
+
+            maxSoFar = max(nums[i], max(currMax,currMin))
+            minSoFar = min(nums[i], min(currMax,currMin))
         return maxProduct
 
         

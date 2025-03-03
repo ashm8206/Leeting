be565efc4229de9cbc37f8dcf695fc948c24f537
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-03 11:53:31 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (61.56%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,60 +1,72 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
 
-            # Line Sweep:
-            count = 0
-            events =  []
-            res = []
+            # # Line Sweep:
+            # count = 0
+            # events =  []  # or SortedList
+            # res = []
 
-            for s, e in intervals + [newInterval]:
-                # events.add((s,1))
-                # events.add((e,-1))
-                events.append((s,1))
-                events.append((e,-1))
+            # for s, e in intervals + [newInterval]:
+            #     # events.add((s,1))
+            #     # events.add((e,-1))
+            #     events.append((s,1))
+            #     events.append((e,-1))
                 
 
-            events.sort()
+            # events.sort()
 
-            n = len(events)
-            start = -1
+            # n = len(events)
+            # start = -1
 
-            # print(events)
-            for i in range(n):
-                if count == 0:
-                    start = events[i][0]
-                count += events[i][1]
-                if count == 0:
-                    if res and res[-1][1]== start:
-                        res[-1] = [min(res[-1][0], start), max(res[-1][1], events[i][0])]
-                    else:
-                        res.append([start, events[i][0]])
-            return res
+            # for i in range(n):
+            #     if count == 0:
+            #         start = events[i][0]
+
+            #     count += events[i][1]
+            #     if count == 0:
+            #         if res and res[-1][1]== start:
+            #             res[-1] = [min(res[-1][0], start), max(res[-1][1], events[i][0])]
+            #         else:
+            #             res.append([start, events[i][0]])
+            # return res
                 
 
             # Linear Scan
 
-            # n = len(intervals)
-            # i = 0
-            # res = []
+            n = len(intervals)
+            if n == 0:
+                return [newInterval]
+            
+            i = 0
+            res = []
 
             # # Case 1: No overlapping before merging intervals
-            # while i < n and intervals[i][1] < newInterval[0]:
-            #     res.append(intervals[i])
-            #     i += 1
+            #  e1 < s2
+            while i < n and intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
+                i += 1
 
-            # # Case 2: Overlapping and merging intervals
-            # while i < n and newInterval[1] >= intervals[i][0]:
-            #     newInterval[0] = min(newInterval[0], intervals[i][0])
-            #     newInterval[1] = max(newInterval[1], intervals[i][1])
-            #     i += 1
-            # res.append(newInterval)
+            # Case 2: Overlapping and merging intervals
+            # [1,5]
+            while i < n and  intervals[i][0] <= newInterval[1]:
+                
+                newInterval[0] = min(newInterval[0], intervals[i][0])
+                newInterval[1] = max(newInterval[1], intervals[i][1])
+                i += 1
+            res.append(newInterval)
 
+            
+        
+
+            while i < n:
+                res.append(intervals[i])
+                i += 1
+
+            return res
+
+            
             # # Case 3: No overlapping after merging newInterval
-            # while i < n:
-            #     res.append(intervals[i])
-            #     i += 1
-
-            # return res
+            
 
             # BS
             # if not intervals:

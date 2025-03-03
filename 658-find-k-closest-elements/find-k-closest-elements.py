0f16f8eafb281d414c73fdadc24dd725221d3739
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-03 14:19:51 -0800

    Time: 3 ms (73.67%) | Memory: 19.3 MB (32.51%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,5 +1,21 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+
+
+        # Initialize binary search bounds
+        left = 0
+        right = len(arr) - k
+        
+        # Binary search against the criteria described
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left:left + k]
+
         # pq = [(abs(arr[i]-x),arr[i]) for i in range(k)]
      
         # for num in arr[k:]:

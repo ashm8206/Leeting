90180492cc20f89c0939aa864817a18bb454f1eb
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-03 14:21:25 -0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (47.67%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,7 +1,7 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
 
-
+  
         # Initialize binary search bounds
         left = 0
         right = len(arr) - k
@@ -9,8 +9,13 @@ class Solution:
         # Binary search against the criteria described
         while left < right:
             mid = (left + right) // 2
+
+            # x - arr[mid]: How far x is above arr[mid]
+            # arr[mid + k] - x: How far arr[mid + k] is above x
             if x - arr[mid] > arr[mid + k] - x:
-                left = mid + 1
+                left = mid + 1 # K is closer to right
+
+                # The given condition x - arr[mid] > arr[mid + k] - x assumes x lies between arr[mid] and arr[mid + k],
             else:
                 right = mid
 

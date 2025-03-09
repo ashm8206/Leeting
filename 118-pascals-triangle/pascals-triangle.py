8ea47da5e13dd67862a08ca4fc234a808c00520d
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-08 21:10:11 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (16.82%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,13 +1,29 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = [ [1 for j in range(i+1)] for i in range(numRows)]
+        res = [[1]]
 
-        # Start i in range(2, numRows)
-        # In col process [1, i]
-
-        for i in range(2, numRows):
-            for j in range(1, i):
-                res[i][j] = res[i-1][j-1] + res[i-1][j] 
-
+        if numRows >=2:
+            res = [[1],[1,1]]
+            prev = res[-1]
+            for i in range(2, numRows):
+                curr = [1]
+                for j in range(1,i):
+                    curr.append(prev[j] + prev[j-1])
+                curr.append(1)
+                res.append(curr)
+                prev = curr
         return res 
+
+        # res = [ [1 for j in range(i+1)] for i in range(numRows)]
+
+
+        # # # Start i in range(2, numRows)
+        # # # In col process [1, i]
+
+        # for i in range(2, numRows):
+        #     for j in range(1, i):
+        #         res[i][j] = res[i-1][j-1] + res[i-1][j]
+        # #         res[i][j] = res[i-1][j-1] + res[i-1][j] 
+
+        # return res 
         

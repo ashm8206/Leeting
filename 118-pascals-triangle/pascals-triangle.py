fc2374cfa5fe50ee0fb94dbd78344e0ac2997331
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-08 21:13:58 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (87.34%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,10 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows   == 0: 
-            return []
-        elif numRows == 1: 
+       
+        if numRows == 1: 
             return [[1]]
-            
+
         Tri = [[1]]
         for i in range(1,numRows):
             row = [1]

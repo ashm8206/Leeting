c5e6177d678564f106ee4c70ed237271d1296c9b
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-10 15:52:08 -0700

    Time: 59 ms (25.12%) | Memory: 38.1 MB (72.69%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,18 +1,27 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        hmap = {0: -1}
+
+        # Neetcode #https://www.youtube.com/watch?v=OKcrLfR-8mE
+        hmap = {0: -1} # we have seen 0%k --> 0 at -1 index
         curr_sum = 0
         n = len(nums)
 
         for i in range(n):
-            curr_sum = (curr_sum + nums[i]) % k
+            curr_sum+= nums[i]
+            key = curr_sum % k
 
-            
-            if curr_sum in hmap.keys():
-                if i - hmap[curr_sum] > 1:
+            # curr_sum = (curr_sum + nums[i]) % k
+
+            # (prefix[j] - prefix[i])%k = 0
+            #  or prefix[j]%k = prefix[i]%k
+            if key in hmap.keys():
+                if i - hmap[key] >= 2:
                     return True
+                # At this place we must not change the remainder index
+                # if second Condition is not met
+                # Test Case: [5,0,0,0,0], k = 3
             else:
-                hmap[curr_sum] = i
+                hmap[key] = i
         return False
 
         

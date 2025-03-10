1defe1559eda1d3c142925d6b12043f5b685e471
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-10 16:55:22 -0700

    Time: 19 ms (76.66%) | Memory: 21.1 MB (89.55%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -3,34 +3,41 @@ class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         # remainder = defaultdict(int)
 
-        # # Distributive mod
-        # # (a + b) mod = (a/mod + b/mod) % mod
-        # count = 0
-        # for t in time:
-        #     if (t%60)==0:
-        #         count += remainder[0]
-        #     else:
-        #         count+= remainder[60-t%60]
-        #     remainder[t%60]+=1
-        # # print(remainder)
-        # return count
+        
 
-    # Mix of two Sum, +  Number of Good Pairs 
-    # (a+b)%60 i.e 
-        # 1. both are divisble by 60 R1 = 0
-        # 2. or their pair divisble sum = 60 
-    # i.e a%60  + b%60 = 60
+ 
 
         hmap = {}
         count = 0
+        # Mix of two Sum, +  Number of Good Pairs 
+        # Distributive mod
+        #  (a + b) % K = (a%k + b%k) % k
 
         for num in time:
+            #  Vanilla Two Sum
+            #  key = target - num:
+
+            # Two sum Divisible pair?
+            # find a in hmap ?
+            # b ==> num
+            # (a+b)%60 = 0 
+            # (a%60  + b%60)%60 = 0
+            #  Two cases
+                # 1. a%60  + b%60 = 0  
+                #       i.e since only positve vals, the both a%60 == b%60 = 0 
+                #       i.e key = 0
+                # 2. a%60  + b%60 = 60
+
+                #  OR  a%60 = 60 - B%60
+            # HENCE TWO CASES!!!
+        
+
             key = num%60
 
             if key == 0:
                 count+= hmap.get(key,0) # both are divisible, so get 0
             else:
-                count+=hmap.get(60-key,0) # diff = b%60
+                count+=hmap.get(60-key,0) # key = b%60
 
             hmap[key] = hmap.get(key,0) + 1
         return count

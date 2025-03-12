af7f9eb83e8f517c5bd2b4048cdce31768d01431
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-12 00:11:26 -0700

    Time: 285 ms (22.75%) | Memory: 25.7 MB (15.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,70 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        m = len(grid)
-        n = len(grid[0])
-        
-
-        def dfs(i,j):
-           
-            if i < 0 or i >= m or  j < 0 or j >= n or grid[i][j]!="1" or (i,j) in seen:
-                return 
-            # watch out
-
-            # watch out for seen
-            
-            seen.add((i,j))
-            for nr, nc in [(i-1,j),(i+1,j), (i,j+1), (i,j-1)]:
-                # if 0 <= nr < m and 0<=nc < n and grid[nr][nc]=="1" and (nr,nc) not in seen:
-                # watch out
-
-                    dfs(nr,nc)
-                    
-                    
+        count =  0
+        n = len(grid)
+        m = len(grid[0])
         seen = set()
-        count = 0
-        for i in range(m):
-            for j in range(n):
+
+        def dfs(r, c):
+
+            seen.add((r,c))
+            for dx, dy in [[0,1],[1,0],[0,-1], [-1,0]]:
+                nr = r + dx
+                nc = c + dy
+                if 0 <= nr < n and 0 <= nc< m and (nr,nc) not in seen and grid[nr][nc]=="1":
+                    dfs(nr,nc)
+
+
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j]=="1" and (i,j) not in seen:
                     dfs(i,j)
                     count+=1
-           
         return count
-
-
-
-
-
-
-
-
-
-
-
-        
-        # M = len(grid)
-        # N = len(grid[0])
-        # seen = set()
-
-        
-        # def dfs(i,j):
-
-        #     if i < 0 or i >=M or j < 0 or j >=N or (i,j) in seen:
-        #         return
-            
-        #     if grid[i][j]== "1":
-        #         seen.add((i,j))
-        #         dfs(i+1,j)
-        #         dfs(i-1,j)
-        #         dfs(i,j-1)
-        #         dfs(i,j+1)
-        #     return 
-        
-
-        # count = 0
-        # for i in range(M):
-        #     for j in range(N):
-        #         if grid[i][j]== "1" and (i,j) not in seen:
-        #             dfs(i,j)
-        #             count+=1
-        # return count

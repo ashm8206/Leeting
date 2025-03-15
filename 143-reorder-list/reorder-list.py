868054e94a1a16175799919b788db7ad8ba29683
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-14 17:52:08 -0700

    Time: 0 ms (100.00%) | Memory: 23.3 MB (38.25%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,78 +8,57 @@ class Solution:
     def middle(self, head) -> ListNode:
         slow, fast = head, head
         prev = None
-
         while fast and fast.next:
-            # prev = slow 
+            # we check fast.next to ensure we can take 2 steps
+            # 1x : 2x fast
+            # 1x --> reach the middle most point
+            prev = slow
             slow = slow.next
             fast = fast.next.next
-        # if prev:
-        #     prev.next = None
+        prev.next = None
         return slow
 
     def reverse(self, head) -> ListNode:
-        if head is None or head.next is None:
-            return head
-        
-        newHead = self.reverse(head.next)
-        last = head.next
-        last.next = head
-        head.next = None
-        
-        return newHead
-        
-        #Iterative
-
-        # dummy = ListNode(-1)
-        # dummy.next = head
-        # prev = dummy
-        # curr = head
-
-        # while curr:
-        #    new = curr.next
-        #    curr.next = prev
-        #    prev = curr
-        #    curr = new 
-
-        # dummy.next = prev
-        # return dummy.next
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
 
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        # middle of linkedlist
-        # n/2 + 1 --> used in this prb
-        # n/2-1
-        prehead = ListNode(-1)
-        prehead.next = head
-        prev = prehead
-        mid = self.middle(head)
-        l1, l2 = head, self.reverse(mid)
-      
-        flip = 1
-        while l1 is not None and l2 is not None:
-            # print(l1.val, l2.val)
-            if flip:
-                prev.next = l1
-                l1 = l1.next
-            else:
-                prev.next = l2
-                l2 = l2.next
-            flip^=1
-            # print(l1.val if l1 else None, l2.val if l2 else None)
-            # print("***")
-            prev = prev.next
-        # first half is still connected to second half at Mid
-        # second half Mid is the last node and it is None
+        if head is None or head.next is None:
+            return head
 
-        # mid can be printed either by l1 or l2 branch
-        # but as soon as it is printed, its Next is NONE
-        #  AND we break out of l1 is Not None and l2 is Not None.
-        
-        # print(l1, l2)
-        return prehead.next
-       
+        # this will put middle in l2
+        # remember to detach the prev from Middle
+
+        mid = self.middle(head)
+
+        l1, l2 = head, self.reverse(mid)
 
     
-
+        prehead = ListNode(-1)
+        prehead.next = head
+        curr = prehead
+        while l1 or l2:
+            if l1:
+                curr.next = l1
+                l1 = l1.next
+                curr = curr.next
+            else:
+                curr.next = None
+                l1 = None
+            if l2:
+                curr.next = l2
+                l2 = l2.next
+                curr = curr.next
+            else:
+                curr.next = None
+                l2 = None
+        return prehead.next

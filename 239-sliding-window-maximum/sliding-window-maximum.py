ee7a6e5df961e9c1e220e66e6339e8aa21aeb18e
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-15 00:09:44 -0700

    Time: 163 ms (67.70%) | Memory: 32.2 MB (23.20%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,26 +1,12 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        #  Max, the next max  etc
 
-        # l, r = 0, k-1
-        # n = len(nums)
-       
-        # res = [0] * (n-k+1)
-        
-        # while r < n:
-            
-        #     max_val = max(nums[l:r+1])
-        #     #print(max_val)
-        #     res[l] = max_val
-        #     l+= 1
-        #     r+=1
-        # return res
-
-        
-        # 
         n = len(nums)
         dq = deque()
         res = []
         for i in range(n):
+            #  0, 1, 2, 3, 4
 
             if dq and dq[0] == i-k: 
                 # we know for window ending at "i"
@@ -28,23 +14,19 @@ class Solution:
                 # i-k+1 or R-L+1 is idx of the first element
                 dq.popleft()
 
-           
-            # if i >= k and dq:
-                # dq.popleft()
-            # Each iteration pop although logical, doesnt work 
-            # As we pop the queue anytime we see a larger element
-            #  This random pops, mess up the window logic.
-
-                
-
             # Monotonically decreasing queue
-            while dq and nums[i] > nums[dq[-1]]:
+            while dq and nums[dq[-1]] < nums[i]:
                 # Largest, then next largest
                 dq.pop()
 
             dq.append(i)
-            res.append(nums[dq[0]])
+            res.append(nums[dq[0]]) # will be the max element
+             # add next smaller 
         
-        # n-k+1 = num of windows
-        # windows start from ?  k-1
+        # Number of windows
+        # N-K: idx of last valid window
+        #  Number of windows = # of arrays ending at this index  = idx+1
+        #  Number of windows = N-K +1
+              
         return res[k-1:] 
+        # return starting from first window

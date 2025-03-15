89d8d21aaec453f9c17a06deaa11c85a56ac40d8
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-15 15:39:19 -0700

    Time: 7 ms (70.99%) | Memory: 21.9 MB (36.99%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
@@ -10,41 +10,91 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+    # def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
         
-        def findMiddle(root):
-            if root is None or root.next is None:
-                return None, root, None
 
-            left = head
-            slow = head
-            fast = head
-            prev = None
-            while fast and fast.next:
-                prev = slow
-                slow = slow.next
-                fast = fast.next.next
+        # def findMiddle(root):
+        #     if root is None or root.next is None:
+        #         return root
 
-            if prev:
-                # so that you can send left
-                prev.next = None
+        #     slow = head
+        #     fast = head
+        #     prev = None
+        #     while fast and fast.next:
+        #         prev = slow
+        #         slow = slow.next
+        #         fast = fast.next.next
+
+        #     if prev:
+        #         #unlink lift
+        #         prev.next = None
             
-            # unlink slow   
-            right = slow.next
-            slow.next = None
-            
-            return left, slow, right
-  
+        #     return slow
+
+          
   
 
-        if head is None:
-            return head
         
-        left, mid, right = findMiddle(head)
-        node = TreeNode(mid.val)
-        node.left = self.sortedListToBST(left)
-        node.right = self.sortedListToBST(right)
-        return node
+        # if head is None:  # or head.next is None: return head of Type TREENODE
+        #     return None
+
+        # if head.next is None:
+        #     return TreeNode(head.val)
+        
+   
+        # mid =  findMiddle(head)
+        # node = TreeNode(mid.val)
+
+        # node.left = self.sortedListToBST(head)
+        # node.right = self.sortedListToBST(mid.next)
+        # return node
+
+        # # Taking the Middle most ensure balanced Property
+        # #TC:  ONLOGN : 
+        # #SC:  O(LogN)
+
+
+    def findSize(self, head: ListNode) -> int:
+        ptr = head
+        c = 0
+        while ptr:
+            ptr = ptr.next
+            c += 1
+        return c
+
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+
+        # Get the size of the linked list first
+        size = self.findSize(head)
+
+        # Recursively form a BST out of linked list from l --> r
+        def convert(l: int, r: int) -> TreeNode:
+            nonlocal head
+
+            # Invalid case
+            if l > r:
+                return None
+        
+            mid = (l + r) // 2
+            
+            # recurse left
+            left = convert(l, mid - 1)
+
+            # place
+            node = TreeNode(head.val)
+            node.left = left
+
+            # head will pt always need to pt to next node to place
+            # hence made head Non-local
+            head = head.next
+
+            # Recurse on right
+            node.right = convert(mid + 1, r)
+            return node
+
+        return convert(0, size - 1)
+
+        
         
         
 

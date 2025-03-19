e8057f013038862a1531f33a9f8dac4389d80140
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-19 14:31:49 -0700

    Time: 32 ms (28.82%) | Memory: 19.5 MB (22.67%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,18 +1,6 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
-        # seen = set()
-        # def area(r, c):
-        #     if not (0 <= r < len(grid) and 0 <= c < len(grid[0])
-        #             and (r, c) not in seen and grid[r][c]):
-        #         return 0
-        #     seen.add((r, c))
-        #     return (1 + area(r+1, c) + area(r-1, c) +
-        #             area(r, c-1) + area(r, c+1))
-
-        # return max(area(r, c)
-        #            for r in range(len(grid))
-        #            for c in range(len(grid[0])))
 
         N = len(grid)
         M = len(grid[0])
@@ -25,7 +13,7 @@ class Solution:
 
             seen.add((r, c))
 
-            ans = 1
+            ans = 1 # Imp
             ans+=get_area(r+1, c)
             ans+=get_area(r-1, c)
             ans+=get_area(r, c-1) 

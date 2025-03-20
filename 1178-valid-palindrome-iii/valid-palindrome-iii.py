6fe46262fe0923d75173a4b1f1b335725361723d
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-20 14:07:41 -0700

    Time: 373 ms (95.13%) | Memory: 91 MB (49.51%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.py
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.py
@@ -1,17 +1,45 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
+
+        memo = {}
+        i = 0
+        j = len(s) - 1
         n = len(s)
-        memo =  [[0 for j in range(n+1)] for i in range(n+1)]
 
-        t = s[::-1]
+        def helper(i, j):
+            if j < i  or i==j:
+                # single numbered are palindromes
+                # if they cross each other return
+                return 0
 
-        for i in range(1, n+1):
-            for j in range(1,n+1):
-                if s[i-1]==t[j-1]:
-                    memo[i][j] = 1 + memo[i-1][j-1]
-                else:
-                    memo[i][j] = max(memo[i][j-1], memo[i-1][j])
-        
-    
-   
-        return n - memo[n][n] <= k
+           
+            if (i, j) in memo:
+                return memo[(i, j)]
+                
+            if s[i] == s[j]:
+                memo[(i, j)] = helper(i + 1, j - 1)
+            else:
+                # print(s[i],s[j], i, j, helper(i + 1, j), helper(i, j - 1))
+                memo[(i, j)] = 1 + min(helper(i + 1, j), helper(i, j - 1))
+
+            return memo[(i, j)]
+
+        ans = helper(i, j)
+        return ans <= k
+
+        # aaba
+
+        # n = len(s)
+        # memo =  [[0 for j in range(n+1)] for i in range(n+1)]
+
+        # t = s[::-1]
+
+        # for i in range(1, n+1):
+        #     for j in range(1, n+1):
+        #         if s[i-1]==t[j-1]:
+        #             memo[i][j] = 1 + memo[i-1][j-1]
+        #         else:
+        #             memo[i][j] = max(memo[i][j-1], memo[i-1][j])
+
+        # # #  TotalLen - LPS = Substitutions
+        # return n - memo[n][n] <= k

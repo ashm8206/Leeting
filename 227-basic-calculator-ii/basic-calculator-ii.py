f31906cbc4b06f21a32c113d8f484db9f79acf15
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-20 13:16:33 -0700

    Time: 43 ms (97.58%) | Memory: 18.9 MB (79.74%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,26 +1,30 @@
 class Solution:
     def calculate(self, s: str) -> int:
-
-        
         # stack = []
-        pre_operation = '+'
         curr_num = 0
         last_num = 0
         result = 0
+        
+        pre_operation = '+'
         s += '+'
         for ch in s:
-    
+            
             if ch.isdigit():
                 curr_num = curr_num * 10 +  int(ch)
 
             elif ch in ('+','-','*','/'):
-                
-                # curr_num = int(curr_num)
+            
 
                 if pre_operation == '-':
-                    # stack.append(-curr_num)
+                    #print(result, last_num, curr_num, ch)
                     result += last_num
                     last_num = - curr_num
+                
+                elif  pre_operation == '+':
+                    # stack.append(int(curr_num))
+                    result += last_num
+                    last_num = curr_num
+                    
 
                 elif pre_operation =='*':
                     # no check required as expression is valid
@@ -28,20 +32,18 @@ class Solution:
                     # op2 = curr_num
                     # stack.append(op1*op2)
                     last_num = last_num * curr_num
-                elif pre_operation =='/':
+                else:
                     # op1 = stack.pop()
                     # op2 = curr_num
                     # stack.append(int(op1/op2))
                     last_num = int(last_num / curr_num)
-                else:
-                    # stack.append(int(curr_num))
-                    result += last_num
-                    last_num = curr_num
+                
 
                 # update the operation
                 # update the curr_number
-                curr_num = 0
+                
                 pre_operation = ch
+                curr_num = 0
 
         return result + last_num
         

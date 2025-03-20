6db262976d3791470b3f48fb454a22d04b261619
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-20 14:01:08 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.49%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,36 +1,62 @@
 from collections import deque
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-    #    a  p  p  l, e
-    #    0, 1, 2, 3, 4
-    # Double digit char has to be the complete range
-    # i + (dd + 1)? == nextDogit
 
         abrLen = len(abbr)
         wordLen = len(word)
         numberStr = ''
+        incre = 0
 
-        word_idx = 0
+        j = 0
         i = 0
-        while i < abrLen and word_idx < wordLen:
-       
-            if word[word_idx] == abbr[i]:
-                i+=1
-                word_idx+=1
-
-            elif abbr[i]=="0":
-                
-                return False
         
-            elif abbr[i].isdigit():
-                while i < abrLen and abbr[i].isdigit():
-                    numberStr += abbr[i]
-                    i+=1
-                if len(numberStr) > 0:
-                    word_idx += int(numberStr)
-                    numberStr = ''
+        while i < abrLen and j < wordLen:
+
+            
+            if abbr[i]==word[j]:
+                i+=1
+                j+=1
             else:
-                return False
+                # if num is digit number: try adjust for next round
+                # else: mistmatch return False
+                if abbr[i].isdigit():
+                    num_str = ''
+                    while i < abrLen and abbr[i].isdigit():
+                        num_str+=abbr[i]
+                        i+=1
+            
+                    if len(num_str) > 0:
+                        if num_str[0]=="0":
+                            return False
+                        j+= int(num_str)
+                        num_str = ''
+                else:
+                    #mistmatch
+                    return False
+           
+        return j == wordLen and i == abrLen
+                
+
+            
+
+       
+            # if word[word_idx] == abbr[i]:
+            #     i+=1
+            #     word_idx+=1
+
+            # elif abbr[i]=="0":
+                
+            #     return False
+        
+            # elif abbr[i].isdigit():
+            #     while i < abrLen and abbr[i].isdigit():
+            #         numberStr += abbr[i]
+            #         i+=1
+            #     if len(numberStr) > 0:
+            #         word_idx += int(numberStr)
+            #         numberStr = ''
+            # else:
+            #     return False
             
         return i==abrLen and word_idx == wordLen
             

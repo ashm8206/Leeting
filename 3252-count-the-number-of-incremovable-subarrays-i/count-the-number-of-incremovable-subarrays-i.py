a673467356f1f2ebc2047da4a145a0243c21abcc
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-22 17:55:01 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.64%) - LeetSync

--- 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.py
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.py
@@ -6,37 +6,52 @@ class Solution:
         # Handle simple edge cases
         if n == 1:
             return 1
-        # if n == 2:
-        #     return 3
-            
+        
         # Find longest prefix that is strictly increasing
         left = 0
-        while left < n - 1 and nums[left] < nums[left + 1]:
+        while left + 1 < n and nums[left] < nums[left + 1]:
             left += 1
+
+         # If the entire array is already strictly increasing
+        if left == n - 1:
+            return (n * (n + 1)) // 2
             
         # Find longest suffix that is strictly increasing
         right = n - 1
-        while right > 0 and nums[right] > nums[right - 1]:
+        while left < right  and nums[right - 1] < nums[right]:
             right -= 1
             
-        # If the entire array is already strictly increasing
-        if left >= right:
-            return (n * (n + 1)) // 2
+       
             
         # Count subarrays to remove
-        result = n + left - right + 2
-        left = 0
-        
-        # Count valid removals when connecting prefix and suffix
-        while left <= left and right < n:
-            if nums[left] < nums[right]:
+        # remove each individual elements + remove entire array
+        result = n + 1 + (left+1 - right) # overlap
+        i = 0
+
+        while i <=left and right < n:
+            if nums[i] < nums[right]:
                 result += n - right
-                
-                if left < n - 1 and nums[left] < nums[left + 1]:
-                    left += 1
+                # len == [right-i-1]
+                #{len subarray that if removed will: make result sorted}         
+                if i + 1 < n and nums[i] < nums[i + 1]:
+                    i+=1
                 else:
                     break
             else:
-                right += 1
-                
+                right+=1
         return result
+
+        
+        # # Count valid removals when connecting prefix and suffix
+        # while left <= left and right < n:
+        #     if nums[left] < nums[right]:
+        #         result += n - right
+                
+        #         if left < n - 1 and nums[left] < nums[left + 1]:
+        #             left += 1
+        #         else:
+        #             break
+        #     else:
+        #         right += 1
+                
+        # return result

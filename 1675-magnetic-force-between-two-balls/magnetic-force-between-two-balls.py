7becc07ce9eb1ca41dc28ffb49af2a8a70d58860
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-03-31 14:30:12 -0700

    Time: 507 ms (48.58%) | Memory: 30.4 MB (53.19%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
@@ -3,8 +3,14 @@ class Solution:
         n = len(position)
         position.sort()
         
+        # in this problem although niot explicit said
+        # adjaceny matters
+
+        # For example, if positions were [10, 1, 5, 8], without sorting, your algorithm would consider adjacent positions as 10→1, 1→5, 5→8, which doesn't represent their actual order on the number line.
+# 
         def count(d):
-            ans, curr = 1, position[0]
+            ans = 1
+            curr = position[0]
             for i in range(1, n):
                 if position[i] - curr >= d:
                     ans += 1
@@ -13,7 +19,7 @@ class Solution:
         
         l, r = 0, position[-1] - position[0]
         while l < r:
-            mid = r - (r - l) // 2
+            mid = (l+r+1)//2
             if count(mid) >= m:
                 l = mid
             else:

f3457fef60b7feacacf47800f1e8946f149847e6
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-04-02 22:14:14 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.09%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,34 +3,43 @@ class Solution:
         if n < 0:
             x = 1/x
             n = abs(n) 
-
-        ans = 1
         
-        def helper(n) -> float:
-            if n == 0:
-                return 1
+        # def helper(n) -> float:
+        #     if n == 0:
+        #         return 1
 
-            half = helper(n//2)
+        #     half = helper(n//2)
 
-            if n%2:
-               return half * half * x
-            else:
-                return half * half
+        #     if n%2:
+        #        return half * half * x
+        #     else:
+        #         return half * half
 
-        return helper(n)
+        # return helper(n)
+
+        result = 1
+        while n != 0:
+            # If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
+            if n % 2 == 1:
+                result *= x
+                # n -= 1
+            # We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x *= x
+            n //= 2
+        return result
 
 
-
+        # ans = 1
         # nn = n
-        while nn:
+        # while nn:
 
-            if nn%2:
-                ans = ans * x
-                nn -=1
-            else:
-                x = x * x
-                nn = nn // 2
-        return ans
+        #     if nn%2:
+        #         ans = ans * x
+        #         nn -=1
+        #     else:
+        #         x = x * x
+        #         nn = nn // 2
+        # return ans
 
         
 

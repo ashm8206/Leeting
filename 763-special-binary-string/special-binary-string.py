b27046d5a60368a61cfe15e0e978e2d3680fd252
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-04-06 19:25:24 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.54%) - LeetSync

--- 763-special-binary-string/special-binary-string.py
+++ 763-special-binary-string/special-binary-string.py
@@ -1,18 +1,22 @@
 class Solution:
+    # https://leetcode.com/problems/special-binary-string/solutions/155621/logical-thinking-with-clear-code/
     def makeLargestSpecial(self, s: str) -> str:
-        if not s:
-            return s
+        
+        sublist = []
         bal = 0
-        anchor = 0
-        mountains = []
-        for i, x in enumerate(s):
-            if x == "1":
+        l = 0
+        for r, ch in enumerate(s):
+            if ch == "1":
                 bal+=1
             else:
-                bal-=1
+                bal -=1
             if bal == 0:
-                mountains.append("1{}0".format(
-                    self.makeLargestSpecial(s[anchor+1: i])))
-                anchor = i + 1
-        mountains.sort(reverse = True)
-        return "".join(mountains)
+                sublist.append("1" + self.makeLargestSpecial(s[l+1:r])+"0")           
+                # l... r
+                # 1[l+1....r)0 
+                # construct deeper parenthis
+                # so once valid parenthis found, 
+                # nest them in each other
+                l = r + 1
+        sublist.sort(reverse = True)
+        return "".join(sublist)

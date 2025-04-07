affab7c4cac0ab355aae599fbecd6e9716b9e121
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-04-06 18:03:11 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (36.09%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,42 +1,71 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
         
+        memo = {}
 
         def helper(expression):
+
+            if len(expression)==0:
+                return []
+            if expression in memo:
+                return memo[expression]
+        
             result = []
-
-            if len(expression) == 0:
-                return result # empty list
-
+            if len(expression)<=2 and expression.isdigit():
+                result.append(int(expression))
+                return result
             
-            if len(expression) <= 2 :
-                return [int(expression)]
-
-            # if len(expression) == 1:
-            #     return [int(expression)]
-            
-            # if len(expression) == 2 and expression[0].isdigit():
-            #     return [int(expression)]
-            
-
             for i, ch in enumerate(expression):
-
                 if ch.isdigit():
                     continue
                 
-                left = helper(expression[:i])
+                left =  helper(expression[:i])
                 right = helper(expression[i+1:])
 
-                for left_val in left:
-                    for right_val in right:
+                for l in left:
+                    for r in right:
                         if ch =="+":
-                            result.append(left_val+right_val)
-                        
+                            result.append(l+r)
                         elif ch =="-":
-                            result.append(left_val-right_val)
-                        
+                            result.append(l-r)
                         else:
-                            result.append(left_val*right_val)
-            return result
-                       
+                            result.append(l*r)
+            memo[expression] = result
+            return memo[expression] 
+            
         return helper(expression)
+
+        # def helper(expression):
+        #     result = []
+
+        #     if len(expression) == 0:
+        #         return result # empty list
+
+            
+        #     if len(expression) <= 2 :
+        #         return [int(expression)]
+
+        #     # if len(expression) == 1:
+        #     #     return [int(expression)]
+
+        #     for i, ch in enumerate(expression):
+
+        #         if ch.isdigit():
+        #             continue
+                
+        #         left = helper(expression[:i])
+        #         right = helper(expression[i+1:])
+
+        #         for left_val in left:
+        #             for right_val in right:
+        #                 if ch =="+":
+        #                     result.append(left_val+right_val)
+                        
+        #                 elif ch =="-":
+        #                     result.append(left_val-right_val)
+                        
+        #                 else:
+        #                     result.append(left_val*right_val)
+        #     return result
+                       
+        # return helper(expression)

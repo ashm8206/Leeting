c94c7860e16824f1a163f58835f765fd806f8d9f
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-04-10 13:35:27 -0700

    Time: 259 ms (64.53%) | Memory: 17.8 MB (88.38%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -17,24 +17,34 @@ class Solution:
         #     return maxSum
         # return place(0)
 
-        memo = {}
+        # memo = {}
   
-        def place(idx):
-            if idx==n:
-                return 0
+        # def place(idx):
+        #     if idx==n:
+        #         return 0
 
-            if idx in memo:
-                return memo[idx]
+        #     if idx in memo:
+        #         return memo[idx]
 
-            maxEle = float("-inf")
-            maxSum = float("-inf")
-            for j in range(idx, min(idx+k,n)):
+        #     maxEle = float("-inf")
+        #     maxSum = float("-inf")
+        #     for j in range(idx, min(idx+k,n)):
                 
+        #         maxEle = max(maxEle, arr[j])
+        #         curr_sum = (maxEle * (j-idx+1)) + place(j+1)
+        #         maxSum = max(maxSum,curr_sum)
+        #     memo[idx] = maxSum
+        #     return memo[idx] 
+        # return place(0)
+
+        dp = [0]*(n+1)
+
+        for i in range(n-1,-1,-1):
+            maxEle = 0
+            for j in range(i, min(i+k,n)):
                 maxEle = max(maxEle, arr[j])
-                curr_sum = (maxEle * (j-idx+1)) + place(j+1)
-                maxSum = max(maxSum,curr_sum)
-            memo[idx] = maxSum
-            return memo[idx] 
-        return place(0)
+                curr_sum = (maxEle * (j-i+1)) + dp[j+1]
+                dp[i] = max(dp[i], curr_sum)
+        return dp[0]
 
                 

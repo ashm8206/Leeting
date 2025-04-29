62b4d38a3d1cae2566531252f3a3cef2afdb6ca4
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-04-29 13:28:28 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (57.37%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -6,29 +6,44 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
 
-        res = []
-        def helper(root, path):
-            if not root:
-                return 
+        def preorder(r: TreeNode, curr_number: int) -> None:
+            nonlocal root_to_leaf
+            if r:
+                curr_number = curr_number * 10 + r.val
+                # if it's a leaf, update root-to-leaf sum
+                if not (r.left or r.right):
+                    root_to_leaf += curr_number
 
-            path.append(str(root.val))
-            if root.left is None and root.right is None:
-                res.append(int("".join(path[:])))
-                return 
+                preorder(r.left, curr_number)
+                preorder(r.right, curr_number)
 
-            if root.left:
-                helper(root.left,path)
-                path.pop()
+        root_to_leaf = 0
+        preorder(root, 0)
+        return root_to_leaf
+        # if not root:
+        #     return 0
 
-            if root.right:
-                helper(root.right,path)
-                path.pop()
+        # res = []
+        # def helper(root, path):
+        #     if not root:
+        #         return 
+
+        #     path.append(str(root.val))
+        #     if root.left is None and root.right is None:
+        #         res.append(int("".join(path[:])))
+        #         return 
+
+        #     if root.left:
+        #         helper(root.left,path)
+        #         path.pop()
+
+        #     if root.right:
+        #         helper(root.right,path)
+        #         path.pop()
         
-        helper(root, [])
-        return sum(res)
+        # helper(root, [])
+        # return sum(res)
   
             
             

449608ef8d5a51db556245bb1c7443d8174a87ec
Aishwarya Mali <aishwaryamali94@gmail.com>
2025-12-30 23:11:45 -0800

    Time: 110 ms (83.82%) | Memory: 17.9 MB (100.00%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -13,9 +13,16 @@ class Solution:
             while j < n and target[j]=='_':
                 j+=1
             
+           
+            #Wrong
+            # if (i >= n and j < n) or (j >= n and i < n):
+            #     return False 
+            # i== n and j == n: return True is missing : Why wrong?
+
+            # Why better?
             # if one string exhausted, both strings should be exhausted
             if i == n or j == n:
-                return (i==n and j == n)
+                return (i==n and j == n) # this cond checks that and give F/T accord
             
             if start[i]!=target[j]:
                 return False

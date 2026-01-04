4916d7fcfa473e711e8e9daec5494b0f184454b3
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-03 22:50:36 -0800

    Time: 136 ms (36.54%) | Memory: 36.4 MB (73.08%) - LeetSync

--- 2055-describe-the-painting/describe-the-painting.py
+++ 2055-describe-the-painting/describe-the-painting.py
@@ -1,22 +1,26 @@
 class Solution:
     def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
         mapping = defaultdict(int)
-        maxNum = -1
-        minNum = 10**6
+        # maxNum = -1
+        # minNum = 10**6
         for s, e, c in segments:
             mapping[s] += c
             mapping[e] -= c
-            maxNum = max([maxNum, s, e])
-            minNum = min([minNum, s, e])
+            # maxNum = max([maxNum, s, e])
+            # minNum = min([minNum, s, e])
         
         res = []
         prev, color = None, 0
-        for now in range(minNum, maxNum+1):
-            if now in mapping.keys():
-                if color:
-                    res.append([prev, now, color])
 
-                color += mapping[now]
-                prev = now 
+        for now, _ in sorted(mapping.items(), key = lambda x: x[0]):
+        # for now in range(minNum, maxNum+1):
+            # if now in mapping.keys():
+            if color:
+                res.append([prev, now, color])
+
+            color += mapping[now]
+            prev = now 
         return res 
+
+        
         

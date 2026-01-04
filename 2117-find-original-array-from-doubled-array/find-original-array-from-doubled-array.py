c1baed45229fb02b3b16b48c2ead0c3929b49d5f
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-03 17:09:21 -0800

    Time: 218 ms (31.82%) | Memory: 35 MB (71.33%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.py
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
         # if len(changes) not even return []
@@ -16,35 +16,52 @@ class Solution:
 
 
         arr = []
-        hashMap = defaultdict(list)
+        hashmap = Counter(changed)
 
-        for i, val in enumerate(changed):
-            hashMap[val].append(i)
+        for num in changed:
+            if hashmap.get(num) > 0:
+                hashmap[num] -= 1
+                # decrementing first handles the 0 case
+
+                # always check count > 0 leaves no reason to del
+                twice = num * 2
+                if (hashmap.get(twice) and hashmap.get(twice) > 0):
+                    hashmap[twice] -= 1
+                    arr.append(num)
+                else:
+                    return []
+        return arr
 
 
-        for  i, num in enumerate(changed):
-            if num < 0:
-                continue
+        # hashMap = defaultdict(list)
+
+        # for i, val in enumerate(changed):
+        #     hashMap[val].append(i)
+
+
+        # for  i, num in enumerate(changed):
+        #     if num < 0:
+        #         continue
             
-            double = num*2
+        #     double = num*2
 
-            if double in hashMap:
-                if double == 0:
-                    hashMap[double].remove(i) # index of 0
-                    if len(hashMap[double])==0:
-                        break
+        #     if double in hashMap:
+        #         if double == 0:
+        #             hashMap[double].remove(i) # index of 0
+        #             if len(hashMap[double])==0:
+        #                 continue
 
                 
-                changed[hashMap[double][0]] = -1 
-                # mark pair as used dont use in iteration
-                hashMap[double].pop(0) 
-                if len(hashMap[double]) == 0:
-                    del hashMap[double]
-                # remove its index from list
+        #         changed[hashMap[double][0]] = -1 
+        #         # mark pair as used dont use in iteration
+        #         hashMap[double].pop(0) 
+        #         if len(hashMap[double]) == 0:
+        #             del hashMap[double]
+        #         # remove its index from list
 
-                arr.append(num)
+        #         arr.append(num)
 
-        return arr if len(arr)==half else []
+        # return arr if len(arr)==half else []
 
        
     

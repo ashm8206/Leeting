da47c15765377d10f15adbd3bb4e51c9fdb4222b
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-09 22:55:30 -0800

    Time: 51 ms (76.83%) | Memory: 24.5 MB (6.44%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,22 +6,19 @@ class Solution:
         maxSize = 0
 
         zeros = 0
-        curr_sum = 0 # number of 1s
-        for r in range(n):
-            curr_sum+= nums[r]
 
+        for r in range(n):
             if nums[r]==0:
                 zeros+=1
 
-            while (r-l+1) - curr_sum > 1:
-                curr_sum -=nums[l]
+            while zeros > 1:
                 if nums[l]==0:
                     zeros-=1
                 l+=1
-                
-            if zeros == 0:
-                maxSize = max(maxSize, r-l)
-            else:
-                maxSize = max(maxSize, r-l+1 - zeros) # 
+
+        
+            maxSize = max(maxSize, r-l) 
+            # delete either 1  or O zero from keeping valid window with 1 0
+
         return maxSize
             

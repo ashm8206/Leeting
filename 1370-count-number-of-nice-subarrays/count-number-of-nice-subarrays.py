81c001c269238037bf566001e875cd60e46984f7
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-13 21:22:12 -0800

    Time: 140 ms (19.54%) | Memory: 25.1 MB (19.13%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -29,30 +29,30 @@ class Solution:
 
 
 
-        prefixMap = {0:1}
-        count = 0
+        # prefixMap = {0:1}
+        # count = 0
 
-        curr_sum = 0
+        # curr_sum = 0
 
-        #  Subarray sum equals k
-        #       Keep adding curr_sum to Map with Unique counts
+        # #  Subarray sum equals k
+        # #       Keep adding curr_sum to Map with Unique counts
 
-        #    1. We are looking for curr_sum[j] - curr_sum[i] = k
-                #   or curr[j]-k = curr[i], we are lookin for curr[i]
+        # #    1. We are looking for curr_sum[j] - curr_sum[i] = k
+        #         #   or curr[j]-k = curr[i], we are lookin for curr[i]
 
-        #    if we get curr_sum-k in prefixMap,
-                # we found a some previous value curr[i]... curr[j](curr_sum)
-                # that can be combined with curr_sum
+        # #    if we get curr_sum-k in prefixMap,
+        #         # we found a some previous value curr[i]... curr[j](curr_sum)
+        #         # that can be combined with curr_sum
             
-        #  Subarray has k odd numbers 
-        #  We can model this as nums[i] even = 0 and nums[i] = 1
+        # #  Subarray has k odd numbers 
+        # #  We can model this as nums[i] even = 0 and nums[i] = 1
         
-        for num in nums:
+        # for num in nums:
             
-            curr_sum+= num%2 # odd the 1, even 0
+        #     curr_sum+= num%2 # odd the 1, even 0
             
-            if curr_sum - k  in prefixMap:
-                count+= prefixMap[curr_sum - k ]
+        #     if curr_sum - k  in prefixMap:
+        #         count+= prefixMap[curr_sum - k ]
            
-            prefixMap[curr_sum] = prefixMap.get(curr_sum, 0) + 1
-        return count
+        #     prefixMap[curr_sum] = prefixMap.get(curr_sum, 0) + 1
+        # return count

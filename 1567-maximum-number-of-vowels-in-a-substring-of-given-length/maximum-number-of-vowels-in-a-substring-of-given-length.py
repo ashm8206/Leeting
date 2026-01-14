624e78313ffcaa7485610abb084773aad0b850d4
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-13 16:54:59 -0800

    Time: 71 ms (71.77%) | Memory: 20 MB (6.20%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -11,8 +11,8 @@ class Solution:
         # Slide the window to the right, focus on the added character and the
         # removed character and update "count". Record the largest "count".
         for i in range(k, len(s)):
-            count += int(s[i] in vowels)
             count -= int(s[i - k] in vowels)
+            count += int(s[i] in vowels)
             answer = max(answer, count)
         
         return answer

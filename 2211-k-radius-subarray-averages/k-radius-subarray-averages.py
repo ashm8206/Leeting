8ec5985b780199eb308edeff9b7b4302a22cb8c6
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-13 16:50:04 -0800

    Time: 119 ms (45.88%) | Memory: 35.9 MB (14.64%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -1,19 +1,35 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        divisor = 2*k + 1
+        window_len = 2*k + 1
         n = len(nums)
         res = [-1] * n
 
-        prefixSum = [0]*(n+1)
+        # prefixSum = [0]*(n+1)
+
+        # for i in range(n):
+        #     prefixSum[i+1] = prefixSum[i] + nums[i]
+        
+        # for i in range(n):
+        #     if i-k < 0 or i+k >= n:
+        #         continue
+        #     # i+k == n  i+k == n-1 and thats not in 0 index prefix array of sise n+1
+        #     res[i] = (prefixSum[i+k+1] - prefixSum[i-k]) // window_len
+        
+        # return res
+        # #  TC: O(N)
+        # #  SC: O(N)
+
+        if window_len > n:
+            return res
+             
+        curr_window = sum(nums[:window_len])
 
         for i in range(n):
-            prefixSum[i+1] = prefixSum[i] + nums[i]
-        
-        for i in range(n):
-            if i-k < 0 or i+k >= n:
-                continue
-            # i+k == n  i+k == n-1 and thats not in 0 index prefix array of sise n+1
-            res[i] = (prefixSum[i+k+1] - prefixSum[i-k]) // divisor
-        
+            
+            if i-k >= 0 and i+k < n:
+                res[i] = curr_window // window_len
+                curr_window -= nums[i-k]
+
+                if (i+k+1) < n: # Prep for the next iteration
+                    curr_window += nums[i+k+1]
         return res
-             

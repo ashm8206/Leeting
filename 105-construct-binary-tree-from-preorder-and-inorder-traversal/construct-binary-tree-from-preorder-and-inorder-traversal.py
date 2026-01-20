be1c340e3b629728728369b9e72275e8c656e26f
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-19 21:17:12 -0800

    Time: 329 ms (5.02%) | Memory: 344.4 MB (5.85%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -10,18 +10,21 @@ class Solution:
         # if not preorder and not inorder:
         #     return None
 
-        if not inorder:
-            return None
+        hmap = {val: i for i, val in enumerate(inorder)}
+        def helper(preorder, inorder):
+            if not inorder:
+                return None
 
-        val = preorder.pop(0)
-        root = TreeNode(val)
-        mid = inorder.index(val)
-        
-        root.left = self.buildTree(preorder, inorder[:mid])
-        root.right = self.buildTree(preorder, inorder[mid+1:])
+            val = preorder.pop(0)
+            root = TreeNode(val)
+            mid = hmap.get(val)
+            
+            root.left = self.buildTree(preorder, inorder[:mid])
+            root.right = self.buildTree(preorder, inorder[mid+1:])
 
-        # root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        # root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        # print(preorder[1:mid+1], mid, preorder[mid+1:])
-        return root
+            # root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+            # root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+            # print(preorder[1:mid+1], mid, preorder[mid+1:])
+            return root
+        return helper(preorder, inorder)
         

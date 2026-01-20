ea638b03b03cad3779db16939d65954f6cafa819
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-19 18:11:08 -0800

    Time: 78 ms (13.13%) | Memory: 48 MB (16.18%) - LeetSync

---
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+    # Total 21
+    # 1; 10,  11. = 110
+    # 2: 12   9.  = 108
+    # 3, 17, 4.   =  68
+    # 4. 16  5.   = 80
+    # 5. 15, 6.   = 90
+
+    # 1: Prefix Sum and Store in R 
+    # 2. total Count =  counts
+    # 3. Need visited array
+
+    # 4: One more VLR
+        maxProd = 0
+        MOD = 10**9 + 7
+        def get_tree_sum(root):
+            if root is None:
+                return 0
+            left_sum = get_tree_sum(root.left)
+            right_sum = get_tree_sum(root.right)
+
+            return root.val + left_sum + right_sum
+        
+        total_sum =  get_tree_sum(root)
+
+        def helper(root):
+            nonlocal maxProd, total_sum
+
+            if root is None:
+                return 0
+            
+            leftSum = helper(root.left)
+            rightSum = helper(root.right)
+
+            total_sub_tree_sum = (root.val + leftSum + rightSum)
+            product = (total_sub_tree_sum * (total_sum - total_sub_tree_sum))
+
+            maxProd = max(product, maxProd)
+
+            return total_sub_tree_sum
+
+        helper(root)
+        return maxProd % MOD

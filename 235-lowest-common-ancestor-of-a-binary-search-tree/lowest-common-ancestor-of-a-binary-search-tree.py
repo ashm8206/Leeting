2e5218f2029c67f96b8a994a8786cea5cec4462e
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-21 22:18:21 -0800

    Time: 54 ms (82.21%) | Memory: 23 MB (5.94%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -9,16 +9,23 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
         #Method I
-        # if not root or p == root or q == root:
-        #     return root
+        if not root or p == root or q == root:
+            return root
 
-        # left = self.lowestCommonAncestor(root.left, p, q)
-        # right = self.lowestCommonAncestor(root.right, p, q)
-
-        # if left and right:
-        #     return root
+        left = None
+        right = None
+        if  p.val < root.val and  q.val < root.val:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            right = self.lowestCommonAncestor(root.right, p, q)
         
-        # return left or right
+        else:
+            return root
+
+        return left or right
+
+
+
 
         # Method II
 
@@ -31,4 +38,8 @@ class Solution:
                 curr = curr.left
             elif (p.val >= curr.val and q.val <= curr.val) or (p.val <= curr.val and q.val >= curr.val):
                 return curr
+
+                # if p < curr and curr > q we found a split
+                # but its possible that p==curr or q>=q
+                # p=2, q=6, in this case 6 is the LCA
         return None

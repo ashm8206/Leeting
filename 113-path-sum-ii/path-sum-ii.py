58ac6d9cb4c8cfaf086a0efd843d11678733b2c2
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-21 16:44:51 -0800

    Time: 0 ms (100.00%) | Memory: 20.1 MB (39.05%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -11,6 +11,7 @@ class Solution:
         path = []
         curr_sum = 0
         def helper(root, curr_sum, path):
+
             if not root:
                 return
 
@@ -22,21 +23,21 @@ class Solution:
                     res.append(path[:]) # new list 
         
 
-                #Boolean Problems vs Filling the list In Problems
-            if root.left:
-                helper(root.left,curr_sum,path)
+            #Boolean Problems vs Filling the list In Problems
+            # if root.left:
+            helper(root.left,curr_sum,path)
                 # path.pop()
             
-            if root.right:
-                helper(root.right,curr_sum,path)
-                # path.pop()
+            # if root.right:
+            helper(root.right,curr_sum,path)
+            path.pop()
             #This Step is Important while passing a Mutable list as slate.
             # for #7 the if condition is not met
             # 7 ---> Two null nodes on Left and Right
             # pops 7 off the path
 
             # then for #2  (7 is no longer on the path)
-            path.pop()
+            # path.pop()
 
     
 

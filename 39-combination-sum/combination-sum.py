de8b8b78705f70b07f505ebcf9eeef1226f0581c
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-27 15:52:51 -0800

    Time: 7 ms (79.71%) | Memory: 19.9 MB (14.93%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,23 +1,43 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:      # THEY HAVE MADE SURE THERE ARE NO DUPLICATES
+
+        
+        n = len(candidates)
         res = []
 
-        n = len(candidates)
-        
-        def backtrack(slate, currSum, start):
-            if currSum == 0:
+        def helper(index, curr_sum, slate):
+
+            if curr_sum == target:
                 res.append(slate[:])
                 return
 
-            for i in range(start, n):
-                if currSum - candidates[i]  >= 0:
-                    # constraints
-                    
-                    slate.append(candidates[i])
-                    backtrack(slate, currSum - candidates[i], i) #---> base Case
-                    slate.pop()
+            if index == n:
+                return
+    
+            if curr_sum + candidates[index] <= target:
+                helper(index, curr_sum + candidates[index], slate +[candidates[index]])       # You can. re-use the elements
+            
+            helper(index+1, curr_sum, slate)
+        helper(0, 0, [])
+        return res
+        # res = []
 
-        backtrack([], target, 0)
+        # n = len(candidates)
+        
+        # def backtrack(slate, currSum, start):
+        #     if currSum == 0:
+        #         res.append(slate[:])
+        #         return
+
+        #     for i in range(start, n):
+        #         if currSum - candidates[i]  >= 0:
+        #             # constraints
+                    
+        #             slate.append(candidates[i])
+        #             backtrack(slate, currSum - candidates[i], i) #---> base Case
+        #             slate.pop()
+
+        # backtrack([], target, 0)
 
         # we are only adding start cuz,
         #  We don;t want duplicate ans tupples

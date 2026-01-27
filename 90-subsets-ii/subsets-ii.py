faecd3446a45be4d6c36d489c6068fe160218d9e
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-27 15:46:17 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (23.28%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -7,26 +7,44 @@ class Solution:
 
         nums.sort()
 
-        def dfs(slate, curr_idx):
+        res = []
+        n = len(nums)
+        def helper(index, slate):
+            
+            if index == n:
+                res.append(slate)
+                return
+            
+            helper(index + 1, slate + [nums[index]])
+    
+            # Skip current element AND all duplicates
+            while index + 1 < n and nums[index] == nums[index + 1]:
+                index += 1
+            
+            helper(index + 1, slate) # dont take
+        helper(0, [])
+        return res
+
+        # def dfs(slate, curr_idx):
       
-            res.append(slate[:])
+        #     res.append(slate[:])
 
-            for idx in range(curr_idx, n):
+        #     for idx in range(curr_idx, n):
 
-                if idx!=curr_idx and nums[idx-1]==nums[idx]:
+        #         if idx!=curr_idx and nums[idx-1]==nums[idx]:
                     
-                    # there is one element before it
-                    #  If the element before it is same
-                    continue
-                #else
+        #             # there is one element before it
+        #             #  If the element before it is same
+        #             continue
+        #         #else
 
-                slate.append(nums[idx]) 
-                dfs(slate, idx+1)
-                slate.pop()
+        #         slate.append(nums[idx]) 
+        #         dfs(slate, idx+1)
+        #         slate.pop()
             
 
-        dfs([], 0)
-        return res
+        # dfs([], 0)
+        # return res
         # nums.sort()
 
         # def backtrack(slate,curr, k):

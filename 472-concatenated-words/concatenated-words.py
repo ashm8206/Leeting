eead9aff317d8ee85e8e7e185622b112c722579d
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-28 12:13:43 -0800

    Time: 162 ms (55.74%) | Memory: 35.7 MB (27.50%) - LeetSync

--- 472-concatenated-words/concatenated-words.py
+++ 472-concatenated-words/concatenated-words.py
@@ -2,18 +2,28 @@ class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
 
         originalSet = set(words)
-        @lru_cache()
+        memo = {}
         def helper(word):
-            
-            for i in range(1, len(word)):
-                prefix = word[:i]
-                suffix = word[i:]
 
+            if word in memo:
+                return memo[word]
+
+            n = len(word)
+
+            res = False
+            for split in range(n):
+                # atleast 2
+                prefix = word[:split+1]
+                suffix = word[split+1:]
+                
+                
                 if prefix in originalSet:
-                    if suffix in originalSet or helper(suffix):
-                            return True
-            return False
-              
+                    if suffix in originalSet or helper(suffix): 
+                        # to divide further
+                        res = True
+                        break
+            memo[word] = res
+            return memo[word]
 
         res = []
 

f93574deaa18e9953d4c899c0116fcd6e608e3ad
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-28 17:59:49 -0800

    Time: 3 ms (98.91%) | Memory: 19.6 MB (39.28%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,47 +1,71 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
 
-        subsetSum, remain = divmod(sum(nums), k)
-        if max(nums) > subsetSum or remain > 0: 
-            return False  # Prune since we can't divide `nums` into subsets where each sums is equal to `subsetSum`
-        n = len(nums)
+        # subsetSum, remain = divmod(sum(nums), k)
+        # if max(nums) > subsetSum or remain > 0: 
+        #     return False  # Prune since we can't divide `nums` into subsets where each sums is equal to `subsetSum`
+        # n = len(nums)
 
-        @lru_cache(None)
-        def dp(mask):
-            if mask == 0: return 0
-            for i in range(n):
-                if (mask >> i) & 1:
-                    newMask = mask ^ (1 << i)
-                    remain = dp(newMask)
-                    if remain == -1: continue  # Skip case can't divide by using `newMask`
-                    if remain + nums[i] <= subsetSum:
-                        return (remain + nums[i]) % subsetSum
-            return -1
+        # @lru_cache(None)
+        # def dp(mask):
+        #     if mask == 0: return 0
+        #     for i in range(n):
+        #         if (mask >> i) & 1:
+        #             newMask = mask ^ (1 << i)
+        #             remain = dp(newMask)
+        #             if remain == -1: continue  # Skip case can't divide by using `newMask`
+        #             if remain + nums[i] <= subsetSum:
+        #                 return (remain + nums[i]) % subsetSum
+        #     return -1
 
-        return dp((1 << n) - 1) == 0
+        # return dp((1 << n) - 1) == 0
         
         nums_sum = sum(nums)
         if nums_sum % k != 0:
             return False
-        subset_sum = nums_sum / k
-
-        ks = [0] * k
+        
+        subset_sum = nums_sum // k
         nums.sort(reverse=True)
-
-
-        def can_partition(j):
-            if j == len(nums):
-                for i in range(k):
-                    if ks[i] != subset_sum:
-                        return False
-                return True
-
-            for i in range(k):
-               
-                if ks[i] + nums[j] <= subset_sum:
-                    ks[i] += nums[j]
-                    if can_partition(j + 1):
-                        return True
-                    ks[i] -= nums[j]
+        n = len(nums)
+        
+        if nums[0] > subset_sum:
             return False
-        return can_partition(0)
+        
+        used = [False] * n
+        
+        def helper(bucket_idx, current_sum, start_idx):
+            # All buckets filled
+            if bucket_idx == k:
+                return True
+            
+            # Current bucket complete, start next bucket
+            if current_sum == subset_sum:
+                return helper(bucket_idx + 1, 0, 0)
+            
+            # For current bucket, decide on each element
+            for i in range(start_idx, n):
+                if used[i]:
+                    continue
+                
+                # Skip duplicates
+                if i > 0 and not used[i-1] and nums[i] == nums[i-1]:
+                    continue
+                
+                # Try taking this element for current bucket
+                if current_sum + nums[i] <= subset_sum:
+                    used[i] = True
+                    
+                    # "Take" this element
+                    if helper(bucket_idx, current_sum + nums[i], i + 1):
+                        return True
+                    
+                    used[i] = False
+                
+                # Pruning: if first element fails in empty bucket
+                if current_sum == 0:
+                    break
+            
+            # Implicitly "don't take" any element (return False)
+            return False
+        
+        return helper(0, 0, 0)

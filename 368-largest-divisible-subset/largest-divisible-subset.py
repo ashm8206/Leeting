773cfe318b2d2e96a9814b0888d746c32bdd5908
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-01-31 14:13:58 -0800

    Time: 163 ms (91.32%) | Memory: 19.6 MB (25.27%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -2,31 +2,38 @@ class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         # a | b and b | c then a | c
 
+        # Sorted: You only need to check nums[j] % nums[i] == 0.
+        # Unsorted: You would have to check both nums[i] % nums[j] == 0 and nums[j] % nums[i] == 0
         n = len(nums)
         dp = [ 1 for i in range(n)]
         nums.sort()
-        prev = [-1  for i in range(n)]
+
+        
+        parent = [0  for i in range(n)]
         maxLen = 1
-        reverseStart = -1
+        lastIndex = 0
 
         for i in range(n):
+            parent[i] = i
             for j in range(0, i):
                 if nums[i]%nums[j]==0:
                     if dp[i] < dp[j] + 1:
                         dp[i] = dp[j] + 1
-                        prev[i] = j
+                        parent[i] = j
 
             if maxLen < dp[i]:
                 maxLen = dp[i]
-                reverseStart = i
+                lastIndex = i
         
         ans = []
         
-        if reverseStart!=-1:
-            nextIdx = reverseStart
-            while nextIdx!=-1:
-                ans.append(nums[nextIdx])
-                nextIdx = prev[nextIdx]
-            return ans[::-1]
-        else:
-            return [nums[0]]
+        i = lastIndex
+
+        while parent[i]!=i:
+            ans.append(nums[i])
+            i = parent[i]
+        
+        # Append the last element
+        ans.append(nums[i])
+        print(parent, dp, i)
+        return ans[::-1]

7fe9f6a5a5a4ed22c6ef67464c62ff1fd1827bf3
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-02 18:55:55 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (31.82%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -3,24 +3,50 @@ class Solution:
         
         
         n, m = len(obstacleGrid), len(obstacleGrid[0])
-        dp =  [[0 for _ in range(m+1)] for _ in range(n+1)]
+        # dp =  [[0 for _ in range(m+1)] for _ in range(n+1)]
 
         
 
         if obstacleGrid[n-1][m-1] == 1 or obstacleGrid[0][0] == 1:
             return 0
         else:
-            dp[n-1][m-1] = 1
+            # dp[n-1][m-1] = 1
+            prev= [0]* (m+1)
+           
+            prev[1] = 1
+            curr = [0]*(m+1)
+
+            for row in range(1, n+1):
+                
+                for col in range(1, m+1):
+                
+                    if obstacleGrid[row-1][col-1] == 1:
+                        # dp[row][col] = 0
+                        curr[col] = 0
+
+                    else:
+                        #dp[row][col]+= (dp[row+1][col] + dp[row][col+1])
+                        curr[col] = (prev[col] +  curr[col-1])
+                prev = curr[:]
+               
+            # print(prev, curr)
+            return prev[m]
+
 
         for row in range(n-1, -1, -1):
+            curr = [0]*(m+1)
             for col in range(m-1, -1, -1):
                 
                 if obstacleGrid[row][col] == 1:
-                    dp[row][col] = 0
+                    # dp[row][col] = 0
+                    curr[col] = 0
 
                 if obstacleGrid[row][col] == 0:
-                    dp[row][col]+= (dp[row+1][col] + dp[row][col+1])
-        return dp[0][0]
+                    #dp[row][col]+= (dp[row+1][col] + dp[row][col+1])
+                    curr[col] += prev[col]+  curr[col+1]
+            prev = curr
+        # return dp[0][0]
+        return prev[0]
 
         # @lru_cache / Recursive
         # def helper(r,c):

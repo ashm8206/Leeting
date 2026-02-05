697111445b14b5093470dcfc0084233fcaaaa7be
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-04 20:31:06 -0800

    Time: 42 ms (93.58%) | Memory: 31.5 MB (38.35%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -7,8 +7,11 @@ class Solution:
         for i in range(1, n):
             current = arr[i] - arr[i-1]
             if current < minimum:
+                # reset array
                 ans = [[arr[i-1], arr[i]]]
                 minimum = current
             elif current == minimum :
                 ans.append([arr[i-1], arr[i]])
         return ans
+
+        

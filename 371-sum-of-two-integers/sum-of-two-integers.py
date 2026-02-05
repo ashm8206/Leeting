76737d717b11cf92bc63d213b6f60072aea7c19c
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-05 15:26:39 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (10.17%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,34 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        x, y = abs(a), abs(b)
+        if x < y: 
+            return self.getSum(b, a)
+        
+        sign = 1
+        if not(a >= 0 and b >= 0):
+            sign = -1
+
+        # OR
+        # abs(a) >= abs(b) --> 
+        # a determines the sign
+        sign = 1 if a > 0 else -1
+        
+        
+        if a * b >= 0:
+            # sum of two positive integers x + y
+            # where x > y
+            while y:
+                answer = x ^ y
+                carry = (x & y) << 1
+                x, y = answer, carry
+        else:
+            # difference of two integers x - y
+            # where x > y
+        
+            while y:
+                answer = x ^ y
+                borrow = ((~x) & y) << 1
+                x, y = answer, borrow
+        
+        return x * sign
+        

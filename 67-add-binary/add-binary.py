cb80883bbdedaf9a73bc1ba263f5cebea3f37c90
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-05 14:56:57 -0800

    Time: 2 ms (51.23%) | Memory: 19.6 MB (9.74%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -2,8 +2,14 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         x, y = int(a, 2), int(b, 2)
         
-        while y: # while carry, keep Summing
-            answer = x ^ y
-            carry = (x & y) << 1
+        while y: # while carry
+
+            answer = x ^ y 
+            # XOR: 0+1/1+0, 1+1 = 0, 0+0= 0
+
+            carry = (x & y) << 1 
+            #(x & y) detects the 1+1, 
+            # which are bound to generate carry 
+            # << 1  and shift the 1 by 1 postio to left making it 10
             x, y = answer, carry
         return bin(x)[2:]

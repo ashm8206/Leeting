22c0471c8870ae2a83759d38a41fcbea8776f2c4
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-05 16:32:58 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (41.78%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -12,9 +12,9 @@ class Solution:
         while l < r:
             mid = (l+r)//2
 
-            #    arr = [2, 3, 4, 7, 11, 12]
+            #    arr =    [2, 3, 4, 7, 11, 12]
             # # Missing = [1, 1, 1, 3, 6, 6] ?
-            # Missing = 11 - [4+1] = 6
+            # Missing = 11 - [index+1] = 6
 
             # a[i]- [ith Index + 1]
             if arr[mid] - (mid +1) < k:
@@ -24,4 +24,28 @@ class Solution:
         #found the pt where there are k missing numbers
         #Jumping k steps to the missing number: return ans
 
+        
         return l + k
+
+
+        
+        # l = 0 Index
+        # r = n
+
+        # if arr[mid] - (mid+1) < k
+        #     l = mid +1 
+               
+        # [2,3,4,7,11]
+        # [1,1,1,3, 6] {arr[i] - (idx+1)} How many missing
+
+        # WHY  l + k?
+
+        # k-th missing = arr[l-1] + (k - missing_before_l)
+        # = arr[l-1] + k - (arr[l-1] - l)
+        # = arr[l-1] + k - arr[l-1] + l
+        # = l + k
+
+        #  7 +  (5 - (arr[l-1] - ((l-1)+1))
+
+        # arr[l-1] +  (5 - (arr[l-1] - (l+1)))
+        # arr[l-1] -arr[l-1] +5 + l 

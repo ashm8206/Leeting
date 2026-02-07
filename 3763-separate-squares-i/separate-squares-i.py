5bf9f99731fcd04853bd96704d8e730b141dfeb8
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-06 23:42:05 -0800

    Time: 1702 ms (63.48%) | Memory: 48.4 MB (41.40%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.py
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -1,13 +1,15 @@
 class Solution:
     def separateSquares(self, squares: List[List[int]]) -> float:
-        # squares.sort(lambda x:x[0])
-        # l = squares[1] + squares[2]
-        # r = squares[-1] + squares[2]
+        
 
+        
+        
 
+        min_y = float("inf")
         max_y, total_area = 0, 0
         for x, y, l in squares:
             total_area += l**2
+            min_y = min(min_y, y)
             max_y = max(max_y, y + l)
 
         def feasible(mid):
@@ -17,7 +19,7 @@ class Solution:
                     area += l * min(mid - y, l)
             return area >= total_area / 2
 
-        l = 0
+        l = min_y
         r = max_y
         eps = 10**-5
         while abs(r - l) > eps:

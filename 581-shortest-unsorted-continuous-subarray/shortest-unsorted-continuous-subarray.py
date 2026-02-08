cae5970ab9a02535d83b4d40a042540cc08b0902
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-08 15:40:16 -0800

    Time: 59 ms (5.06%) | Memory: 20.6 MB (26.40%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,9 +1,29 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
+
+
+        # [2,6,4,8,10,9,15]
+        # [2,4,6,8,9,10,15]
+
+        sorted_arr = sorted(nums)
+        start = -1
+        end = -1
+        for i, (x, y) in enumerate(zip(nums,sorted_arr)):
+            if x!=y:
+                if start==-1:
+                    start = i
+                else:
+                    end = i + 1
+                print(x, y)
+        # print(start, end)
+        return end - start
+            
         n = len(nums)
 
+        #r-l+1
         right = -1
         left = 0
+
 
         big = float("-inf")
         small = float("inf")
@@ -20,10 +40,6 @@ class Solution:
         
         return right - left + 1
         
-
-        
-
-                
        
         # 2,6,4,5,14,9, 11,12,15
         # 2,4 5,6, 9,11,12,14, 15

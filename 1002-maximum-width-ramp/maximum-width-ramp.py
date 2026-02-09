d892da27245efc9c845b70d2adf1d352dbb25b19
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-08 17:56:05 -0800

    Time: 47 ms (80.16%) | Memory: 26.6 MB (60.31%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.py
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,9 +1,26 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
         
-        # Sliding Window
+        # Two ptrs, and monotonic stack
         #Maximum Width Ramp - Leetcode 962 - Python
         # https://www.youtube.com/watch?v=3pTEJ1vzgSI
+
+        # [6,0,10,8,1,5]
+        # [10.10,10,8 5 5]
+        # if nums[i] <= right_max[j]: j+=1
+        # 6...10 : 2. OR
+        # 6.....8: 3 
+
+        # Keep moving j till nums[i] <= right_max[j]
+        # else: move i +=1 to next ptr and compare the rest
+
+
+        
+        
+
+        
+
+
 
         n = len(nums)
         right_max = [None] * n
@@ -12,9 +29,7 @@ class Solution:
         right_max[n - 1] = nums[n - 1]
         for i in range(n - 2, -1, -1):
             right_max[i] = max(right_max[i + 1], nums[i])
-        
-        # print(right_max)
-
+    
         left = 0
         right = 0
         max_width = 0

2a724153b9b4bed4c92d2e66ae6fc2cadfb97772
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-08 16:11:53 -0800

    Time: 0 ms (100.00%) | Memory: 20.1 MB (46.25%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -19,22 +19,23 @@ class Solution:
 
             # Is Overlap ?
             # max(start), min(end) 
-            # last meeting to start. has not end before the first meeting to end
+
             
             new_start = max(A[currA][start],B[currB][start])
             new_end = min(A[currA][end],B[currB][end]) 
 
             if new_start <= new_end:
+                # Start > end, thats not a valid answer
                 result.append([new_start,new_end])
             # https://www.piratekingdom.com/leetcode/tricks/is-overlap
             
             # Since intervals are pairwise disjoint
-            # [1,5], [2,6] --> Wrong testcase since they both overlap in B
+            # currB = [[1,5], [2,6]] --> Wrong testcase since they both overlap in B
             # [1,5], [6,10] --> Good Test case, since values need to be strictly increasing
 
             if A[currA][end] < B[currB][end]:
                 # increment the smaller endpt 
-                # as it has wont intersect any further
+                # and see if it intersects with the larger end 
                 currA+=1
             else:
                 currB+=1

50ed18a41839e20478d89332148bacf9a3de40f1
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-15 12:11:25 -0800

    Time: 99 ms (77.44%) | Memory: 70 MB (34.98%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -6,9 +6,9 @@ class TimeMap:
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.keyMap:
-            self.keyMap[key] =[(timestamp,value)]
+            self.keyMap[key] =[[timestamp,value]]
         else:
-            self.keyMap[key].append((timestamp,value))
+            self.keyMap[key].append([timestamp,value])
 
         
 
@@ -23,31 +23,29 @@ class TimeMap:
             return "" 
 
 
-        # idx = bisect.bisect_right(self.keyMap[key], [timestamp])
+        idx = bisect.bisect_right(self.keyMap[key], [timestamp])
 
-        left = 0
-        right = len(self.keyMap[key])
-        # right binsearch
-        while left < right:
-            mid = left + (right-left) // 2
-            if self.keyMap[key][mid][0] <= timestamp:
-                left = mid + 1
-            else:
-                right = mid
-        idx = left
-        # idx = bisect.bisect_left(self.keyMap[key], [timestamp])
-        # print(self.keyMap[key], timestamp)
-        # print(idx)
+        # left = 0
+        # right = len(self.keyMap[key])
+        # # right binsearch
+        # while left < right:
+        #     mid = (left + right) // 2
+        #     if self.keyMap[key][mid][0] <= timestamp:
+        #         left = mid + 1
+        #     else:
+        #         right = mid
+        # idx = left
+        
 
         if idx < len(self.keyMap[key]) and self.keyMap[key][idx][0] == timestamp:
-            # if [idx][0] == timestamp, 
+            
             return self.keyMap[key][idx][1] 
         # if [idx][0] > timestamp, then return idx-1
         # if idx-1 < 0 , return empty
         
         return self.keyMap[key][idx-1][1] if idx - 1 >= 0 else ""
        
-        # return self.keyMap[key][idx-1][1] if idx else ""
+ 
       
 
 # Assumption: timestamp value will be strictly increasing

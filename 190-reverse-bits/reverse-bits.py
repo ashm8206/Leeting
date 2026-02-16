926664f525ad818cd284ca131cdc3f66fda3d8ac
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-15 20:58:41 -0800

    Time: 45 ms (49.39%) | Memory: 19.3 MB (22.24%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,28 +1,10 @@
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
+    def reverseBits(self, n: int) -> int:
+        ret,  power = 0, 31
 
-        # Method I
-
-        # arr = list(bin(n))[2:]
-        # if len(arr) < 32:
-        #     arr= ['0']*(32-len(arr)) + arr
-        
-        # for i in range(16):
-        #     arr[i], arr[31-i] = arr[31-i], arr[i]
-        # # print(arr)
-        # return int("".join(arr),2)
-
-        # Method II
-        ret, power = 0, 31
         while n:
-
-            # isolate the LSB
             ret += (n & 1) << power
-            power -=1
-            n >>= 1
+            # (n & 1) << power only adds to ret when the current bit is 1
+            n = n >> 1
+            power-=1
         return ret
-
-     
-        

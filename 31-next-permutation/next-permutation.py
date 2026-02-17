3c02bb9268920acc50570bbd21e238afd974310d
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-16 20:00:19 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (49.64%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -9,73 +9,51 @@ class Solution:
         # If such arrangement is not possible, the array must be rearranged as the lowest possible order 
         (i.e., sorted in ascending order)
         """
+        # https://leetcode.com/problems/next-permutation/solutions/3473399/beats-100-full-explanation-in-steps-by-r-mdqu
 
-        i = j = len(nums)-1
-        while i > 0 and nums[i-1] >= nums[i]:
-            i -= 1
 
-        if i == 0:   # nums are in descending order
-            nums.reverse()
-            return 
+        # [1, 2, 5, 4, 3]
+        #  break pt is 2
+    
+        # [1, 3, 5, 4, 2]
+        # smallest num (viz is the first num from right)   > 2
+
+        # Now to get the permuation, reverse from swap pt
+        # to find next smallest permuation
+
+        # [1, 3, 2, 5, 4]
+        n = len(nums)
+        breakpt = n - 1
+
+        while breakpt > 0 and nums[breakpt-1] >= nums[breakpt]:
+            breakpt-=1 
+            # find the first number that is nums[i] < nums[i+1]
+            # from end
+
         
-        # find the last "ascending" position
-        while nums[j] <= nums[i-1]:
-            j -= 1
+        if breakpt==0: 
+            # no next permuation exists for a descending array
+            # move to start
+            nums.reverse()
+            return
 
-        nums[i-1], nums[j] = nums[j], nums[i-1]  
-        l, r = i, len(nums)-1  # reverse the second part
+    
+        # find swap index
+        j =  n - 1
 
+        # smallest num (viz is the first num from right)   > breakpt
+        while j > 0 and nums[breakpt-1] >= nums[j]: 
+            j-=1
+        
+        # Swap
+        nums[breakpt-1], nums[j] = nums[j], nums[breakpt-1]
+
+        # Reverse the subsequence to the right of breakpt-1
+        l = breakpt
+        r = n - 1
         while l < r:
             nums[l], nums[r] = nums[r], nums[l]
-            l +=1 
-            r -= 1
+            l+=1
+            r-=1
 
-        # [1,2,3,6,5]
-        #  Step I :
-        #       find where ther number decrease from end [6,5]--> largest arrangement
-        #       3 if the place where changes can be made
-        # Step II:
-        #       Swap 3, with its next largets from the back to get just Next Greater Permuation
-        #       [1,2,5,6,3]
-        # Step III:
-        # to make just next greater, we need to reverse the elements till swap position
-        # [1,2,5,6,3]. --> [1,2,5,3,6].
-
-        # n = len(nums)
-        # i = n - 2
-
-        # # Step 1: Find First Decreasing element, 
-        # while i >=0 and nums[i] >= nums[i+1]:
-        #     i-=1
         
-        # swap = i
-        # # incase of Descending it will be -ve 
-
-        # if i>=0:
-        #     indexVal =  nums[i]
-        #     i = n - 1
-
-        #     # STEP 2: Find its next greater value that the decreasing element  ans Swap
-
-        #     while i>=0 and nums[i]<=indexVal:
-        #         i-=1
-        
-        #     #Element found now, swap it
-
-        #     nums[swap], nums[i] = nums[i], nums[swap]
-
-        #     # print(nums)
-        #     # print(swap, n-1)
-    
-        # start = swap + 1
-        # end = n - 1
-
-        # while start < end:
-        #     nums[start], nums[end] = nums[end], nums[start]
-        #     start+=1
-        #     end-=1
-
-        # return nums
-
-
-

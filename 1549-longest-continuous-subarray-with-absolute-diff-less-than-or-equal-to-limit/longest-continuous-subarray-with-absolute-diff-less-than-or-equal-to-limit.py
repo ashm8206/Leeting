a876ccf61e778e0c83d2c5809c83872212394083
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-17 15:22:31 -0800

    Time: 883 ms (5.03%) | Memory: 29.9 MB (77.51%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -2,34 +2,6 @@ from sortedcontainers import SortedList
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
 
-        # minNum = nums[0]
-        # maxNum = nums[0]
-
-        # n = len(nums)
-        # left = 0
-        # right = 0
-
-        # ans = -1
-        # for right in range(n):
-
-        #     minNum = min(minNum, nums[right])
-        #     maxNum = max(maxNum, nums[right])
-
-        #     if abs(maxNum - minNum) > limit :
-        
-        #         left = right
-        #         minNum = nums[right]
-        #         maxNum = nums[right]
-
-        #         while left > 0  and abs(nums[right] - nums[left-1]) <= limit:
-        #             left-=1
-        #             minNum = min(minNum, nums[left])
-        #             maxNum = max(maxNum, nums[left])
-            
-        #     ans = max(ans, right - left + 1)
-
-        # return ans
-
         '''
         two pointer technique, we need to make it as long as possible need to keep expanding
         need fast way to find min and maximum between some interval (i,j) inclusive

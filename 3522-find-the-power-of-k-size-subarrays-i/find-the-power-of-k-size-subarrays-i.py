eda143c93eaf0d45bdc960f40c9483669d005bd0
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-17 16:19:20 -0800

    Time: 3 ms (88.43%) | Memory: 19.4 MB (40.61%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,19 +1,25 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
 
+        if k == 1:
+            return nums  # If k is 1, every single element is a valid subarray
+
         n = len(nums)
-        res = [0] * (n - k + 1)
-        
+        result = [-1] * (n - k + 1)
+        consecutive_count = 1  # Count of consecutive elements
 
-        for i in range(n-k+1):
-            state = 0
-            for j in range(i+1, i+k):
-                if (nums[j] - nums[j-1])!= 1:
-                    state = -1
-                    break
-                    
-                
-            res[i] = -1 if state==-1 else nums[i+k-1]
+        for i in range(n-1):
+            if nums[i] + 1 == nums[i + 1]:
+                consecutive_count += 1
+            else:
+                consecutive_count = 1  # Reset count if not consecutive
 
+            # If we have enough consecutive elements, update the result
+            if consecutive_count >= k:
+                result[i+1-k+1] = nums[i+1]
+
+        return result        
+    
         return res
-            
+
+    

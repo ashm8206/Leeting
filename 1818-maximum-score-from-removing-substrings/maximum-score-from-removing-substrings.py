6e678c59c182413b7814208af4387fdfd16321d5
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-22 20:28:39 -0800

    Time: 169 ms (79.57%) | Memory: 20.7 MB (44.09%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        total_score = 0
+        high_priority_pair = "ab" if x > y else "ba"
+        low_priority_pair = "ba" if high_priority_pair == "ab" else "ab"
+
+        # First pass: remove high priority pair
+        string_after_first_pass = self.remove_substring(s, high_priority_pair)
+        removed_pairs_count = (len(s) - len(string_after_first_pass)) // 2
+
+        # Calculate score from first pass
+        total_score += removed_pairs_count * max(x, y)
+
+        # Second pass: remove low priority pair
+        string_after_second_pass = self.remove_substring(
+            string_after_first_pass, low_priority_pair
+        )
+        removed_pairs_count = (
+            len(string_after_first_pass) - len(string_after_second_pass)
+        ) // 2
+
+        # Calculate score from second pass
+        total_score += removed_pairs_count * min(x, y)
+
+        return total_score
+
+    def remove_substring(self, input: str, target_pair: str) -> str:
+        stack = []
+
+        for ch in input:
+            if (
+                ch == target_pair[1]
+                and stack
+                and stack[-1] == target_pair[0]
+            ):
+                stack.pop()  # Remove the matching character from the stack
+            else:
+                stack.append(ch)
+
+        # Reconstruct the remaining string after removing target pairs
+        return "".join(stack)
+

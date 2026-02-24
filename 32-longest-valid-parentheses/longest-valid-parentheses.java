48e78924ad140b106b6e5ea804d3085083b3b6eb
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-23 17:53:57 -0800

    Time: 2 ms (95.62%) | Memory: 44.7 MB (91.18%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        
+        // This. is not simple isValid or not
+        // This is whether it can be valid ?
+
+        // L - R, your longest valid parenthis depends on close brack matched
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+
+        // R - L, your longest valid parenthesis depends on opening bracket matched
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+    return maxlength; 
+    }
+}

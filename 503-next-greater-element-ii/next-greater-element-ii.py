a2a3d887643293b4de45d798b121e938c2dc39ca
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-23 23:25:37 -0800

    Time: 23 ms (59.63%) | Memory: 21.3 MB (35.91%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -6,11 +6,13 @@ class Solution:
         nextGreater = [-1]*n
         stack = []
 
-
+        # loop twice
+        # Basically, the 2nd loop is for the Last element / elements in stack
+        # idx, will be added to the stack, 
+            # we do a nother loop to catch any element greater than curr
         for i in range(2*n):
             while stack and nums[stack[-1]] < nums[i%n]:
                 stackTopIdx = stack.pop()
                 nextGreater[stackTopIdx] = nums[i%n]
-            # print(i, nextGreater, stack)
             stack.append(i%n)
         return nextGreater

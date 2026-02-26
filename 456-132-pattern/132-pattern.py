e249120140301d78e7ae8e4c169845105e4c3fd7
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-02-25 22:50:26 -0800

    Time: 43 ms (85.37%) | Memory: 36.6 MB (23.26%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -1,7 +1,27 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        # i, j,  k
-        # 1 < 4 > 2
+
+        k = float("-inf")
+        n = len(nums)
+        stack = []
+        for i in range(n - 1, -1, -1):
+            if nums[i] < k:
+                return True
+
+            while stack and stack[-1] < nums[i]:
+                # found a higher peak in nums[i] --> j
+                k = stack.pop() # previous is a k candidate
+                # we want K to be as large as possible
+                # maximize changes of getting  j between i .. k
+                # so we keep popping
+
+            
+            stack.append(nums[i])
+        return False
+
+        
+
+        
 
         #Approach 
         #  O(n^3)
@@ -11,37 +31,48 @@ class Solution:
         # to broaden the Range nums[i]..........nums[j]
         # to find a nums[k] between such that nums[i] < num[k] < nums[j]
 
+        # Method I : Brute Force
+        # min_i = inf
+        # for j in range(len(nums) - 1):
+        #     min_i = min(min_i, nums[j])
+        #     for k in range(j + 1, len(nums)):
+        #         if min_i < nums[k] < nums[j]:
+        #             return True
+        # return False
+
         n = len(nums)
+        smallest_to_left = [math.inf]*n
+        
+        for i in range(1,n):
+            smallest_to_left[i] = min(smallest_to_left[i-1], nums[i-1])
+        
         stack = []
-        potentialOnes = [10**10]*n
 
+        # 1 < 3 < 2
+        # 1.....2 
+        #  2 > 3 # 
 
+        #  Now I need to find previous greater, that is > nums[i]
         for i in range(n):
-
-            if i==0:
-                potentialOnes[i] = 0
-            elif nums[i] < nums[potentialOnes[i-1]]:
-                potentialOnes[i] = i
-            else:
-                potentialOnes[i] = potentialOnes[i-1]
-
 
             while stack and nums[stack[-1]] <= nums[i]:
                 stack.pop()
-
-            if stack: # 3 element or "j", the new element hasnt displaced it
                 
-                if nums[potentialOnes[stack[-1]]] < nums[i]:
-                    # get "1" or "i"
-                    # going to index 3 in PotentialOnes to get index of 1 
-                    # and find that index in nums to get "i"
-                    # compare to current element or nums[i]== k or "2"
+            if stack:
+                # previousGreater[i] = stack[-1] 
+                # previousGreater[nums[k]] = nums[j], viz stack stop
+
+                if smallest_to_left[stack[-1]] < nums[i]:
+                    # smallest_to_left[j] ==> nums[i] for this "j"
                     return True
-            
+
             stack.append(i)
-    
         return False
 
+
+
+
+        
         # 4 - (WinSize) + 1
         
         # 0, 1, 2, 3, 4(x) 4 not included as it is len(s)

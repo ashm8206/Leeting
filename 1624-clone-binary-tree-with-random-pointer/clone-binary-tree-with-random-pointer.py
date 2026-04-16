42e2bc246d96862e864d19b7a4f8c7f668bc4d15
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-04-15 22:05:52 -0700

    Time: 144 ms (19.86%) | Memory: 24.9 MB (35.62%) - LeetSync

--- 1624-clone-binary-tree-with-random-pointer/clone-binary-tree-with-random-pointer.py
+++ 1624-clone-binary-tree-with-random-pointer/clone-binary-tree-with-random-pointer.py
@@ -7,73 +7,78 @@ from collections import deque
 #         self.right = right
 #         self.random = random
 
+from collections import deque
 class Solution:
     def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
         
     
-        # if root is None:
-        #     return None
+        if root is None:
+            return None
         
-        # queue = collections.deque([root])
-        # seen = {}
-        # level = 0
+        queue = deque()
+        seen = {} # DICT , not for cycles, in BST, but for quick access
         
 
-        # while queue:
-        #     node = queue.popleft()
-        #     new_node = NodeCopy(node.val)
-        #     seen[new_node] = new_node
-            
+        queue.append(root)
+        seen[(root)] = NodeCopy(root.val)
+    
 
-        #     if node.left:
-        #         queue.append(node.left)
-        #         seen[node.left] = NodeCopy(node.left.val)
-        #         new_node.left = seen[node.left]
 
-        #     if node.right:
-        #         queue.append(node.right)
-        #         seen[node.right] = NodeCopy(node.right.val)
-        #         new_node.right = seen[node.right]
 
-        #     if node.random:
-        #         seen[node.random] = NodeCopy(node.random.val)
-        #         new_node.random = seen[node.random]
-            
-        #     print(seen[new_node].val)
-        #     if seen[new_node].left:
-        #         print(new_node.left.val)
-        #     if seen[new_node].right:
-        #         print(new_node.right.val)
-        #     print("-----")
-        #     # print(seen[new_node])
-        #     # seen[node] = new_node
+        while queue:
+            node = queue.popleft()
 
-        #     if level == 0:
-        #         new_root = new_node
-        #     level+=1
-        # return seen[new_root]
-
-        def clone_bt(node):
-
-            if node is None:
-                return None
-            
-            if node in maps: # maps acts like a visited array
-                return maps[node]
-
-            # set in map as PreOrder incase the random ptrs points bck
-            # this avoids maximum recursion exceeded.
-            maps[node] = NodeCopy(node.val)
+            if node.left:
+                
+                queue.append(node.left)
+                seen[node.left] = NodeCopy(node.left.val)
+                seen[node].left = seen[node.left]
         
-            maps[node].left = clone_bt(node.left)
-            maps[node].right = clone_bt(node.right)
-            maps[node].random = clone_bt(node.random)
 
-            return maps[node]
-
-        maps = {}
-        new_root = clone_bt(root)
-        return new_root
+            if node.right:
+                queue.append(node.right)
+                seen[node.right] = NodeCopy(node.right.val)
+                seen[node].right = seen[node.right]
             
             
+        # Pass 2   
+        queue = deque([root])
+        while queue:
+            curr = queue.popleft()
+            
+            if curr.random is not None:
+                # Point to the COPY, not the original node
+                seen[curr].random = seen[curr.random]
+            
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+        
+        return seen[root]
+
+
+        # def clone_bt(node):
+
+        #     if node is None:
+        #         return None
+            
+        #     if node in maps: # maps acts like a visited array
+        #         return maps[node]
+
+        #     # set in map as PreOrder incase the random ptrs points bck
+        #     # this avoids maximum recursion exceeded.
+        #     maps[node] = NodeCopy(node.val)
+        
+        #     maps[node].left = clone_bt(node.left)
+        #     maps[node].right = clone_bt(node.right)
+        #     maps[node].random = clone_bt(node.random)
+
+        #     return maps[node]
+
+        # maps = {}
+        # new_root = clone_bt(root)
+        # return new_root
+            
+        # https://leetcode.com/problems/clone-binary-tree-with-random-pointer/solutions/742100/python-recursive-iterative-dfs/
 

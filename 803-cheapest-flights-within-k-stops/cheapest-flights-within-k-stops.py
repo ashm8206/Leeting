d98d0eebdc455f4c160de13b75ed93bae2b13bd3
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-04-27 14:15:38 -0700

    Time: 7 ms (66.69%) | Memory: 21 MB (28.14%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,76 +1,30 @@
-from collections import defaultdict
-import heapq
-import copy
-class Graph:
-    def __init__(self, vertices):
-        self.vertices = vertices
-        self.adj_list = defaultdict(list)
-
-    def addEdge(self,u,v, wt):
-        self.adj_list[u].append((v,wt))
-
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        graph = defaultdict(list)
+        for u, v, wt in flights:
+            graph[u].append((v, wt))
 
-        #  Bellman
+        # (cost, stops, node)
+        pq = [(0, 0, src)]
 
-        # Distance from source to all other nodes.
-        # dist = [2**31 for i in range(n)]
-        # dist[src] = 0
-
-        # # Run only K+1 times since we want shortest distance in K hops. --> In bellman we run once more to compute shortest distnace with edge
-        # for i in range(k+1):
-        #     #Create a copy of dist vector.
-        #     temp = copy.deepcopy(dist)
-
-        #     for flight in flights: 
-        #         if (dist[flight[0]] != 2**31):
-        #             temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2])
-                
-            
-        #     #Copy the temp vector into dist.
-        #     dist = temp
-        
-        # return -1 if dist[dst] == 2**31 else dist[dst]
-    
-
-
-        # Cheapest flights
-        # flight with lower cost
-        # flight with lower stops. i.e we track 
-
-        g = Graph(n)
-
-        for start, end, wt in flights:
-            g.addEdge(start,end,wt)
-        
-        pq = [(0,0,src)]
-
-        visited = { i: 2**31 for i in range(n)}
-        visited[src] = 0
+        # visited tracks best stops to reach each node
+        # (fewer stops = more flexibility remaining)
+        visited = {}
 
         while pq:
-    
-            curr_wt, curr_stops, node = heapq.heappop(pq)
-
-            # we are allowed a maximum of K levels
-            # we can go upto a maximum of k+1 level
-            # curr_stops - 1 > k
-            if visited[node] < curr_stops or curr_stops - 1 > k :
-                continue
-
-            visited[node] = curr_stops  
+            cost, stops, node = heapq.heappop(pq)
 
             if node == dst:
-                return curr_wt
+                return cost
 
-            
-            for nei, nei_wt in g.adj_list[node]:
-                
-                heapq.heappush(pq,(nei_wt+curr_wt, curr_stops+1, nei))
-    
+            # skip if we've been here with fewer or equal stops
+            if node in visited and visited[node] <= stops or stops > k:
+                continue
+            visited[node] = stops
+
+            for nei, nei_wt in graph[node]:
+                heapq.heappush(pq, (cost + nei_wt, stops + 1, nei))
+
         return -1
-
 
 

9cb87782beed7ec1a277dce8f10cbde04e3188bd
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-04-29 18:55:26 -0700

    Time: 333 ms (5.97%) | Memory: 37.6 MB (7.46%) - LeetSync

---
+++ 3712-minimum-cost-to-make-arrays-identical/minimum-cost-to-make-arrays-identical.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minCost(self, arr: List[int], brr: List[int], k: int) -> int:
+        cost = 0
+        n = len(arr)
+        if arr == brr:
+            return cost 
+    
+        
+        # 1. Find the order of indices that would sort brr descending
+        # This tells us: "The largest value is at index 0, the next at index 2, etc
+        order = sorted(range(len(brr)), key=lambda i: brr[i], reverse=True)
+
+        # 2. Sort arr descending
+        arr_sorted = sorted(arr, reverse=True)
+
+        
+
+        # 3. Place the sorted values of arr into the result based on brr's order
+        shuffled = [0] * len(arr)
+        for rank, original_index in enumerate(order):
+            shuffled[original_index] = arr_sorted[rank]
+
+        cost1 = k + sum(abs(shuffled[i]- brr[i]) for i in range(n))
+        cost2 = sum(abs(arr[i]- brr[i]) for i in range(n))
+        return min(cost1, cost2)

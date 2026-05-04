5a6cac1c6bb4f406589b4b809ef0ec4a2279b81f
Aishwarya Mali <aishwaryamali94@gmail.com>
2026-05-04 11:12:07 -0700

    Time: 134 ms (41.32%) | Memory: 76.8 MB (73.91%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -18,29 +18,24 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key in self.map.keys():
             node_remove = self.map[key]
-            self._remove(node_remove)
-            self._add(node_remove)
-            # remove node
-            # add node at end
-           
+            self._remove(node_remove) # remove node
+            self._add(node_remove)   #  add node at end
             return node_remove.val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.map.keys():
-            node_remove = self.map[key]
-            node_remove.val = value
-            self._remove(node_remove) # dangling now
-            
+            node_remove = self.map[key] # remove
+            node_remove.val = value   # overwrite value
+            self._remove(node_remove) # prev, next None, dangling
         else:
-            self.map[key] = Node(key, value) 
+            self.map[key] = Node(key, value) # prev, next None, dangling
         
-        node = self.map[key]
-        # add the node at tail with updated value 
-        self._add(node)
+        node = self.map[key] # safely access 
+        self._add(node) # add end
 
         if len(self.map) > self.capacity:
-            node_remove = self.head.next
+            node_remove = self.head.next # if exceeds cap, add at begining
             self._remove(node_remove)
             del self.map[node_remove.key]
         
